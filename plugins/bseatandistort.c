0c30f805fbf994c7f3bedc3d91ce1a1542b7fa72
Tim Janik <timj@gtk.org>
2002-05-14 19:04:59 +0000

    get rid of this, it's a misnamed and badly parameterized module.
    Tue May 14 21:05:49 2002  Tim Janik  <timj@gtk.org>
    
            * bsecompressor.[hc]: get rid of this, it's a misnamed and badly
            parameterized module.
    
            * bseatandistort.[hc]: new module, offering the atan distortion
            that could be achived with bsecompressor. this offers a saner linear
            parameterization range and is a lot faster by using an equivalent
            approximation of GSL.
    
    svn path=/trunk/; revision=437

---
+++ plugins/bseatandistort.c
@@ -0,0 +1,270 @@
+/* BseAtanDistort - BSE Arcus Tangens alike Distortion
+ * Copyright (C) 1999, 2000-2002 Tim Janik
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+#include "bseatandistort.h"
+
+#include <bse/gslengine.h>
+#include <bse/gslsignal.h>
+
+
+/* --- parameters --- */
+enum
+{
+  PARAM_0,
+  PARAM_BOOST_AMOUNT
+};
+
+
+/* --- prototypes --- */
+static void	 bse_atan_distort_init		      (BseAtanDistort		*self);
+static void	 bse_atan_distort_class_init	      (BseAtanDistortClass	*class);
+static void	 bse_atan_distort_set_property	      (GObject			*object,
+						       guint                     param_id,
+						       const GValue             *value,
+						       GParamSpec               *pspec);
+static void	 bse_atan_distort_get_property	      (GObject			*object,
+						       guint                     param_id,
+						       GValue                   *value,
+						       GParamSpec               *pspec);
+static void	 bse_atan_distort_context_create      (BseSource		*source,
+						       guint			 context_handle,
+						       GslTrans			*trans);
+static void	 bse_atan_distort_update_modules      (BseAtanDistort		*comp);
+
+
+/* --- variables --- */
+static GType	       type_id_atan_distort = 0;
+static gpointer	       parent_class = NULL;
+static const GTypeInfo type_info_atan_distort = {
+  sizeof (BseAtanDistortClass),
+  
+  (GBaseInitFunc) NULL,
+  (GBaseFinalizeFunc) NULL,
+  (GClassInitFunc) bse_atan_distort_class_init,
+  (GClassFinalizeFunc) NULL,
+  NULL /* class_data */,
+  
+  sizeof (BseAtanDistort),
+  0 /* n_preallocs */,
+  (GInstanceInitFunc) bse_atan_distort_init,
+};
+
+
+/* --- functions --- */
+static void
+bse_atan_distort_class_init (BseAtanDistortClass *class)
+{
+  GObjectClass *gobject_class = G_OBJECT_CLASS (class);
+  BseObjectClass *object_class = BSE_OBJECT_CLASS (class);
+  BseSourceClass *source_class = BSE_SOURCE_CLASS (class);
+  guint channel_id;
+  
+  parent_class = g_type_class_peek_parent (class);
+
+  gobject_class->set_property = bse_atan_distort_set_property;
+  gobject_class->get_property = bse_atan_distort_get_property;
+  
+  source_class->context_create = bse_atan_distort_context_create;
+  
+  bse_object_class_add_param (object_class, "Adjustments",
+			      PARAM_BOOST_AMOUNT,
+			      bse_param_spec_float ("boost_amount", "Boost Amount [%]",
+						    "The atan distortion boost amount (strength) ranges "
+						    "from maximum attenuation (0%) to maximum boost (100%).",
+						    0, 100.0, 50, 5,
+						    BSE_PARAM_DEFAULT | BSE_PARAM_HINT_SCALE));
+
+  channel_id = bse_source_class_add_ichannel (source_class, "Audio In", "Audio Input SIgnal");
+  g_assert (channel_id == BSE_ATAN_DISTORT_ICHANNEL_MONO1);
+  channel_id = bse_source_class_add_ochannel (source_class, "Audio Out", "Distorted Audio Output");
+  g_assert (channel_id == BSE_ATAN_DISTORT_OCHANNEL_MONO1);
+}
+
+static void
+bse_atan_distort_init (BseAtanDistort *self)
+{
+  self->boost_amount = 0.5;
+  self->prescale = gsl_approx_atan1_prescale (self->boost_amount);
+}
+
+static void
+bse_atan_distort_set_property (GObject      *object,
+			       guint         param_id,
+			       const GValue *value,
+			       GParamSpec   *pspec)
+{
+  BseAtanDistort *self = BSE_ATAN_DISTORT (object);
+  
+  switch (param_id)
+    {
+    case PARAM_BOOST_AMOUNT:
+      self->boost_amount = g_value_get_float (value) / 100.0;
+      self->prescale = gsl_approx_atan1_prescale (self->boost_amount);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (self, param_id, pspec);
+      break;
+    }
+  bse_atan_distort_update_modules (self);
+}
+
+static void
+bse_atan_distort_get_property (GObject    *object,
+			       guint       param_id,
+			       GValue     *value,
+			       GParamSpec *pspec)
+{
+  BseAtanDistort *self = BSE_ATAN_DISTORT (object);
+  
+  switch (param_id)
+    {
+    case PARAM_BOOST_AMOUNT:
+      g_value_set_float (value, self->boost_amount * 100.0);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (self, param_id, pspec);
+      break;
+    }
+}
+
+typedef struct
+{
+  gdouble prescale;
+} AtanDistortModule;
+
+static void
+bse_atan_distort_update_modules (BseAtanDistort *self)
+{
+  /* BseAtanDistort's settings changed, so we need to update
+   * all associated AtanDistortModules, created by this
+   * BseAtanDistort object.
+   */
+  if (BSE_SOURCE_PREPARED (self))
+    {
+      /* we're prepared, that means we have engine modules currently
+       * processing data (each associated with a unique context ID).
+       * now we need to let each of these modules know about the new
+       * settings. bse_source_update_modules() will visit all modules
+       * that got created in bse_atan_distort_context_create().
+       * upon visiting each, it'll copy the contents of &self->prescale
+       * into AtanDistortModule->prescale as soon as that module goes
+       * idle (stops calculating sound data in atan_distort_process()).
+       */
+      bse_source_update_modules (BSE_SOURCE (self),
+				 G_STRUCT_OFFSET (AtanDistortModule, prescale),
+				 &self->prescale, sizeof (self->prescale),
+				 NULL);
+    }
+}
+
+static void
+atan_distort_process (GslModule *module,
+		      guint      n_values)
+{
+  AtanDistortModule *admod = module->user_data;
+  const gfloat *sig_in = module->istreams[BSE_ATAN_DISTORT_ICHANNEL_MONO1].values;
+  gfloat *sig_out = module->ostreams[BSE_ATAN_DISTORT_OCHANNEL_MONO1].values;
+  gfloat *bound = sig_out + n_values;
+  gdouble prescale = admod->prescale;
+
+  /* we don't need to process any data if our input or
+   * output stream isn't connected
+   */
+  if (!module->istreams[BSE_ATAN_DISTORT_ICHANNEL_MONO1].connected ||
+      !module->ostreams[BSE_ATAN_DISTORT_OCHANNEL_MONO1].connected)
+    {
+      /* reset our output buffer to static-0s, this is faster
+       * than using memset()
+       */
+      module->ostreams[BSE_ATAN_DISTORT_OCHANNEL_MONO1].values = gsl_engine_const_values (0);
+      return;
+    }
+
+  /* do the mixing */
+  do
+    *sig_out++ = gsl_approx_atan1 (prescale * *sig_in++);
+  while (sig_out < bound);
+}
+
+static void
+bse_atan_distort_context_create (BseSource *source,
+				 guint      context_handle,
+				 GslTrans  *trans)
+{
+  static const GslClass admod_class = {
+    BSE_ATAN_DISTORT_N_ICHANNELS, /* n_istreams */
+    0,                            /* n_jstreams */
+    BSE_ATAN_DISTORT_N_OCHANNELS, /* n_ostreams */
+    atan_distort_process,	  /* process */
+    NULL,                         /* process_defer */
+    NULL,                         /* reconnect */
+    (GslModuleFreeFunc) g_free,	  /* free */
+    GSL_COST_NORMAL,		  /* cost */
+  };
+  BseAtanDistort *self = BSE_ATAN_DISTORT (source);
+  AtanDistortModule *admod;
+  GslModule *module;
+
+  /* for each context that BseAtanDistort is used in, we create
+   * a GslModule with data portion AtanDistortModule, that runs
+   * in the synthesis engine.
+   */
+  admod = g_new0 (AtanDistortModule, 1);
+
+  /* initial setup of module parameters */
+  admod->prescale = self->prescale;
+
+  /* create a GslModule with AtanDistortModule as user_data */
+  module = gsl_module_new (&admod_class, admod);
+
+  /* the istreams and ostreams of our GslModule map 1:1 to
+   * BseAtanDistort's input/output channels, so we can call
+   * bse_source_set_context_module() which does all the internal
+   * crap of mapping istreams/ostreams of the module to
+   * input/output channels of BseAtanDistort
+   */
+  bse_source_set_context_module (source, context_handle, module);
+
+  /* commit module to engine */
+  gsl_trans_add (trans, gsl_job_integrate (module));
+  
+  /* chain parent class' handler */
+  BSE_SOURCE_CLASS (parent_class)->context_create (source, context_handle, trans);
+}
+
+
+/* --- Export to BSE --- */
+#include "./icons/atan.c"
+BSE_EXPORTS_BEGIN (BSE_PLUGIN_NAME);	/* start of macro magic for BSE plugins */
+BSE_EXPORT_OBJECTS = {			/* this plugin implements an object type */
+  { &type_id_atan_distort,		/* variable to store object type id in */
+    "BseAtanDistort",			/* name of this object type */
+    "BseSource",			/* parent type */
+    "BseAtanDistort compresses or expands the input signal with distortion "
+    "(in a manner similar to the atan(3) mathematical function, thus it's name). "
+    "The strength with which the input signal is treated is adjustable from "
+    "maximum attenuation to maximum boost.",
+    &type_info_atan_distort,		/* type information */
+    "/Modules/Distort/Atan Distort",	/* category (menu hierachy entry) */
+    { ATAN_IMAGE_BYTES_PER_PIXEL | BSE_PIXDATA_1BYTE_RLE,
+      ATAN_IMAGE_WIDTH, ATAN_IMAGE_HEIGHT,
+      ATAN_IMAGE_RLE_PIXEL_DATA, },
+  },
+  { NULL, },
+};
+BSE_EXPORTS_END;			/* end of macro magic for BSE plugins */
---
+++ plugins/bseatandistort.h
@@ -0,0 +1,75 @@
+/* BseAtanDistort - BSE Arcus Tangens alike Distortion
+ * Copyright (C) 1999, 2000-2002 Tim Janik
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+#ifndef __BSE_ATAN_DISTORT_H__
+#define __BSE_ATAN_DISTORT_H__
+
+#define  BSE_PLUGIN_NAME  "BseAtanDistort"
+
+#include <bse/bseplugin.h>
+#include <bse/bsesource.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+
+/* --- object type macros --- */
+#define BSE_TYPE_ATAN_DISTORT              (type_id_atan_distort)
+#define BSE_ATAN_DISTORT(object)           (G_TYPE_CHECK_INSTANCE_CAST ((object), BSE_TYPE_ATAN_DISTORT, BseAtanDistort))
+#define BSE_ATAN_DISTORT_CLASS(class)      (G_TYPE_CHECK_CLASS_CAST ((class), BSE_TYPE_ATAN_DISTORT, BseAtanDistortClass))
+#define BSE_IS_ATAN_DISTORT(object)        (G_TYPE_CHECK_INSTANCE_TYPE ((object), BSE_TYPE_ATAN_DISTORT))
+#define BSE_IS_ATAN_DISTORT_CLASS(class)   (G_TYPE_CHECK_CLASS_TYPE ((class), BSE_TYPE_ATAN_DISTORT))
+#define BSE_ATAN_DISTORT_GET_CLASS(object) (G_TYPE_INSTANCE_GET_CLASS ((object), BSE_TYPE_ATAN_DISTORT, BseAtanDistortClass))
+
+
+/* --- BseAtanDistort source --- */
+typedef struct _BseAtanDistort      BseAtanDistort;
+typedef struct _BseAtanDistortClass BseAtanDistortClass;
+struct _BseAtanDistort
+{
+  BseSource         parent_object;
+
+  gfloat	    boost_amount;
+  gdouble	    prescale;
+};
+struct _BseAtanDistortClass
+{
+  BseSourceClass parent_class;
+};
+
+
+/* --- channels --- */
+enum
+{
+  BSE_ATAN_DISTORT_ICHANNEL_MONO1,
+  BSE_ATAN_DISTORT_N_ICHANNELS
+};
+enum
+{
+  BSE_ATAN_DISTORT_OCHANNEL_MONO1,
+  BSE_ATAN_DISTORT_N_OCHANNELS
+};
+
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* __BSE_ATAN_DISTORT_H__ */
