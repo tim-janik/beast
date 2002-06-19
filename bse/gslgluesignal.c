7f8cf11611330381f1a9520aefa502ebee438fc6
Tim Janik <timj@gtk.org>
2002-06-19 01:19:30 +0000

    new object, emitting notification of incomming MIDI events.
    Thu Jun 13 07:28:34 2002  Tim Janik  <timj@gtk.org>
    
            * bsemidinotifier.[hcp]*: new object, emitting notification of incomming
            MIDI events.
    
            * bsescriptcontrol.[hcp]*: new object, proxying running scripts and
            allowing for user side control of scripts.
    
            * *.c: more i/o channel name fixups.
    
            * bsebindata.c: code cleanups.
    
            * bsecategories.c: check type name syntax.
    
            * bsecomwire.[hc]: removed temporary hacks, slight fixup to
            random request id generation.
    
            * bsecontainer.proc: added lookup procedure to find a named child
            of a container.
    
            * *.proc: const cleanups.
    
            * bseenums.[hc]: added user error enum values.
    
            * bseitem.proc: added is_a check procedure.
    
            * bsemidimodule.c:
            * bsemididevice-oss.c: changes due to new timestamped and thread-safe
            MIDI decoder API.
    
            * bsemidievent.[hc]: midi decoder API extensions to provide
            time stamped MIDI events. explicitely marked mutex-affected
            functions and implemented thread-safety measures.
    
            * bseobject.[hc] (bse_object_do_dispose): set the
            BSE_OBJECT_FLAG_DISPOSED flag on objects, since we don't support
            resurrection in general.
            (bse_object_class_add_dsignal): allow addition of detailed signals.
            (bse_object_from_id): don't hand out disposed objects.
    
            * bseparasite.c: fixed string-escaping bug.
    
            * bsepart.[hc]: for now, limit the maximum tick count to 0x7fffffff,
            which is a valid signed integer.
    
            * bsescripthelper.[hc]: properly support the GslGlue layer and
            BseScriptControl.
    
            * bseserver.[hcp]*: support container usage of the server.
            moved script handling into bsescriptcontrol.c.
            implment procedure to retrive a MIDI notifier obejct.
    
            * bsesongthread.c: silence debugging output.
    
            * bsesource.proc: allow setting a module's input by name.
            added more getters for channels.
    
            * bsestorage.c: fix string escaping bug.
    
            * bswcommon.c: added handler to convert a BswNoteDescription
            into a GslGlueRecord.
    
    Wed Jun 12 06:31:34 2002  Tim Janik  <timj@gtk.org>
    
            * GSL Glue layer introduction. the code is mature enough for
            our scheme bindings, but still evolving.
    
            * gslglue.[hc]: new files implementing the GslGlue layer.
            the GslGlue layer is a presumably simple API abstraction
            which allowes acces to foreign object systems with notification
            signals, object properties and object procedures.
    
            * gslgluecodec.[hc]: serialization/deserialization layer
            which allowes GslGlue to be used from remote processes
            transparently.
    
            * gslgluesignal.c: implementation of signal notification.
    
            * bseglue.[hc]: new file, imlpementing the GslGlueVTable backend
            for BSE.
    
    Mon May 27 16:45:48 2002  Tim Janik  <timj@gtk.org>
    
            * glib-extra.[hc]: remove the buggy and old g_strdup_quoted() function.
            changed callers to use g_strescape (string, NULL) instead.
    
            * bsequery.c (show_procdoc): fallback to procedure blurb if there's
            no help available.
    
            * mkcproc.pl: support procedure BLURBs.
    
            * gslcommon.[hc]: added function to return main thread handle.
    
            * gsl*.[hc]: nuked GslPollFD, use GPollFD instead.
    
    svn path=/trunk/; revision=462

---
+++ bse/gslgluesignal.c
@@ -0,0 +1,230 @@
+/* GSL - Generic Sound Layer
+ * Copyright (C) 2002 Tim Janik
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+
+typedef struct {
+  gulong    proxy;
+  gchar    *signal;
+  GHookList hooks;
+} GlueSignal;
+
+typedef struct {
+  gchar      *signal;
+  GslGlueRec *args;
+  gboolean    destroyed;
+} GlueEvent;
+
+
+/* --- prototypes --- */
+static guint    glue_signal_hash        (gconstpointer  key);
+static gboolean glue_signal_equal       (gconstpointer  key1,
+					 gconstpointer  key2);
+
+
+/* --- functions --- */
+static guint
+glue_signal_hash (gconstpointer key)
+{
+  const GlueSignal *s = key;
+  guint h = s->proxy;
+
+  h ^= g_str_hash (s->signal);
+
+  return h;
+}
+
+static gboolean
+glue_signal_equal (gconstpointer key1,
+		   gconstpointer key2)
+{
+  const GlueSignal *s1 = key1;
+  const GlueSignal *s2 = key2;
+
+  if (s1->proxy != s2->proxy)
+    return FALSE;
+  return strcmp (s1->signal, s2->signal) == 0;
+}
+
+static GlueSignal*
+glue_signal_lookup (GslGlueContext *context,
+		    gulong          proxy,
+		    const gchar    *signal)
+{
+  GlueSignal key;
+  key.proxy = proxy;
+  key.signal = (gchar*) signal;
+
+  return g_hash_table_lookup (context->sighash, &key);
+}
+
+static void
+gsl_glue_signals_dispatch (GslGlueContext *context)
+{
+  static gboolean in_glue_signals_dispatch = FALSE;
+
+  g_return_if_fail (in_glue_signals_dispatch == FALSE);
+
+  in_glue_signals_dispatch = TRUE;
+  
+  while (context->sigqueue)
+    {
+      GslRing *node = context->sigqueue;
+      GlueEvent *sevent = node->data;
+      GlueSignal *sig;
+      gulong proxy;
+      GHook *hook;
+
+      g_return_if_fail (sevent->args->fields[0].glue_type == GSL_GLUE_TYPE_PROXY);
+
+      context->sigqueue = gsl_ring_remove_node (context->sigqueue, node);
+      proxy = sevent->args->fields[0].value.v_proxy;
+      sig = glue_signal_lookup (context, proxy, sevent->signal);
+
+      hook = g_hook_first_valid (&sig->hooks, FALSE);
+      while (hook)
+	{
+	  GslGlueSignalFunc func = (GslGlueSignalFunc) hook->func;
+	  gboolean was_in_call = G_HOOK_IN_CALL (hook);
+
+	  hook->flags |= G_HOOK_FLAG_IN_CALL;
+	  func (hook->data, sevent->signal, sevent->args);
+	  if (!was_in_call)
+	    hook->flags &= ~G_HOOK_FLAG_IN_CALL;
+	  hook = g_hook_next_valid (&sig->hooks, hook, FALSE);
+	}
+      
+      gsl_glue_free_rec (sevent->args);
+      g_free (sevent->signal);
+      g_free (sevent);
+    }
+
+  while (context->gc_signals)
+    {
+      GslRing *node = context->gc_signals;
+      GlueSignal *sig = node->data;
+      GHook *hook;
+
+      context->gc_signals = gsl_ring_remove_node (context->gc_signals, node);
+
+      sig = g_hash_table_lookup (context->sighash, sig);
+      hook = g_hook_first_valid (&sig->hooks, TRUE);
+      if (hook)
+	g_hook_unref (&sig->hooks, hook);
+      else
+	{
+	  g_hook_list_clear (&sig->hooks);
+	  g_hash_table_remove (context->sighash, sig);
+	  context->table.signal_connection (context, sig->signal, sig->proxy, FALSE);
+	  g_free (sig->signal);
+	  g_free (sig);
+	}
+    }
+
+  in_glue_signals_dispatch = FALSE;
+}
+
+gulong
+gsl_glue_signal_connect (const gchar      *signal,
+			 gulong            proxy,
+			 GslGlueSignalFunc func,
+			 gpointer          sig_data,
+			 GDestroyNotify    sig_data_free)
+{
+  GslGlueContext *context = gsl_glue_fetch_context (G_STRLOC);
+  GlueSignal *sig;
+  GHook *hook;
+  
+  g_return_val_if_fail (proxy > 0, 0);
+  g_return_val_if_fail (signal != NULL, 0);
+  g_return_val_if_fail (func != NULL, 0);
+
+  sig = glue_signal_lookup (context, proxy, signal);
+  if (!sig)
+    {
+      if (context->table.signal_connection (context, signal, proxy, TRUE))
+	{
+	  sig = g_new0 (GlueSignal, 1);
+	  g_hook_list_init (&sig->hooks, sizeof (GHook));
+	  sig->proxy = proxy;
+	  sig->signal = g_strdup (signal);
+	  g_hash_table_insert (context->sighash, sig, sig);
+	}
+      else
+	{
+	  if (sig_data_free)
+	    sig_data_free (sig_data);
+	  return 0;
+	}
+    }
+  hook = g_hook_alloc (&sig->hooks);
+  hook->data = sig_data;
+  hook->destroy = sig_data_free;
+  hook->func = func;
+  g_hook_append (&sig->hooks, hook);
+
+  return hook->hook_id;
+}
+
+void
+gsl_glue_signal_disconnect (const gchar *signal,
+			    gulong       proxy,
+			    gulong       connection_id)
+{
+  GslGlueContext *context = gsl_glue_fetch_context (G_STRLOC);
+  GlueSignal *sig;
+  GHook *hook;
+  gboolean destroyed = FALSE;
+
+  g_return_if_fail (proxy > 0);
+  g_return_if_fail (signal != NULL);
+  g_return_if_fail (connection_id > 0);
+
+  sig = glue_signal_lookup (context, proxy, signal);
+  if (sig)
+    {
+      destroyed = g_hook_destroy (&sig->hooks, connection_id);
+      hook = g_hook_first_valid (&sig->hooks, TRUE);
+      if (hook)
+	g_hook_unref (&sig->hooks, hook);
+      else if (!gsl_ring_find (context->gc_signals, sig))
+	context->gc_signals = gsl_ring_prepend (context->gc_signals, sig);
+    }
+  if (!destroyed)
+    g_message ("no such signal (id=%lu) to disconnect", connection_id);
+}
+
+void
+gsl_glue_enqueue_signal_event (const gchar *signal,
+			       GslGlueRec  *args,
+			       gboolean     disabled)
+{
+  GslGlueContext *context = gsl_glue_fetch_context (G_STRLOC);
+  GlueEvent *sevent;
+
+  g_return_if_fail (signal != NULL);
+  g_return_if_fail (args != NULL);
+  g_return_if_fail (args->n_fields > 0);
+  g_return_if_fail (args->fields[0].glue_type == GSL_GLUE_TYPE_PROXY);
+
+  sevent = g_new0 (GlueEvent, 1);
+  sevent->signal = g_strdup (signal);
+  sevent->args = gsl_glue_recdup (args);
+  sevent->destroyed = disabled != FALSE;
+  context->sigqueue = gsl_ring_append (context->sigqueue, sevent);
+}
