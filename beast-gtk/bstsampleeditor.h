db16478d4c8327a09892951c01bcdcab36de85a8
Tim Janik <timj@gtk.org>
2002-08-30 01:28:49 +0000

    up version to v0.4.1rc4.
    Fri Aug 30 03:21:31 2002  Tim Janik  <timj@gtk.org>
    
            * configure.in: up version to v0.4.1rc4.
    
            * bsw-shell/*.[hc]:
            * bsw/*.[hc]:
            * beast-gtk/*.[hc]: update copyrights and license headers.
    
            * AUTHORS: make this the canonical AUTHORS file for the whole
            package, update names.
    
            * put notes about licensing into COPYING. ship COPYING.GPL
            and COPYING.LGPL.
    
    svn path=/trunk/; revision=533

--- beast-gtk/bstsampleeditor.h
+++ beast-gtk/bstsampleeditor.h
@@ -1,5 +1,5 @@
 /* BEAST - Bedevilled Audio System
- * Copyright (C) 1998-1999, 2000-2002 Tim Janik
+ * Copyright (C) 1998-2002 Tim Janik
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
