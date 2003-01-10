c742029b32dbc4bcea666f180db77719ea117412
Tim Janik <timj@gtk.org>
2003-01-10 06:43:24 +0000

    fix filename markup, added constants matching rules.
    Fri Jan 10 07:42:02 2003  Tim Janik  <timj@gtk.org>
    
            * docs/generated/log2texi.sh (ChangeLog2texi): fix filename markup,
            added constants matching rules.
    
    svn path=/trunk/; revision=839

--- docs/generated/log2texi.sh
+++ docs/generated/log2texi.sh
@@ -2,23 +2,40 @@
 
 SYMBOL_PATTERN='[*0-9A-Z?][*0-9A-Za-z_?]*\(()\)\{0,1\}'
 FUNC_PATTERN='[*0-9A-Za-z?][*0-9A-Za-z_?]*\(()\)\{0,1\}'
-SYMBOL_PREFIX='\(Sfi\|Gsl\|Bse\|Bst\|Gtk\|Gdk\|Gnome\)'
-  FUNC_PREFIX='\(sfi\|gsl\|bse\|bst\|gtk\|gdk\|gnome\)'
- MACRO_PREFIX='\(SFI\|GSL\|BSE\|BST\|GTK\|Gdk\|GNOME\)'
+SYMBOL_PREFIX='\(Sfi\|Gsl\|Bse\|Bst\|Gxk\|Gtk\|Gdk\|Gnome\|G\)'
+  FUNC_PREFIX='\(sfi\|gsl\|bse\|bst\|gxk\|gtk\|gdk\|gnome\|g\)'
+ MACRO_PREFIX='\(SFI\|GSL\|BSE\|BST\|GXK\|GTK\|GDK\|GNOME\|G\)'
+CONST1_EXPR='\b\([0-9]\+\.[0-9]\+\([eE][+-]\?[0-9]\+\)\?\)'
+CONST2_EXPR='\b\([0-9]\+\.\?\([eE][+-]\?[0-9]\+\)\)'
+CONST3_EXPR='\b\([0-9]\+\.\)'
+CONST4_EXPR='\B\(\.[0-9]\+\([eE][+-]\?[0-9]\+\)\?\)'
+CONST5_EXPR='\b\([0-9]\+[LlUu]\+\)\b'
+CONST6_EXPR='\b\([0-9]\+\)'
+CONST7_EXPR='\b\(0[xX][A-Fa-f0-9]\+\)'
 
 export SYMBOL_PATTERN FUNC_PATTERN SYMBOL_PREFIX FUNC_PREFIX MACRO_PREFIX
 
 function ChangeLog2texi ()
 {
+    # file name markup: file names may be pretty much everything except
+    # - names starting with an @
+    # - names containing ( or ) or , or : or ' or spaces
+    # - names containing @ or { or } (due to a makeinfo bug)
+    # file start: [-!-&+.-9;-?A-z|~*]
+    # file rest:  [-!-&+.-9;-?A-z|~*]
     sed \
 	-e 's/[{}@]/@&/g' \
+        -e '/^[ 	]\+\* [^:]\+:/ { ' \
+          -e ':NextFile;' \
+          -e 's/^\([^:]*\*[^:]*\) \([-!-&+.-9;-?A-z|~*][-!-&+.-9;-?A-z|~*]*\)/\1 @file{\2}/;' \
+          -e 'tNextFile;' \
+        -e '}' \
 	-e 's/<\([^@]*@[^>]*\)>/mailto:\1:end-mailto/g' \
 	-e 's/mailto:\([^@]*@[^ ]*\):end-mailto/<@uref{mailto:\/\/\1}\>/g' \
 	-e 's/\(ftp:\/\/[^ 	]*[^.;,]\)/@uref{\1}/g' \
         -e 's/[^">]\(ftp:\/\/[^ 	]*$\)/@uref{\1}/g' \
 	-e 's/[^">]\(http:\/\/[^ 	]*$\)/@uref{\1}/g' \
 	-e 's/\(http:\/\/[^ 	]*[^.;,]\)/@uref{\1}/g' \
-	-e '/^[ 	]\+\* [^:]\+:/s/[ 	]*\(\* \)\([^:(<>)]*\)/\1@file{\2}/' \
 	-e '/^[ 	]\+[^*]/s/^[ 	]\+/  /' \
 	-e '/^[		]\+\* [^:]\+$/s/^[ 	]\+//' \
 	-e '/^[12][-0-9]\{9\}/s/^\(.*\)$/@unnumberedsec @code{\1}/' \
@@ -26,9 +43,19 @@ function ChangeLog2texi ()
 	-e 's/\(\b'"$SYMBOL_PREFIX"''"$SYMBOL_PATTERN"'\)/@reference_type{\1}/g' \
 	-e 's/\(\b'"$FUNC_PREFIX"'_'"$FUNC_PATTERN"'\)/@reference_function{\1}/g' \
 	-e 's/\(\b'"$MACRO_PREFIX"'_'"$FUNC_PATTERN"'\)/@reference_constant{\1}/g' \
+	-e '/^[ 	]\+/ { ' \
+	  -e 's/'"$CONST1_EXPR"'/@reference_constant{\1}/g;' \
+	  -e 's/'"$CONST2_EXPR"'/@reference_constant{\1}/g;' \
+	  -e 's/'"$CONST3_EXPR"'/@reference_constant{\1}/g;' \
+	  -e 's/'"$CONST4_EXPR"'/@reference_constant{\1}/g;' \
+	  -e 's/'"$CONST5_EXPR"'/@reference_constant{\1}/g;' \
+	  -e 's/'"$CONST6_EXPR"'/@reference_constant{\1}/g;' \
+	  -e 's/'"$CONST7_EXPR"'/@reference_constant{\1}/g;' \
+        -e '}' \
 	-e 's/$/@*/g' \
 	-e 's/^[ 	]*@\*$//g' \
-	-e '/^@[a-z]/ s/@\*$//g' | perl -e '$_ = join("", <>); s/@\*\n^$/\n/gm; print'
+	-e '/^@[a-z]/ s/@\*$//g' \
+    | perl -e '$_ = join("", <>); s/@\*\n^$/\n/gm; print'
 }
 
 function print_template ()
