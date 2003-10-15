6aed1c544c7f2cd2996f554119ac67df1fff43f5
Tim Janik <timj@gtk.org>
2003-10-15 01:24:13 +0000

    figure versioned gettext domain (GETTEXT_PACKAGE) from ../po-helper.sh.
    Wed Oct 15 02:36:02 2003  Tim Janik  <timj@gtk.org>
    
            * update.sh: figure versioned gettext domain (GETTEXT_PACKAGE)
            from ../po-helper.sh.
    
    svn path=/trunk/; revision=1373

--- po-bse/update.sh
+++ po-bse/update.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-PACKAGE="bse"
+[ -f POTFILES.in -a -f ../po-helper.sh ] || {
+	echo "missing POTFILES.in or ../po-helper.sh, aborting..."
+	exit 1
+}
+
+# define *_GETTEXT_DOMAIN
+. ../po-helper.sh
+
+PACKAGE="$BSE_GETTEXT_DOMAIN"
 PATH="$PATH:.."
 
-echo "Calling intltool-update for you ..."
+echo "intltool-update --gettext-package $PACKAGE $*"
 intltool-update --gettext-package $PACKAGE $*
