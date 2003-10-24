7080fd66fe9e0b5f4ca062b3e6b1a7460505ed4e
Tim Janik <timj@gtk.org>
2003-10-24 23:58:44 +0000

    removed.
    Sat Oct 25 01:58:24 2003  Tim Janik  <timj@gtk.org>
    
            * library/scripts/test.scm: removed.
    
    svn path=/trunk/; revision=1437

--- library/scripts/test.scm
+++
@@ -1,9 +0,0 @@
-(bse-server-register-plugins bse-server)
-(define project (bse-server-use-new-project bse-server "foo"))
-(bse-project-restore-from-file project "../test/simple-loop.bse")
-(bse-server-run-project bse-server project)
-
-(while (bse-project-is-playing project)
-       (display "still playing\n")
-       (sleep 10))
-; (bse-server-halt-project bse-server project)
