aeaa79fc25834cf5d81aeecdcffed29b6141fc0c
Stefan Westerfeld <stefan@space.twc.de>
2003-10-25 15:26:24 +0000

    Licensed under the Green OpenMusic License
    Sat Oct 25 17:14:45 2003  Stefan Westerfeld  <stefan@space.twc.de>
    
    	* library/demo/partymonster.bse: Licensed under the Green OpenMusic
    	License (http://openmusic.linuxtag.org).
    
    	* library/scripts/song-parts-operations.scm: Licensed GPL.
    
    svn path=/trunk/; revision=1446

--- library/scripts/song-parts-operations.scm
+++ library/scripts/song-parts-operations.scm
@@ -1,12 +1,32 @@
+;; 
+;; Copyright (C) 2003 Stefan Westerfeld, stefan@space.twc.de
+;; 
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+;;
+
+;;
 ;; (bse-script-register <func> <category> <blurb> <help> <author> <license> ARGS...)
+;;
 
 (bse-script-register 'song-parts-crop
                      "/Song/Crop parts (loop range)"
 		     "Crop parts within the loop range"
 		     (string-append "This function crops all parts within the loop range "
 				    "and moves parts after the loop range backwards accordingly.")
-		     "Stefan Westerfeld (Author)"
-		     "Copyright (C) 2003 Stefan Westerfeld"
+		     "Stefan Westerfeld"
+		     "GNU General Public License"
 		     (bse-param-song "song"))
 
 (bse-script-register 'song-parts-duplicate
@@ -14,8 +34,8 @@
 		     "Duplicate parts within the loop range"
 		     (string-append "This function duplicates all parts within the loop range "
 				    "and moves parts after the loop range forward accordingly.")
-		     "Stefan Westerfeld (Author)"
-		     "Copyright (C) 2003 Stefan Westerfeld"
+		     "Stefan Westerfeld"
+		     "GNU General Public License"
 		     (bse-param-song "song"))
 
 ;; common code for duplicate & crop:
