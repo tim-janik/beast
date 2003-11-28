6cc6a4cb81b3c1ec495d78bac60c4879d1a009fd
Tim Janik <timj@gtk.org>
2003-11-28 02:35:18 +0000

    reenabled closure test, use BSE_TYPE_AMPLIFIER as test object which is
    Fri Nov 28 03:34:15 2003  Tim Janik  <timj@gtk.org>
    
            * tests/testcxx.cc: reenabled closure test, use BSE_TYPE_AMPLIFIER as
            test object which is implemented in c++.
    
    svn path=/trunk/; revision=1590

--- bse/tests/testcxx.cc
+++ bse/tests/testcxx.cc
@@ -1,5 +1,6 @@
 #include <bse/bsecxxbase.h>
 #include <bse/bsemain.h>
+#include <bse/bseamplifier.gen-idl.h>
 
 namespace {
 using namespace Bse;
@@ -26,7 +27,9 @@ main (int   argc,
 {
   g_thread_init (NULL);
   bse_init_intern (&argc, &argv, NULL);
-
+  std::set_terminate (__gnu_cxx::__verbose_terminate_handler);
+  // g_log_set_always_fatal ((GLogLevelFlags) (G_LOG_LEVEL_WARNING | G_LOG_LEVEL_CRITICAL | (int) g_log_set_always_fatal ((GLogLevelFlags) G_LOG_FATAL_MASK)));
+   
   // test closure Arg types
   Arg<FooTest*> a1;
   a1.token();
@@ -42,13 +45,11 @@ main (int   argc,
   a5.token();
   // tokenize_gtype (0);
 
-#if 0   // need non-abstract C++ object in the core for this test
-  GObject *o = (GObject*) g_object_new (BSE_TYPE_CXX_BASE, NULL);
+  GObject *o = (GObject*) g_object_new (BSE_TYPE_AMPLIFIER, NULL);
   CxxBase *b = cast (o);
   Foo f;
   b->connect ("notify", Closure (&f, &Foo::bar));
   b->connect ("notify", Closure (&f, &Foo::baz));
-#endif
   
   return 0;
 }
