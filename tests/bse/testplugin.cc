bdee969887b22628c867f69b28c6879d50b30938
Tim Janik <timj@gtk.org>
2004-01-26 01:49:15 +0000

    disable C++ API code which breaks again, due to lack of object reference
    Mon Jan 26 02:45:21 2004  Tim Janik  <timj@gtk.org>
    
            * tests/Makefile.am: disable C++ API code which breaks again,
            due to lack of object reference support.
    
            * tests/testplugin.idl: added object properties of types
            Bse::Effect, imlpemented in C++ in Bse and Bse::Item,
            implemented in C in Bse.
    
    svn path=/trunk/; revision=1912

--- tests/bse/testplugin.cc
+++ tests/bse/testplugin.cc
@@ -16,6 +16,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
+#include "bse/bsecxxmodule.h"
 #include "testplugin.gen-idl.h"
 #include <stdexcept>
 #include <math.h>
