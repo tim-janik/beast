6b6de4a3db701492b0f0e14b886a9cc344e41d69
Tim Janik <timj@src.gnome.org>
2004-07-07 17:46:33 +0000

    mor ethread renames
    svn path=/trunk/; revision=2264

--- bse/gslengine.c
+++ bse/gslengine.c
@@ -1333,11 +1333,11 @@ gsl_engine_init (gboolean run_threaded)
 	}
       if (err)
 	g_error ("failed to create wakeup pipe: %s", g_strerror (errno));
-      master_thread = sfi_thread_run ("Synthesis0", (SfiThreadFunc) _engine_master_thread, &master_data);
+      master_thread = sfi_thread_run ("DSP #1", (SfiThreadFunc) _engine_master_thread, &master_data);
       if (!master_thread)
 	g_error ("failed to create master thread");
       if (0)
-	sfi_thread_run ("Synthesis1", slave, NULL);
+	sfi_thread_run ("DSP #2", slave, NULL);
     }
 }
 
