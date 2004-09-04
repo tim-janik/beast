893e3e913b6f0b9346fd8fd7637b9b065cf1fe1d
Tim Janik <timj@src.gnome.org>
2004-09-04 14:54:12 +0000

    license clarification
    svn path=/trunk/; revision=2462

--- sfi/toyprof-mem.c
+++ sfi/toyprof-mem.c
@@ -1,9 +1,20 @@
 /* TOYPROF - Poor man's profiling toy
  * Copyright (C) 2000-2003 Tim Janik
  *
- * This code is distributed in the hope that it will be useful,
+ * This software is provided "as is"; redistribution and modification
+ * is permitted, provided that the following disclaimer is retained.
+ *
+ * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * In no event shall the authors or contributors be liable for any
+ * direct, indirect, incidental, special, exemplary, or consequential
+ * damages (including, but not limited to, procurement of substitute
+ * goods or services; loss of use, data, or profits; or business
+ * interruption) however caused and on any theory of liability, whether
+ * in contract, strict liability, or tort (including negligence or
+ * otherwise) arising in any way out of the use of this software, even
+ * if advised of the possibility of such damage.
  */
 #define	 _GNU_SOURCE	/* enable various glibc features we depend on */
 #include "toyprof-mem.h"
