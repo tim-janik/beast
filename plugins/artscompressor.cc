8ed9a8678212ffb5c8175882cffb81b01bd4b74c
Stefan Westerfeld <stefan@space.twc.de>
2004-09-08 17:01:12 +0000

    Cleanup: new documentation, volume related properties are dB now,
    Wed Sep  8 17:00:04 2004  Stefan Westerfeld  <stefan@space.twc.de>
    
    	* artscompressor.idl artscompressor.cc: Cleanup: new documentation,
    	volume related properties are dB now, compressor ratio is given as x:1
    	and not as fraction (more intuitive), new property auto_output allows
    	adjusting the output amplification automatically. Removed stereo
    	version completely in favour of an implementation which handles both,
    	mono and stereo, in one module. Moved into the Bse namespace.
    
    svn path=/trunk/; revision=2505

--- plugins/artscompressor.cc
+++ plugins/artscompressor.cc
@@ -1,6 +1,6 @@
 /* ArtsCompressor - aRts Compressor Effect
  * Copyright (C) 2001 Matthias Kretz <kretz@kde.org>
- * Copyright (C) 2003 Stefan Westerfeld <stefan@space.twc.de>
+ * Copyright (C) 2003-2004 Stefan Westerfeld <stefan@space.twc.de>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,17 +24,20 @@
 
 #define LN2 0.69314718
 
+namespace Bse {
 namespace Arts {
 using namespace std;
-using namespace Bse;
 
 class Compressor : public CompressorBase
 {
   class Module : public SynthesisModule {
     /* state */
-    gfloat volume;
+    double volume;
     /* params */
-    gfloat threshold, ratio, output, attackfactor, releasefactor;
+    double threshold, threshold_db;   /* threshold as normal and as dB value */
+    double ratio;		      /* 0.5 for 2:1 compression */
+    double output;		      /* linear factor */
+    double attackfactor, releasefactor;
   public:
     void reset()
     {
@@ -43,9 +46,10 @@ class Compressor : public CompressorBase
     
     void config(CompressorProperties *params)
     {
-      threshold = params->threshold;
-      ratio = params->ratio;
-      output = params->output;
+      threshold_db = params->threshold_db;
+      threshold = comp_db2linear (threshold_db);
+      ratio = 1 / params->ratio_to_one;
+      output = comp_db2linear (params->output_db);
       
       if( params->attack == 0 )
         attackfactor = 1;
@@ -57,94 +61,144 @@ class Compressor : public CompressorBase
       else
 	releasefactor = LN2 / ( params->release / 1000 * mix_freq() );
     }
-    
-    void process(unsigned int samples)
+
+    /* conversion doesn't test for linear == 0,
+     * as the input (a volume) is guaranteed to be above threshold
+     */
+    double comp_linear2db (double linear)
     {
-      const float *invalue = istream (ICHANNEL_AUDIO_IN).values;
-      float *outvalue = ostream (OCHANNEL_AUDIO_OUT).values;
+      return 20 * log (linear) / log (10);
+    }
+    double comp_db2linear (double db)
+    {
+      return exp (db / 20 * log (10));
+    }
+    double compress (double input_signal)
+    {
+      double volume_db = comp_linear2db (volume);
+      double output_signal = comp_db2linear ((volume_db - threshold_db) * ratio + threshold_db) / volume * input_signal * output;
+
+      return output_signal;
+    }
+
+    static const int CHANNEL_A1 = 1;
+    static const int CHANNEL_A2 = 2;
+    static const int CHANNELS_A1n_A2n = 0;
+    static const int CHANNELS_A1y_A2n = CHANNEL_A1;
+    static const int CHANNELS_A1n_A2y = CHANNEL_A2;
+    static const int CHANNELS_A1y_A2y = CHANNEL_A1 + CHANNEL_A2;
+
+    template<int channels>
+    void process_loop (unsigned int samples)
+    {
+      const float *invalue1 = istream (ICHANNEL_AUDIO_IN1).values;
+      const float *invalue2 = istream (ICHANNEL_AUDIO_IN2).values;
+      float *outvalue1 = ostream (OCHANNEL_AUDIO_OUT1).values;
+      float *outvalue2 = ostream (OCHANNEL_AUDIO_OUT2).values;
       
       for( unsigned int i = 0; i < samples; i++ ) {
-	float delta = fabs( invalue[i] ) - volume;
+	double delta = 0.0;
+	switch (channels)
+	  {
+	  case CHANNELS_A1n_A2n: delta = -volume;
+				 break;
+	  case CHANNELS_A1y_A2n: delta = fabs (invalue1[i]) - volume;
+				 break;
+	  case CHANNELS_A1n_A2y: delta = fabs (invalue2[i]) - volume;
+				 break;
+	  case CHANNELS_A1y_A2y: delta = max (fabs (invalue1[i]), fabs (invalue2[i])) - volume;
+				 break;
+	  }
+
 	if( delta > 0.0 )
 	  volume += attackfactor * delta;
 	else
 	  volume += releasefactor * delta;
         
-	if( volume > threshold ) {
-	  outvalue[i] = ( ( volume - threshold ) * ratio + threshold ) / volume * invalue[i] * output;
-	} else {
-	  outvalue[i] = invalue[i] * output;
-	}
+	if (volume > threshold)
+	  {
+	    if (channels & CHANNEL_A1)
+	      outvalue1[i] = compress (invalue1[i]);
+	    if (channels & CHANNEL_A2)
+	      outvalue2[i] = compress (invalue2[i]);
+	  }
+	else
+	  {
+	    if (channels & CHANNEL_A1)
+	      outvalue1[i] = invalue1[i] * output;
+	    if (channels & CHANNEL_A2)
+	      outvalue2[i] = invalue2[i] * output;
+	  }
       }
+    }
+    void process (unsigned int n_values)
+    {
+      if (istream (ICHANNEL_AUDIO_IN1).connected)
+	{
+	  if (istream (ICHANNEL_AUDIO_IN2).connected)
+	    {
+	      process_loop <CHANNELS_A1y_A2y> (n_values);
+	    }
+	  else
+	    {
+	      process_loop <CHANNELS_A1y_A2n> (n_values);
+	      ostream_set (OCHANNEL_AUDIO_OUT2, const_values (0));
+	    }
+	}
+      else
+	{
+	  if (istream (ICHANNEL_AUDIO_IN2).connected)
+	    {
+	      process_loop <CHANNELS_A1n_A2y> (n_values);
+	      ostream_set (OCHANNEL_AUDIO_OUT1, const_values (0));
+	    }
+	  else
+	    {
+	      process_loop <CHANNELS_A1n_A2n> (n_values);
+	      ostream_set (OCHANNEL_AUDIO_OUT1, const_values (0));
+	      ostream_set (OCHANNEL_AUDIO_OUT2, const_values (0));
+	    }
+	}
     }
   };
 public:
+  void property_changed (CompressorPropertyID prop_id)
+  {
+    switch (prop_id)
+      {
+      /* implement special handling of GUI properties */
+      case PROP_AUTO_OUTPUT:
+      case PROP_RATIO_TO_ONE:
+      case PROP_THRESHOLD_DB:
+	if (auto_output)
+	  output_db = threshold_db / ratio_to_one - threshold_db;
+        notify ("output_db");
+        break;
+      /* compat properties */
+      case PROP_RATIO:
+        if (ratio > 0)
+	  set ("ratio_to_one", 1 / ratio, NULL);
+	else
+	  set ("ratio_to_one", 10000, NULL); /* max ratio */
+	break;
+      case PROP_THRESHOLD:
+        set ("threshold_db", bse_db_from_factor (threshold, -100), NULL);
+        break;
+      case PROP_OUTPUT:
+        set ("output_db", bse_db_from_factor (output, -100), NULL);
+        break;
+      default: ;
+      }
+  }
   BSE_EFFECT_INTEGRATE_MODULE (Compressor, Module, CompressorProperties);
 };
 
 BSE_CXX_DEFINE_EXPORTS();
 BSE_CXX_REGISTER_EFFECT (Compressor);
 
-class StereoCompressor : public StereoCompressorBase
-{
-  class Module : public SynthesisModule {
-    /* state */
-    gfloat volume;
-    /* params */
-    gfloat threshold, ratio, output, attackfactor, releasefactor;
-  public:
-    void reset()
-    {
-      volume = 0;
-    }
-    
-    void config(StereoCompressorProperties *params)
-    {
-      threshold = params->threshold;
-      ratio = params->ratio;
-      output = params->output;
-      
-      if( params->attack == 0 )
-        attackfactor = 1;
-      else
-	attackfactor = LN2 / ( params->attack / 1000 * mix_freq() );
-      
-      if( params->release == 0 )
-        releasefactor = 1;
-      else
-	releasefactor = LN2 / ( params->release / 1000 * mix_freq() );
-    }
-    
-    void process(unsigned int samples)
-    {
-      const float *inleft = istream (ICHANNEL_LEFT_AUDIO_IN).values;
-      const float *inright = istream (ICHANNEL_RIGHT_AUDIO_IN).values;
-      float *outleft = ostream (OCHANNEL_LEFT_AUDIO_OUT).values;
-      float *outright = ostream (OCHANNEL_RIGHT_AUDIO_OUT).values;
-      
-      for( unsigned int i = 0; i < samples; i++ ) {
-	float delta = max(fabs(inleft[i]), fabs(inright[i])) - volume;
-	if( delta > 0.0 )
-	  volume += attackfactor * delta;
-	else
-	  volume += releasefactor * delta;
-        
-	if( volume > threshold ) {
-	  outleft[i] = ( ( volume - threshold ) * ratio + threshold ) / volume * inleft[i] * output;
-	  outright[i] = ( ( volume - threshold ) * ratio + threshold ) / volume * inright[i] * output;
-	} else {
-	  outleft[i] = inleft[i] * output;
-	  outright[i] = inright[i] * output;
-	}
-      }
-    }
-  };
-public:
-  BSE_EFFECT_INTEGRATE_MODULE (StereoCompressor, Module, StereoCompressorProperties);
-};
-
-BSE_CXX_REGISTER_EFFECT (StereoCompressor);
+// printf ("input: %f dB (%f), output: %f dB (%f)\n", comp_linear2db (volume), volume, comp_linear2db (compress (volume)), compress (volume));
 
 } // Arts
+} // Bse
 
 /* vim:set ts=8 sw=2 sts=2: */
