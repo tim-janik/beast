2447d45fabb83830cde15ef6c39c5fbb7b0a044e
Tim Janik <timj@gtk.org>
2004-09-28 19:48:16 +0000

    added sfi_file_check() and sfi_file_equals() (based on functions from
    Tue Sep 28 21:45:12 2004  Tim Janik  <timj@gtk.org>
    
            * sfifilecrawler.[hc]: added sfi_file_check() and sfi_file_equals()
            (based on functions from bse/gslcommon.c). documented g_file_test_all().
    
    svn path=/trunk/; revision=2643

--- sfi/sfifilecrawler.h
+++ sfi/sfifilecrawler.h
@@ -62,6 +62,12 @@ gchar*          sfi_path_get_filename                   (const gchar  *filename,
                                                          const gchar  *parentdir);
 void            sfi_make_dirpath                        (const gchar  *dir);
 void            sfi_make_dirname_path                   (const gchar  *filename);
+
+/* --- file tests --- */
+gboolean        sfi_file_check /* sets errno */         (const gchar  *file_name,
+                                                         const gchar  *mode);
+gboolean        sfi_file_equals                         (const gchar  *file1,
+                                                         const gchar  *file2);
 gboolean        g_file_test_all                         (const gchar  *filename,
                                                          GFileTest     test);
 
