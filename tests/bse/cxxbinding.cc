d94adffd633597e0c844b3cd335e12447bd7e9b3
Tim Janik <timj@gtk.org>
2006-04-04 00:11:05 +0000

    build and check perftest testwavechunk.
    Tue Apr  4 02:08:16 2006  Tim Janik  <timj@gtk.org>
    
            * slowtests/Makefile.am: build and check perftest testwavechunk.
    
            * tests/Makefile.am: removed outdated rules.
    
            * tests/bse/Makefile.am: build and test cxxbinding and testplugin.la.
    
            * tools/scripts/Makefile.am: added script tools waveloadtest.scm and
            noteplaytest.scm.
    
    Tue Apr  4 01:21:32 2006  Tim Janik  <timj@gtk.org>
    
            * bse/tests/testwavechunk.c:
            * tests/cxxbinding.cc:
            * tests/empty.ogg:
            * tests/perftest.cc:
            * tests/testplugin.cc:
            * tests/testplugin.idl:
            * tests/bse/loophandle.c:
            * tests/scripts/noteplaytest.scm:
            * tests/scripts/waveloadtest.scm: removed from CVS.
    
    svn path=/trunk/; revision=3569

--- tests/bse/cxxbinding.cc
+++ tests/bse/cxxbinding.cc
@@ -16,7 +16,7 @@
  * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
  * Boston, MA 02111-1307, USA.
  */
-#include "bsecxxapi.h"
+#include "bsecxxapi.hh"
 #include <bse/bse.h>
 #include <unistd.h>
 
