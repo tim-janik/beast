d2a7fe09fc99bc74045e38ebd02710e7a5a42796
Tim Janik <timj@gtk.org>
2006-04-04 20:31:15 +0000

    allow printf-style arguments to TSTART(). added TERROR() to forcefully
    Tue Apr  4 21:58:49 2006  Tim Janik  <timj@gtk.org>
    
            * birnettests.h: allow printf-style arguments to TSTART().
            added TERROR() to forcefully abort.
    
    svn path=/trunk/; revision=3574

--- birnet/birnettests.h
+++ birnet/birnettests.h
@@ -30,40 +30,75 @@ BIRNET_EXTERN_C_BEGIN();
 #  define U_(x)		(x)
 #endif
 
+/* --- macros --- */
 /* macros used for testing.
  * note that g_print() does fflush(stdout) automatically.
  * however we're using g_printerr() for test messages to also allow testing
  * of programs which generate output on stdout.
  */
 #ifdef	TEST_VERBOSE
-#define TSTART(what)	do { g_printerr ("%s:\n", what); } while (0)	/* test intro */
+#define TSTART(...)	birnet_test_intro (":\n", __VA_ARGS__)		/* test intro */
 #define TOK()           do { g_printerr ("OK.\n"); } while (0)		/* subtest OK */
 #define TICK()          TOK()						/* subtest OK */
 #define TACK()          do { g_printerr ("ACK.\n"); } while (0)		/* alternate OK */
 #define TFAIL()         do { g_printerr ("FAIL.\n"); } while (0)	/* allowed failure */
-#define	TPRINT		g_printerr					/* misc messages */
+#define	TPRINT(...)	g_printerr (__VA_ARGS__)			/* misc messages */
+#define	TASSERT(code)	TASSERT_impl (code)				/* test assertion */
+#define	TERROR(...)	TERROR_impl (__VA_ARGS__)			/* test assertion */
 #define TDONE()         do { g_printerr ("DONE.\n"); } while (0)	/* test outro */
 #else
-#define TSTART(what)	do { g_printerr ("%s [", what); } while (0)	/* test intro */
+#define TSTART(...)	birnet_test_intro (": [", __VA_ARGS__)		/* test intro */
 #define TOK()           do { g_printerr ("-"); } while (0)		/* subtest OK */
 #define TICK()          TOK()						/* subtest OK */
 #define TACK()          do { g_printerr ("+"); } while (0)		/* alternate OK */
 #define TFAIL()         do { g_printerr ("X"); } while (0)		/* allowed failure */
 #define	TPRINT(...)	do { g_printerr ("*"); } while (0)		/* skip messages */
+#define	TASSERT(code)	TASSERT_impl (code)				/* test assertion */
+#define	TERROR(...)	TERROR_impl (__VA_ARGS__)			/* test assertion */
 #define TDONE()         do { g_printerr ("]\n"); } while (0)		/* test outro */
 #endif
 
-/* test assertion */
-#define TASSERT(code)	do { 				\
+/* --- prototypes --- */
+static inline void birnet_init_test (int*, char***);
+static inline void birnet_test_intro (const char *postfix,
+				      const char *format,
+				      ...) G_GNUC_PRINTF (2, 3);
+
+#define TASSERT_impl(code)	do {			\
   if (code) TOK (); else {				\
   TFAIL();						\
-  g_error ("%s:%u: %s: assertion failed: %s",		\
+  g_error ("%s:%u:%s(): assertion failed: %s",		\
            __FILE__, __LINE__, __PRETTY_FUNCTION__,	\
            #code); }					\
 } while (0)
 
+#define TERROR_impl(...)	do {			\
+  TFAIL();						\
+  char *_error_msg_ = g_strdup_printf (__VA_ARGS__);	\
+  g_error ("%s:%u:%s(): %s",				\
+           __FILE__, __LINE__, __PRETTY_FUNCTION__,	\
+           _error_msg_);				\
+  g_free (_error_msg_);					\
+} while (0)
+
+
+
+/* --- implmentation details --- */
+static inline void
+birnet_test_intro (const char *postfix,
+		   const char *format,
+		   ...)
+{
+  va_list args;
+  va_start (args, format);
+  char *msg = g_strdup_vprintf (format, args);
+  va_end (args);
+  g_printerr ("%s%s", msg, postfix);
+  g_free (msg);
+}
+
 /* convenience initialization functions for tests */
-extern inline void
+static inline void
 birnet_init_test (int    *argc,
 		  char ***argv)
 {
