74478b0996f0cf30a228dde58617071db3450c20
Tim Janik <timj@gtk.org>
2006-10-16 23:06:27 +0000

    -programs are supposed to be quick sanity tests, to be executed e.g.
    Mon Oct 16 22:53:35 2006 Tim Janik <timj@gtk.org>
    
            * this change accomplishes the move to distinguish test programs
            only by the makefile rules they are executed at. check:-programs
            are supposed to be quick sanity tests, to be executed e.g. before
            commits. slowcheck:-programs are in place to check implementations,
            features and auxillary files, and may consume significant amounts
            of time. perf:-programs are programs run as performance tests and
            should provide performance measurement information as output.
            all three targets are supposed to pass without errors for make
            distcheck.
    
            * tests/audio/: moved here from slowtests/audio/.
            * tests/filecheck/: moved here from slowtests/filecheck/.
    
            * tests/audio/Makefile.am: slight fixups.
    
            * tests/.:
            * tests/Makefile.am: moved slowtests/ contents here.
            added perftest and testresampler to perf: rule.
    
            * Makefile.decl: added recursive perf: rule. silenced check-local:.
    
    svn path=/trunk/; revision=3982

---
+++ tests/perftest.cc
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2003,2004 Stefan Westerfeld
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+#include "bsecxxapi.hh"
+#include <bse/bse.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <time.h>
+
+static SfiGlueContext *bse_context = NULL;
+
+using namespace Bse;
+using namespace std;
+
+double
+gettime ()
+{
+  timeval tv;
+  gettimeofday (&tv, 0);
+
+  return double(tv.tv_sec) + double(tv.tv_usec) * (1.0 / 1000000.0);
+}
+
+int main(int argc, char **argv)
+{
+  std::set_terminate (__gnu_cxx::__verbose_terminate_handler);
+  bse_init_async (&argc, &argv, "Perftest", NULL);
+  sfi_msg_allow ("misc");
+  bse_context = bse_init_glue_context (argv[0]);
+
+  sfi_glue_context_push (bse_context);
+
+  printf ("%s: testing remote glue layer calls via C++ interface:\n", argv[0]);
+
+  const int max_calls = 10000;
+  double start = gettime ();
+
+  for(int i=0; i < max_calls; i++)
+    note_to_freq (60, 0);
+
+  double t = gettime () - start;
+  printf ("%f seconds for %d invocations => %f invocations/second, %f milli seconds per invocation\n",
+          t, max_calls, max_calls / t, t * 1000 / max_calls);
+
+  sfi_glue_context_pop ();
+}
+
+/* vim:set ts=8 sts=2 sw=2: */
