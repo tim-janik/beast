41e940c34091fa27553c4bd5048560830559d0e3
Tim Janik <timj@gtk.org>
2006-10-16 23:05:58 +0000

    -programs are supposed to be quick sanity tests, to be executed e.g.
    Mon Oct 16 22:53:35 2006 Tim Janik <timj@gtk.org>
    
            * this change accomplishes the move to distinguish test programs
            only by the makefile rules they are executed at. check:-programs
            are supposed to be quick sanity tests, to be executed e.g. before
            commits. slowcheck:-programs are in place to check implementations,
            features and auxillary files, and may consume significant amounts
            of time. perf:-programs are programs run as performance tests and
            should provide performance measurement information as output.
            all three targets are supposed to pass without errors for make
            distcheck.
    
            * tests/audio/: moved here from slowtests/audio/.
            * tests/filecheck/: moved here from slowtests/filecheck/.
    
            * tests/audio/Makefile.am: slight fixups.
    
            * tests/.:
            * tests/Makefile.am: moved slowtests/ contents here.
            added perftest and testresampler to perf: rule.
    
            * Makefile.decl: added recursive perf: rule. silenced check-local:.
    
    svn path=/trunk/; revision=3984

---
+++ tests/audio/bse2wav.scm
@@ -0,0 +1,70 @@
+;; -*- scheme -*-
+;; Copyright (C) 2006 Tim Janik
+;;
+;; This software is provided "as is"; redistribution and modification
+;; is permitted, provided that the following disclaimer is retained.
+;;
+;; This software is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+;; In no event shall the authors or contributors be liable for any
+;; direct, indirect, incidental, special, exemplary, or consequential
+;; damages (including, but not limited to, procurement of substitute
+;; goods or services; loss of use, data, or profits; or business
+;; interruption) however caused and on any theory of liability, whether
+;; in contract, strict liability, or tort (including negligence or
+;; otherwise) arising in any way out of the use of this software, even
+;; if advised of the possibility of such damage.
+
+;; load all stock BSE plugins
+(bse-server-register-blocking bse-server-register-core-plugins #f)
+;; no script registration
+;;(bse-server-register-blocking bse-server-register-scripts #f)
+;; no LADSPA plugin registration
+;;(bse-server-register-blocking bse-server-register-ladspa-plugins #f)
+
+(define (bse-2-wav bse-file wav-file)
+  (let* ((blimp "*")
+	 ;; create a new project
+	 (project (bse-server-use-new-project bse-server bse-file))
+	 ;; load file contents into project
+	 (error (bse-project-restore-from-file project bse-file)))
+    ;; check errors
+    (if (bse-error-test error)
+	(begin
+	  (display (string-append bse-file ": "
+				  "failed to load project: "
+				  (bse-error-blurb error)
+				  "\n")
+		   (current-error-port))
+	  (exit 1)))
+    ;; record to WAV
+    (display (string-append "Recording to WAV file: " wav-file "\n"))
+    (bse-item-set bse-server "wave-file" wav-file)
+    ;; play project and indicate progress
+    (display (string-append "Playing " bse-file ": -"))
+    (bse-project-play project)
+    (while (bse-project-is-playing project)
+	   (usleep 250000)
+	   (display #\backspace)
+	   (display blimp)
+	   (if (string=? blimp "*")
+	       (set! blimp "o")
+	       (set! blimp "*")))
+    ;; cleanup
+    (bse-project-stop project)
+    (display #\backspace)
+    (display "done.\n")
+    ;; release the newly created project
+    (bse-item-unuse project)))
+
+;; check command line and exec
+(let
+    ((args (cdr (command-line))))
+  (if (not (= 2 (length args)))
+      (begin
+	(display
+	 "Usage: bsescm -s bse2wav.scm <bse-file> <wav-file>\n"
+	 (current-error-port))
+	(exit 1))
+      (apply bse-2-wav args)))
