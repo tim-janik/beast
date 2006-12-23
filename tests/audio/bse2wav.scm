e377ea4c359079ec33f564552a26052557e61ee1
Tim Janik <timj@gtk.org>
2006-12-23 21:02:50 +0000

    added sum-diff-test: test rule to test BseSummation audio-diff output
    Sat Dec 23 20:56:33 2006 Tim Janik <timj@gtk.org>
    
            * tests/audio/sum-diff-test.ref:
            * tests/audio/sum-diff-test.bse:
            * tests/audio/Makefile.am: added sum-diff-test: test rule to test
            BseSummation audio-diff output channel.
    
            * tests/audio/bse2wav.scm: added option parsing, support --seconds <>
            option to contrain the time intervall used for recording.
    
            * tests/audio/Makefile.am: lowered threshold for organsong-test and
            velocity-test which seem to occasionally fail due to timing variances.
    
            * Makefile.am: depend on automake 1.9.
    
    svn path=/trunk/; revision=4205

--- tests/audio/bse2wav.scm
+++ tests/audio/bse2wav.scm
@@ -16,6 +16,8 @@
 ;; otherwise) arising in any way out of the use of this software, even
 ;; if advised of the possibility of such damage.
 
+(use-modules (ice-9 getopt-long))
+
 ;; load all stock BSE plugins
 (bse-server-register-blocking bse-server-register-core-plugins #f)
 ;; no script registration
@@ -23,7 +25,8 @@
 ;; no LADSPA plugin registration
 ;;(bse-server-register-blocking bse-server-register-ladspa-plugins #f)
 
-(define (bse-2-wav bse-file wav-file)
+;; playback + recording procedure
+(define (bse-2-wav bse-file wav-file seconds)
   (let* ((blimp "*")
 	 ;; create a new project
 	 (project (bse-server-use-new-project bse-server bse-file))
@@ -40,7 +43,7 @@
 	  (exit 1)))
     ;; record to WAV
     (display (string-append "Recording to WAV file: " wav-file "\n"))
-    (bse-item-set bse-server "wave-file" wav-file)
+    (bse-server-start-recording bse-server wav-file seconds)
     ;; play project and indicate progress
     (display (string-append "Playing " bse-file ": -"))
     (bse-project-play project)
@@ -58,13 +61,27 @@
     ;; release the newly created project
     (bse-item-unuse project)))
 
-;; check command line and exec
-(let
-    ((args (cdr (command-line))))
+;; Program usage
+(define (exit-usage exit-code)
+  (display "Usage: bse2wav [options...] <bse-file> <wav-file>\n")
+  (display "  --help, -h           Show this usage information\n")
+  (display "  --seconds <seconds>  Number of seconds to record\n")
+  (exit exit-code))
+
+;; Command line synopsis
+(define
+  command-synopsis
+  '(
+    (help    (single-char #\h) (value #f))
+    (seconds (value #t))
+    ))
+
+;; Parse command line and exec
+(let* ((options (getopt-long (command-line) command-synopsis))
+       (max-seconds (option-ref options 'seconds 0))
+       (args (option-ref options '() '())))
+  (if (option-ref options 'help #f)
+      (exit-usage 0))
   (if (not (= 2 (length args)))
-      (begin
-	(display
-	 "Usage: bsescm -s bse2wav.scm <bse-file> <wav-file>\n"
-	 (current-error-port))
-	(exit 1))
-      (apply bse-2-wav args)))
+      (exit-usage 1))
+  (apply bse-2-wav (append args (list (string->number max-seconds)))))
