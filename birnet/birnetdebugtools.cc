5809f88841511a675cabf06f63b168e937884968
Tim Janik <timj@gtk.org>
2007-02-02 00:24:10 +0000

    don't write trailing 0s in debug stream, slightly reduced stack
    Fri Feb 2 00:54:32 2007 Tim Janik <timj@gtk.org>
    
            * birnetdebugtools.cc: don't write trailing 0s in debug stream,
            slightly reduced stack requirements, 4000 chars are hopefully
            enough for async debugging messages.
    
    svn path=/trunk/; revision=4266

--- birnet/birnetdebugtools.cc
+++ birnet/birnetdebugtools.cc
@@ -54,15 +54,19 @@ struct DebugChannelFileAsync : public virtual DebugChannel, public virtual Threa
   printf_valist (const char *format,
                  va_list     args)
   {
-    const int bsz = 8192;
+    const int bsz = 4000;
     char buffer[bsz + 2];
     int l = vsnprintf (buffer, bsz, format, args);
     if (l > 0)
       {
         l = MIN (l, bsz);
         if (buffer[l - 1] != '\n')
-          buffer[l++] = '\n';
-        buffer[l++] = 0;
+          {
+            buffer[l++] = '\n';
+            buffer[l] = 0;
+          }
+        if (false) // skip trailing 0 in ring buffer
+          buffer[l++] = 0;
         uint n = aring.write (l, buffer, false);
         if (!n)
           Atomic::uint_swap_add (&skip_count, 1);
