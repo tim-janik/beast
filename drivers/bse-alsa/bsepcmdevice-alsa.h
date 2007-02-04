4f521a4acb607407b8f0ffc8af8eba5e4da3ac89
Tim Janik <timj@gtk.org>
2007-02-04 17:11:53 +0000

    updated all licenses to GNU Lesser General Public License version 2.1.
    Sun Feb  4 18:07:22 2007  Tim Janik  <timj@gtk.org>
    
            * updated all licenses to GNU Lesser General Public License version 2.1.
    
    svn path=/trunk/; revision=4284

--- drivers/bse-alsa/bsepcmdevice-alsa.h
+++ drivers/bse-alsa/bsepcmdevice-alsa.h
@@ -1,20 +1,18 @@
 /* BSE - Bedevilled Sound Engine
  * Copyright (C) 2004 Tim Janik
  *
- * This program is free software; you can redistribute it and/or
+ * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General
- * Public License along with this program; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
- * Boston, MA 02111-1307, USA.
+ * A copy of the GNU Lesser General Public License should ship along
+ * with this library; if not, see http://www.gnu.org/copyleft/.
  */
 #ifndef __BSE_PCM_DEVICE_ALSA_H__
 #define __BSE_PCM_DEVICE_ALSA_H__
