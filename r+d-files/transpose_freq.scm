6cc0510e129c627a06be4c5daf7973e2f85b8724
Stefan Westerfeld <stefan@space.twc.de>
2007-02-11 16:45:13 +0000

    Added this little utility script which is useful for computing 12-TET
    Sun Feb 11 17:42:58 2007  Stefan Westerfeld  <stefan@space.twc.de>
    
    	* r+d-files/transpose_freq.scm: Added this little utility script which is
    	useful for computing 12-TET transposed frequencies. I used it while
    	turning a .bse file with hardcoded oscillator frequencies into one with
    	relative frequencies to the instrument base frequency.
    
    svn path=/trunk/; revision=4310

---
+++ r+d-files/transpose_freq.scm
@@ -0,0 +1,32 @@
+;; 
+;; Copyright (C) 2007 Stefan Westerfeld, stefan@space.twc.de
+;; 
+;; This software is provided "as is"; redistribution and modification
+;; is permitted, provided that the following disclaimer is retained.
+;;
+;; This software is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+;; In no event shall the authors or contributors be liable for any
+;; direct, indirect, incidental, special, exemplary, or consequential
+;; damages (including, but not limited to, procurement of substitute
+;; goods or services; loss of use, data, or profits; or business
+;; interruption) however caused and on any theory of liability, whether
+;; in contract, strict liability, or tort (including negligence or
+;; otherwise) arising in any way out of the use of this software, even
+;; if advised of the possibility of such damage.
+
+;;
+;; usage: bsescm -s transpose_freq.scm <freq> <semitones> <fine-tune>
+;;
+;; mini script to calculate the effect of 12-TET transposing a frequency
+;;
+
+(define (transpose-freq freq semitones fine-tune)
+  (let*
+    ((total-transpose-cent (+ (* 100 (string->number semitones)) (string->number fine-tune)))
+	 (new-freq (* (expt 2 (/ total-transpose-cent 1200)) (string->number freq))))
+	(display new-freq)
+	(newline)))
+
+(apply transpose-freq (cdr (command-line)))
