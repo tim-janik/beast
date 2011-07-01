de900867800afdbd43581c561a1dcdc4d60416e6
Stefan Westerfeld <stefan@space.twc.de>
2011-07-01 17:27:05 +0200

    BSE: compile bseconstant as C++ source

--- bse/bseconstant.cc
+++ bse/bseconstant.cc
@@ -36,7 +36,7 @@ enum
 
 /* --- prototypes --- */
 static void	 bse_constant_init		(BseConstant	  *constant);
-static void	 bse_constant_class_init	(BseConstantClass *class);
+static void	 bse_constant_class_init	(BseConstantClass *klass);
 static void	 bse_constant_set_property	(GObject	  *object,
 						 guint             param_id,
 						 const GValue     *value,
@@ -86,14 +86,14 @@ BSE_BUILTIN_TYPE (BseConstant)
 }
 
 static void
-bse_constant_class_init (BseConstantClass *class)
+bse_constant_class_init (BseConstantClass *klass)
 {
-  GObjectClass *gobject_class = G_OBJECT_CLASS (class);
-  BseObjectClass *object_class = BSE_OBJECT_CLASS (class);
-  BseSourceClass *source_class = BSE_SOURCE_CLASS (class);
+  GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+  BseObjectClass *object_class = BSE_OBJECT_CLASS (klass);
+  BseSourceClass *source_class = BSE_SOURCE_CLASS (klass);
   guint ochannel, i;
   
-  parent_class = g_type_class_peek_parent (class);
+  parent_class = g_type_class_peek_parent (klass);
   
   gobject_class->set_property = bse_constant_set_property;
   gobject_class->get_property = bse_constant_get_property;
@@ -270,7 +270,7 @@ static void
 constant_process (BseModule *module,
 		  guint      n_values)
 {
-  ConstantModule *cmod = module->user_data;
+  ConstantModule *cmod = (ConstantModule*) module->user_data;
   guint i;
   
   for (i = 0; i < BSE_CONSTANT_N_OUTPUTS; i++)
