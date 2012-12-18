f04f1e3dc2efe2931e10f288ee7723fbdf2b3d69
Tim Janik <timj@gnu.org>
2012-12-18 03:01:47 +0100

    PLUGINS: compile DavXtalStrings as C++ resident type

--- plugins/davxtalstrings.h
+++ plugins/davxtalstrings.h
@@ -25,7 +25,7 @@ extern "C" {
 #endif /* __cplusplus */
 
 /* --- object type macros --- */
-#define DAV_TYPE_XTAL_STRINGS		   (BSE_EXPORT_TYPE_ID (DavXtalStrings))
+#define DAV_TYPE_XTAL_STRINGS		   (dav_xtal_strings_get_type())
 #define DAV_XTAL_STRINGS(object)	   (G_TYPE_CHECK_INSTANCE_CAST ((object), DAV_TYPE_XTAL_STRINGS, DavXtalStrings))
 #define DAV_XTAL_STRINGS_CLASS(class)	   (G_TYPE_CHECK_CLASS_CAST ((class), DAV_TYPE_XTAL_STRINGS, DavXtalStringsClass))
 #define DAV_IS_XTAL_STRINGS(object)	   (G_TYPE_CHECK_INSTANCE_TYPE ((object), DAV_TYPE_XTAL_STRINGS))
