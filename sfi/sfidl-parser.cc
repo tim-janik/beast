e067d5d755319f898ade6f62e66d0da4cb6382e3
Tim Janik <timj@gnu.org>
2012-12-23 18:05:19 +0100

    SFI: use one liner license

--- sfi/sfidl-parser.cc
+++ sfi/sfidl-parser.cc
@@ -1,19 +1,4 @@
-/* SFI - Synthesis Fusion Kit Interface
- * Copyright (C) 2002-2007 Stefan Westerfeld
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * A copy of the GNU Lesser General Public License should ship along
- * with this library; if not, see http://www.gnu.org/copyleft/.
- */
+// Licensed GNU LGPL v2.1 or later: http://www.gnu.org/licenses/lgpl.html
 #include <sfi/glib-extra.hh>
 #include <fcntl.h>
 #include <stdio.h>
--- sfi/toyprof.c
+++ sfi/toyprof.c
@@ -1,21 +1,5 @@
-/* TOYPROF - Poor man's profiling toy
- * Copyright (C) 2001-2002 Tim Janik
- *
- * This software is provided "as is"; redistribution and modification
- * is permitted, provided that the following disclaimer is retained.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * In no event shall the authors or contributors be liable for any
- * direct, indirect, incidental, special, exemplary, or consequential
- * damages (including, but not limited to, procurement of substitute
- * goods or services; loss of use, data, or profits; or business
- * interruption) however caused and on any theory of liability, whether
- * in contract, strict liability, or tort (including negligence or
- * otherwise) arising in any way out of the use of this software, even
- * if advised of the possibility of such damage.
- */
+// CC0 Public Domain: http://creativecommons.org/publicdomain/zero/1.0/
+// TOYPROF - Poor man's profiling toy
 #include "toyprof.h"
 
 #ifdef	TOYPROF_GNUC_NO_INSTRUMENT	/* only have this if toyprof is enabled */
