42b173787a7be966a56ad4a32bb55bb4bf1584c2
Stefan Westerfeld <stefan@space.twc.de>
2013-08-06 17:33:27 +0200

    BSE: add bse-internals.hh

---
+++ bse/bse-internals.hh
@@ -0,0 +1,15 @@
+// Licensed GNU LGPL v2.1 or later: http://www.gnu.org/licenses/lgpl.html
+#ifndef __BSE_INTERNALS_HH__
+#define __BSE_INTERNALS_HH__
+
+#include <bse/bse.hh>
+#include <bse/bseenums.hh>
+#include <bse/bsemathsignal.hh>
+#include <bse/bseblockutils.hh>
+#include <bse/gslfft.hh>
+#include <bse/gsldatautils.hh>
+#include <bse/bseloader.hh>
+#include <bse/bsemain.hh>
+#include <bse/bsecxxplugin.hh>
+
+#endif /* __BSE_INTERNALS_HH__ */
