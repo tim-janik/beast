d551e2af35b50e28eda786d5f82d4f95cec45d69
Tim Janik <timj@gnu.org>
2015-07-02 13:47:03 +0200

    BSE: allow as<shared_ptr<>>() from Bse::ItemIface

--- bse/bseserverinsert.hh
+++ bse/bseserverinsert.hh
@@ -5,13 +5,27 @@ struct DUMMY { // dummy class for auto indentation
 
 
 class_scope:Object:
-  template<class BseObjectPtr>
+  // as<BseObjectPtr>()
+  template<class BseObjectPtr, typename ::std::enable_if<std::is_pointer<BseObjectPtr>::value, bool>::type = true>
   BseObjectPtr           as ()
   {
     static_assert (std::is_pointer<BseObjectPtr>::value, "'BseObject*' required");
     typedef typename std::remove_pointer<BseObjectPtr>::type BseObjectT;
     static_assert (std::is_base_of<GObject, BseObjectT>::value, "'BseObject*' required");
     return this ? (BseObjectPtr) this->as_bse_object() : NULL;
+  }
+  // DERIVES_shared_ptr (uses void_t to prevent errors for T without shared_ptr's typedefs)
+  template<class T, typename = void> struct DERIVES_shared_ptr : std::false_type {};
+  template<class T> struct DERIVES_shared_ptr<T, Rapicorn::void_t< typename T::element_type > > :
+  std::is_base_of< std::shared_ptr<typename T::element_type>, T > {};
+  // as<shared_ptr<T>>()
+  template<class ObjectImplP, typename ::std::enable_if<DERIVES_shared_ptr<ObjectImplP>::value, bool>::type = true>
+  ObjectImplP            as ()
+  {
+    typedef typename ObjectImplP::element_type ObjectImplT;
+    static_assert (std::is_base_of<Rapicorn::Aida::ImplicitBase, ObjectImplT>::value, "");
+    ObjectImplT *impl = this ? dynamic_cast<ObjectImplT*> (this) : NULL;
+    return impl ? Rapicorn::shared_ptr_cast<ObjectImplT> (impl) : NULL;
   }
 protected:
   virtual BseObject* as_bse_object() = 0;
