7d1360b41f694ea208a7163a95bdcbf8dc05386f
Tim Janik <timj@gnu.org>
2015-09-17 18:11:51 +0200

    BSE: move bseinfo to "bsetool dump-info"

--- bse/bsetool.cc
+++ bse/bsetool.cc
@@ -1,10 +1,12 @@
 // Licensed GNU LGPL v2.1 or later: http://www.gnu.org/licenses/lgpl.html
 #include <bse/bsemain.hh>
 #include <bse/bseserver.hh>
+#include <bse/bsemathsignal.hh>
 #include <sys/resource.h>
 #include <unordered_map>
 #include <unistd.h>
 #include <stdio.h>
+
 
 using namespace Bse;
 
@@ -136,6 +138,80 @@ crawl (const ArgParser &ap)
   return "";
 }
 
+// == dump-info ==
+static ArgDescription dump_info_options[] = {
+  { "", "", "", "" }, // dummy, no options currently
+};
+
+#define	PREC_SHIFT      16
+#define	FLF	        "26.20"
+
+static String
+dump_info (const ArgParser &ap)
+{
+  auto print_int = [] (const char *name, int integer) {
+    g_print ("%s =%-4d\n", name, integer);
+  };
+  auto print_note = [] (const char *note_name, int note) {
+    char *string = bse_note_to_string (note);
+    g_print ("%s =%-4d \tfactor=%" FLF "f [%-5s] (freq=%" FLF "f)\n",
+             note_name, note,
+             bse_transpose_factor (BSE_MUSICAL_TUNING_12_TET, note - BSE_KAMMER_NOTE),
+             string, bse_note_to_freq (BSE_MUSICAL_TUNING_12_TET, note));
+    g_free (string);
+  };
+  auto print_fine_tune = [] (const char *tune_name, int tune) {
+    g_print ("%s =%-4d \tfactor=%" FLF "f\n",
+             tune_name, tune,
+             bse_cent_tune_fast (tune));
+  };
+  printout ("Rate relevant limits:\n");
+  print_int       ("BSE_MIN_OCTAVE   ", BSE_MIN_OCTAVE);
+  print_int       ("BSE_MAX_OCTAVE   ", BSE_MAX_OCTAVE);
+  print_note      ("BSE_MIN_NOTE     ", BSE_MIN_NOTE);
+  print_note      ("BSE_KAMMER_NOTE  ", BSE_KAMMER_NOTE);
+  print_note      ("BSE_MAX_NOTE     ", BSE_MAX_NOTE);
+  print_note      ("BSE_KAMMER_NOTE-1", BSE_KAMMER_NOTE - 1);
+  print_fine_tune ("BSE_MIN_FINE_TUNE", BSE_MIN_FINE_TUNE);
+  print_fine_tune ("bse-mid-fine-tune", (BSE_MIN_FINE_TUNE + BSE_MAX_FINE_TUNE) / 2);
+  print_fine_tune ("BSE_MAX_FINE_TUNE", BSE_MAX_FINE_TUNE);
+  print_note      ("BSE_KAMMER_NOTE+1", BSE_KAMMER_NOTE + 1);
+
+  int j, k;
+  if (0)
+    for (j = BSE_MIN_NOTE; j <= BSE_MAX_NOTE; j += 3)
+      print_note (":", j);
+  if (0)
+    for (j = BSE_MIN_FINE_TUNE; j <= BSE_MAX_FINE_TUNE; j += 10)
+      print_fine_tune (":", j);
+
+  if (0)
+    for (j = BSE_MIN_NOTE; j <= BSE_MAX_NOTE; j += 3)
+      for (k = BSE_MIN_FINE_TUNE / 2; k <= BSE_MAX_FINE_TUNE / 2; k += 10)
+	{
+	  double f, freq = bse_note_to_tuned_freq (BSE_MUSICAL_TUNING_12_TET, j, k);
+	  int note, fine_tune;
+	  printout ("compose  : note=%4d fine_tune=%4d freq=%" FLF "f\n", j, k, freq);
+	  f = freq;
+	  note = bse_note_from_freq (BSE_MUSICAL_TUNING_12_TET, freq);
+	  fine_tune = bse_note_fine_tune_from_note_freq (BSE_MUSICAL_TUNING_12_TET, note, freq);
+	  freq = bse_note_to_tuned_freq (BSE_MUSICAL_TUNING_12_TET, note, fine_tune);
+	  g_print ("decompose: note=%4d fine_tune=%4d freq=%" FLF "f   (diff=%g)\n", note, fine_tune, freq, freq - f);
+	}
+  if (0)
+    for (j = BSE_MIN_NOTE; j <= BSE_MAX_NOTE; j += 1)
+      {
+	int octave = SFI_NOTE_OCTAVE (j);
+	int semitone = SFI_NOTE_SEMITONE (j);
+	int note = BSE_NOTE_GENERIC (octave, semitone);
+	char *name = bse_note_to_string (j);
+
+	g_print ("note[%3d]: name=%-8s octave=%3d semitone=%3d note=%3d match=%u\n",
+		 j, name, octave, semitone, note, j == note);
+	g_free (name);
+      }
+  return "";
+}
 
 // == render2wav ==
 static ArgDescription render2wav_options[] = {
@@ -258,6 +334,23 @@ main (int argc_int, char *argv[])
       if (!error.empty())
         {
           printerr ("check-load: %s\n", error);
+          return 127;
+        }
+      return 0; // success
+    }
+  else if (option_argc < argc && argv[option_argc] == String ("dump-info"))
+    {
+      ArgParser ap (dump_info_options);
+      String error = ap.parse_args (argc - option_argc - 1, argv + option_argc + 1);
+      if (!error.empty())
+        {
+          printerr ("%s: dump-info: %s\n", argv[0], error);
+          return 127;
+        }
+      error = dump_info (ap);
+      if (!error.empty())
+        {
+          printerr ("dump-info: %s\n", error);
           return 127;
         }
       return 0; // success
