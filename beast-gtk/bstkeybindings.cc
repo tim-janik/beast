29ce221179169cf2d8b43e6e5c9a033a3e08c7d5
Tim Janik <timj@gnu.org>
2015-09-25 00:07:32 +0200

    BEAST: use __func__ to print error messages

--- beast-gtk/bstkeybindings.cc
+++ beast-gtk/bstkeybindings.cc
@@ -255,7 +255,7 @@ key_bindings_check_action (gpointer data,
     case ACTION_LOAD:
       return TRUE;
     default:
-      g_warning ("%s: unknown action: %lu", G_STRFUNC, action);
+      g_warning ("%s: unknown action: %lu", __func__, action);
       return FALSE;
     }
 }
