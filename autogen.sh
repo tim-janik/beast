cfef0465a66860587f268a2bc9cadf98d80161cb
Tim Janik <timj@gnu.org>
2015-10-09 00:14:33 +0200

    BUILD: autogen.sh: simplify to the bare minimum required
    Signed-off-by: Tim Janik <timj@gnu.org>

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-echo "$0: Cleaning configure caches..."
-rm -rf autom4te.cache/
-rm -f  config.cache
+set -xe # be verbose and abort on errors
 
-echo "$0: autoreconf -vfsi -Wno-portability"
-autoreconf -vfsi -Wno-portability || exit $?
+rm -rf autom4te.cache/ config.cache
 
-echo "$0: ./configure $*"
-./configure "$@" || exit $?
+autoreconf -vfsi
+
+./configure "$@"
