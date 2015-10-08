4aa3e55067d4440f00c77f7620ca89d28b68368c
Tim Janik <timj@gnu.org>
2015-10-08 23:18:25 +0200

    BUILD: autogen.sh: cleanups, leave ChangeLog alone
    Signed-off-by: Tim Janik <timj@gnu.org>

--- autogen.sh
+++ autogen.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-CONFIGURE_OPTIONS=
-
 echo "$0: Cleaning configure caches..."
 rm -rf autom4te.cache/
 rm -f  config.cache
 
-# automake *requires* ChangeLog
-echo "$0: Enforce ChangeLog presence"
-test -e ChangeLog || TZ=GMT0 touch ChangeLog -t 190112132145.52
-
 echo "$0: autoreconf -vfsi -Wno-portability"
 autoreconf -vfsi -Wno-portability || exit $?
 
-echo "$0: ./configure $CONFIGURE_OPTIONS $@"
-./configure $CONFIGURE_OPTIONS "$@" || exit $?
+echo "$0: ./configure $*"
+./configure "$@" || exit $?
