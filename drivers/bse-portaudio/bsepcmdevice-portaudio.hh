d3c983f3b37b80ed9bb141d3dfb57c5292559cb0
Tim Janik <timj@gnu.org>
2016-10-26 22:30:58 +0200

    DRIVERS: remove 'extern "C"' from all API
    Signed-off-by: Tim Janik <timj@gnu.org>

--- drivers/bse-portaudio/bsepcmdevice-portaudio.hh
+++ drivers/bse-portaudio/bsepcmdevice-portaudio.hh
@@ -3,8 +3,6 @@
 #define __BSE_PCM_DEVICE_PORTAUDIO_H__
 #include <bse/bsepcmdevice.hh>
 #include <bse/bseplugin.hh>
-G_BEGIN_DECLS
-
 #define BSE_TYPE_PCM_DEVICE_PORT_AUDIO              (BSE_EXPORT_TYPE_ID (BsePcmDevicePortAudio))
 #define BSE_PCM_DEVICE_PORT_AUDIO(object)           (G_TYPE_CHECK_INSTANCE_CAST ((object), BSE_TYPE_PCM_DEVICE_PORT_AUDIO, BsePcmDevicePortAudio))
 #define BSE_PCM_DEVICE_PORT_AUDIO_CLASS(class)      (G_TYPE_CHECK_CLASS_CAST ((class), BSE_TYPE_PCM_DEVICE_PORT_AUDIO, BsePcmDevicePortAudioClass))
@@ -17,7 +15,5 @@ struct BsePcmDevicePortAudio : BsePcmDevice
 
 struct BsePcmDevicePortAudioClass : BsePcmDeviceClass
 {};
-
-G_END_DECLS
 
 #endif /* __BSE_PCM_DEVICE_PORTAUDIO_H__ */
