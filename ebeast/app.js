598bf2e4b450c41c8db28b3f814f59a5eb6f2121
Tim Janik <timj@gnu.org>
2017-03-08 15:33:09 +0100

    EBEAST: add StatusBar and a ToolBar with PlayControls
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ebeast/app.js
+++ ebeast/app.js
@@ -1,47 +1,102 @@
 'use strict';
 
-// load external libraries
+// == Essential Modules ==
+// window.$ = preloaded jQuery
 window.Electron = require ('electron').remote;
 window.Bse = require ('./v8bse/v8bse.node');
-// window.$ = preloaded jQuery
 window.Mithril = require ('mithril');
 
-// Assign global App object with defaults
-window.App = {
-  show_about: false,
+// == App ==
+const AppMethods = {
   __proto__ : Electron.app
 };
+const App = {	// global App object with defaults
+  show_about: false,
+  __proto__ : AppMethods
+};
 Object.preventExtensions (App);
+window.App = App;
 
-// load application parts that depend on App
+// == Load Parts ==
+// load and setup application menus
+const Menus = require ('./menus.js');
+Electron.Menu.setApplicationMenu (Menus.build_menubar());
+// load parts that depend on App
 const Widgets = require ('./assets/widgets.js');
 const Dialogs = require ('./assets/dialogs.js');
-const Menus = require ('./menus.js');
 const m = Mithril;
 
-// assign BrowserWindow's menubar
-Electron.Menu.setApplicationMenu (Menus.build_menubar());
-
-// Application start onready
-module.exports.start_app = function () {
-  Dialogs.toggle_about = function() {
-    const m = Mithril;
+// == App.status() ==
+// update status bar with the given text
+AppMethods.status = function (message) {
+  $('#statusfield').text (message);
+};
+const StatusBar = {
+  view (_vnode) {
     return (
-      m ('button',
-	 { onclick: function () { App.show_about = !App.show_about; } },
-	 'Toggle About')
+      m ('#statusarea.StatusArea.hbox.w100', [
+	m ('#statusbar.StatusBar.maxbox', [
+	  'Status: ',
+	  m ('#statusfield.StatusField'),
+	]),
+      ])
     );
-  };
+  },
+};
 
+// == PlayControls ==
+const PlayControls = {
+  view (vnode) {
+    const B = Widgets.IconButton;
+    return (
+      m ('#playcontrols.PlayControls', vnode.attrs, [
+	m ('span', { style: 'margin: 0 2em' }, [
+	  m (B, { icon: 'fa-cog fa-spin0' }),
+	]),
+	m ('span.ButtonBox', [
+	  m (B, { icon: 'fa-fast-backward', hotkey: 'L',     onclick: () => { App.status ('Last...'); } }),
+	  m (B, { icon: 'fa-backward',      hotkey: 'B',     onclick: () => { App.status ('Backwards...'); } }),
+	  m (B, { icon: 'fa-stop',          hotkey: 'Space', onclick: () => { App.status ('Stop...'); } }),
+	  m (B, { icon: 'fa-play',          hotkey: 'P',     onclick: () => { App.status ('Play...'); } }), // toggle fa-play / fa-pause
+	  m (B, { icon: 'fa-circle',        hotkey: 'R',     onclick: () => { App.status ('...Record...'); } }),
+	  m (B, { icon: 'fa-forward',       hotkey: 'F',     onclick: () => { App.status ('Forwards...'); } }),
+	  m (B, { icon: 'fa-fast-forward',  hotkey: 'N',     onclick: () => { App.status ('Next...'); } }),
+	]),
+	m ('span.ButtonBox', { style: 'margin: 0 2em' }, [
+	  m (B, { icon: 'fa-music' }),
+	]),
+	m ('span', { style: 'margin: 0 2em' }, [
+	  m (B, { icon: 'fa-info-circle' }),
+	]),
+      ])
+    );
+  },
+};
+
+// == Toolbar ==
+const Toolbar = {
+  view (vnode) {
+    return (
+      m ('#toolbar.ToolBar', vnode.attrs, vnode.children, [
+	m (PlayControls),
+      ])
+    );
+  },
+};
+
+// == export start_app() ==
+module.exports.start_app = function () { // application setup after onready
   const MithrilApp = {
     view: function () {
       const m = Mithril;
       const app_body = [
-	Dialogs.toggle_about(),
+	m (Toolbar),
+	m ('.maxbox'),
+	m (StatusBar),
 	Dialogs.about_dialog(),
       ];
       return (
-	m ('div#mithril.Mithril', {
+	m ('div#mithril.Mithril.vbox', {
 	  style: 'position: absolute; top: 0; left: 0; right: 0; bottom: 0;',
 	}, app_body)
       );
