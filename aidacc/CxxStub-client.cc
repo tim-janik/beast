409cdf99af6027c5ecf53a9d5f89b44b90f83bcc
Tim Janik <timj@gnu.org>
2017-09-12 02:59:27 +0200

    AIDACC: remove Rapicorn namespacing from C++ glue code
    Signed-off-by: Tim Janik <timj@gnu.org>

--- aidacc/CxxStub-client.cc
+++ aidacc/CxxStub-client.cc
@@ -2,16 +2,15 @@
 #define __AIDA_CXXSTUB_CLIENT_CC__
 
 namespace { // Anon
-using Rapicorn::Aida::uint64;
 
 namespace __AIDA_Local__ {
-using namespace Rapicorn::Aida;
+using namespace Aida;
 
 // helper
 static inline ProtoMsg*
 new_emit_result (const ProtoMsg *fb, uint64 h, uint64 l, uint32 n)
 {
-  return ProtoMsg::renew_into_result (const_cast<ProtoMsg*> (fb), Rapicorn::Aida::MSGID_EMIT_RESULT, h, l, n);
+  return ProtoMsg::renew_into_result (const_cast<ProtoMsg*> (fb), Aida::MSGID_EMIT_RESULT, h, l, n);
 }
 
 } } // Anon::__AIDA_Local__
--- aidacc/CxxStub-server.cc
+++ aidacc/CxxStub-server.cc
@@ -7,30 +7,29 @@
 #endif
 
 namespace { // Anon
-using Rapicorn::Aida::uint64;
 
 namespace __AIDA_Local__ {
-using namespace Rapicorn::Aida;
+using namespace Aida;
 
 // types
 typedef ServerConnection::EmitResultHandler EmitResultHandler;
 typedef ServerConnection::MethodRegistry    MethodRegistry;
 typedef ServerConnection::MethodEntry       MethodEntry;
 
-static_assert (std::is_base_of<Rapicorn::Aida::ImplicitBase, $AIDA_iface_base$>::value,
-               "IDL interface base '$AIDA_iface_base$' must derive 'Rapicorn::Aida::ImplicitBase'");
+static_assert (std::is_base_of<Aida::ImplicitBase, $AIDA_iface_base$>::value,
+               "IDL interface base '$AIDA_iface_base$' must derive 'Aida::ImplicitBase'");
 
 // objects
 static inline ProtoMsg*
 new_call_result (ProtoReader &fbr, uint64 h, uint64 l, uint32 n = 1)
 {
-  return ProtoMsg::renew_into_result (fbr, Rapicorn::Aida::MSGID_CALL_RESULT, h, l, n);
+  return ProtoMsg::renew_into_result (fbr, Aida::MSGID_CALL_RESULT, h, l, n);
 }
 
 static inline ProtoMsg*
 new_connect_result (ProtoReader &fbr, uint64 h, uint64 l, uint32 n = 1)
 {
-  return ProtoMsg::renew_into_result (fbr, Rapicorn::Aida::MSGID_CONNECT_RESULT, h, l, n);
+  return ProtoMsg::renew_into_result (fbr, Aida::MSGID_CONNECT_RESULT, h, l, n);
 }
 
 // slot
