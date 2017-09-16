84284e83e4e7748959ad22684b749d8710d6816f
Tim Janik <timj@gnu.org>
2017-09-17 01:52:11 +0200

    TESTS: build and run suite1
    Signed-off-by: Tim Janik <timj@gnu.org>

---
+++ tests/suite1-main.cc
@@ -0,0 +1,11 @@
+// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
+#include <sfi/testing.hh>
+
+int
+main (int   argc,
+      char *argv[])
+{
+  Bse::Test::init (&argc, argv);
+
+  return Bse::Test::run();
+}
