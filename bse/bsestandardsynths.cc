477bf38a1f40bdb372ff562fd718b0252667a73f
Tim Janik <timj@gnu.org>
2017-09-18 16:46:59 +0200

    BSE: use Bse::Blob instead of Res and Blob from Rapicorn
    Signed-off-by: Tim Janik <timj@gnu.org>

--- bse/bsestandardsynths.cc
+++ bse/bsestandardsynths.cc
@@ -1,10 +1,8 @@
 // Licensed GNU LGPL v2.1 or later: http://www.gnu.org/licenses/lgpl.html
 #include "bsestandardsynths.hh"
-
 #include "bsesnet.hh"
 #include "bsestandardosc.hh"
 #include <string.h>
-#include "bse/res/bse-resources.cc" // compile standard synth resources
 
 /* --- functions --- */
 gchar*
@@ -13,8 +11,8 @@ bse_standard_synth_inflate (const gchar *synth_name,
 {
   assert_return (synth_name != NULL, NULL);
 
-  const Bse::String synth_res = Bse::String ("@res ") + synth_name + ".bse";
-  Bse::Blob blob = Bse::Res (synth_res);
+  const Bse::String synth_res = Bse::String ("res:bse/") + synth_name + ".bse";
+  Bse::Blob blob = Bse::Blob (synth_res);
   if (blob.size())
     {
       gchar *result = (gchar*) g_malloc (blob.size() + 1);
