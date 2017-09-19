b4430e00bda6637ef62c4c004e107e0533234a34
Tim Janik <timj@gnu.org>
2017-09-19 02:26:42 +0200

    SFI: sfiwrapper.cc: remove useless initialization code
    Signed-off-by: Tim Janik <timj@gnu.org>

--- sfi/sfiwrapper.cc
+++ sfi/sfiwrapper.cc
@@ -12,11 +12,6 @@ sfi_init (int *argcp, char **argv, const Bse::StringVector &args)
   static bool initialized = false;
   if (initialized)
     return;
-  if (args.size() == 1 && args[0] == "rapicorn-test-initialization=1")
-    Bse::Test::init (argcp, argv);
-  else
-    Rapicorn::parse_init_args (argcp, argv);
-
   _sfi_init_values ();
   _sfi_init_params ();
   _sfi_init_time ();
