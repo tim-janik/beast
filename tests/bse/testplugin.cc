8bd8d11298d8108772d58847efbb97486a3637e3
Tim Janik <timj@gnu.org>
2017-10-03 01:18:43 +0200

    TESTS: remove sfidl procedure test
    Signed-off-by: Tim Janik <timj@gnu.org>

--- tests/bse/testplugin.cc
+++ tests/bse/testplugin.cc
@@ -35,27 +35,11 @@ public:
 #endif
 };
 
-SfiInt
-Procedure::test_exception::exec (SfiInt        i,
-                                 TestObject*   o,
-                                 SfiInt        bar,
-                                 FunkynessType ft)
-{
-  printout ("testplugin.cc: test_exception: i=%d obj=%p bar=%d ft=%d (MODERATELY_FUNKY=%d)\n",
-           i, o, bar, ft, (int) MODERATELY_FUNKY);
-  if (ft != MODERATELY_FUNKY)
-    throw std::runtime_error ("need to be moderately funky");
-  if (!o)
-    throw std::runtime_error ("object pointer is NULL");
-  return i + bar;
-}
-
 BSE_CXX_DEFINE_EXPORTS();
 BSE_CXX_REGISTER_ENUM (FunkynessType);
 BSE_CXX_REGISTER_RECORD (TestRecord);
 BSE_CXX_REGISTER_SEQUENCE (TestSequence);
 BSE_CXX_REGISTER_EFFECT (TestObject);
-BSE_CXX_REGISTER_PROCEDURE (test_exception);
 
 } // Test
 
