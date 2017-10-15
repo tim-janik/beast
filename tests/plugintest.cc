4fda2079c3a45c660138d3679fea325bafcc6d7c
Tim Janik <timj@gnu.org>
2017-10-15 22:58:11 +0200

    TESTS: move plugin compilation test here as tests/plugintest.idl
    Signed-off-by: Tim Janik <timj@gnu.org>

--- tests/plugintest.cc
+++ tests/plugintest.cc
@@ -1,6 +1,6 @@
 // Licensed GNU LGPL v2.1 or later: http://www.gnu.org/licenses/lgpl.html
 #include "bse/bsecxxmodule.hh"
-#include "testplugin.genidl.hh"
+#include "tests/plugintest.genidl.hh"
 #include <stdexcept>
 #include <math.h>
 #include <string.h>
