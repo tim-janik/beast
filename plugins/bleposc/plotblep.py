21de0835777d65250fb7f82846365f5b3b639ca7
Stefan Westerfeld <stefan@space.twc.de>
2017-12-09 15:53:00 +0100

    BLEPOSC: support toggle cxx/py source in plot
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- plugins/bleposc/plotblep.py
+++ plugins/bleposc/plotblep.py
@@ -108,10 +108,12 @@ class BlepWidget(QtWidgets.QWidget):
     super(BlepWidget, self).__init__()
     self.setMinimumSize (100, 100)
 
-  def update_params (self, params):
+  def update_params (self, params, cxx):
     try:
-      self.plot_data = osc_cxx (params)
-      # self.plot_data = osc_py (params)
+      if cxx:
+        self.plot_data = osc_cxx (params)
+      else:
+        self.plot_data = osc_py (params)
       self.repaint()
     except KeyError:
       # hacky way of avoiding to run testblep if not all params are known
@@ -159,12 +161,18 @@ class PlotWindow (QtWidgets.QMainWindow):
     central_widget.setLayout (self.grid_layout)
     self.setCentralWidget (central_widget)
 
+    self.source_button = QtWidgets.QPushButton ("Source: CXX/Py")
+
     self.params = dict()
     self.add_slider (1, "shape", 0, -100, 100)
     self.add_slider (2, "sync", 0, 0, 60)
     self.add_slider (3, "pulse", 50, 0, 100)
     self.add_slider (4, "sub", 0, 0, 100)
     self.add_slider (5, "subw", 50, 0, 100)
+    self.source_button.setCheckable (True)
+    self.source_button.checkable = True
+    self.source_button.clicked.connect (self.on_source_clicked)
+    self.grid_layout.addWidget (self.source_button, 4, 0, 1, 6)
 
   def add_slider (self, row, label, vdef, vmin, vmax):
     slider = QtWidgets.QSlider (QtCore.Qt.Vertical)
@@ -184,7 +192,10 @@ class PlotWindow (QtWidgets.QMainWindow):
   def on_param_changed (self, label, value):
     self.params[label].value = value / 10
     self.params[label].value_label.setText ("%s" % self.params[label].value)
-    self.blep_widget.update_params (self.params)
+    self.blep_widget.update_params (self.params, self.source_button.isChecked())
+
+  def on_source_clicked (self):
+    self.blep_widget.update_params (self.params, self.source_button.isChecked())
 
 def main():
   app = QtWidgets.QApplication (sys.argv)
