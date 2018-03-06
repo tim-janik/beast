b3a8e42b826cf6daf5ae784df8a1772dae9df0ac
Tim Janik <timj@gnu.org>
2018-03-06 01:25:19 +0100

    EBEAST: vc/utilities.js: add VueifyObject to turn a objects into Vue instances
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ebeast/vc/utilities.js
+++ ebeast/vc/utilities.js
@@ -1,6 +1,30 @@
 // This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 'use strict';
 
+/** VueifyObject - turn a regular object into a Vue instance.
+ * The @a object passed in is used as the Vue 'data' object. Properties
+ * with a getter (and possibly setter) are turned into Vue 'computed'
+ * properties, methods are carried over as methods on the Vue() instance.
+ */
+exports.VueifyObject = function (object = {}, vue_options = {}) {
+  let voptions = Object.assign ({}, vue_options);
+  voptions.methods = vue_options.methods || {};
+  voptions.computed = vue_options.computed || {};
+  voptions.data = voptions.data || object;
+  const proto = object.__proto__;
+  for (const pname of Object.getOwnPropertyNames (proto)) {
+    if (typeof proto[pname] == 'function' && pname != 'constructor')
+      voptions.methods[pname] = proto[pname];
+    else
+      {
+	const pd = Object.getOwnPropertyDescriptor (proto, pname);
+	if (pd.get)
+	  voptions.computed[pname] = pd;
+      }
+  }
+  return new Vue (voptions);
+};
+
 /// Copy PropertyDescriptors from source to target, optionally binding handlers against closure
 exports.clone_descriptors = (target, source, closure) => {
   // See also: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign completeAssign()
