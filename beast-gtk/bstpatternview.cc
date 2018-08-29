0ce462ef38b2f4d88dc9b73718422b4804facc61
Tim Janik <timj@gnu.org>
2018-08-29 23:05:26 +0200

    BEAST-GTK: bstpatternview: derive BstPatternViewDrag from GxkScrollCanvasDrag
    Signed-off-by: Tim Janik <timj@gnu.org>

--- beast-gtk/bstpatternview.cc
+++ beast-gtk/bstpatternview.cc
@@ -859,11 +859,14 @@ pattern_view_handle_drag (GxkScrollCanvas     *scc,
                           GdkEvent            *event)
 {
   BstPatternView *self = BST_PATTERN_VIEW (scc);
-  BstPatternViewDrag drag_mem = { 0 }, *drag = &drag_mem;
-  gint hvalid = 1, hdrag = scc_drag->canvas_drag || scc_drag->top_panel_drag;
-  gint vvalid = 1, vdrag = scc_drag->canvas_drag || scc_drag->left_panel_drag;
+  BstPatternViewDrag drag_mem, *drag = &drag_mem;
+  int hvalid = 1, hdrag = scc_drag->canvas_drag || scc_drag->top_panel_drag;
+  int vvalid = 1, vdrag = scc_drag->canvas_drag || scc_drag->left_panel_drag;
   /* copy over drag setup */
-  memcpy (drag, scc_drag, sizeof (*scc_drag));  /* sizeof (*scc_drag) < sizeof (*drag) */
+  {
+    GxkScrollCanvasDrag *scdrag = drag;
+    *scdrag = *scc_drag;
+  }
   drag->pview = self;
   /* calculate widget specific drag data */
   if (hdrag)
