12bcf986196b09ca47bb8c77d604a8fb1cf28eed
Tim Janik <timj@gnu.org>
2018-09-17 11:46:12 +0200

    SFI: sfidl: remove --nostdinc argument
    Signed-off-by: Tim Janik <timj@gnu.org>

--- sfi/sfidl-options.cc
+++ sfi/sfidl-options.cc
@@ -30,7 +30,6 @@ Options::Options ()
 bool Options::parse (int *argc_p, char **argv_p[], const Parser& parser)
 {
   bool printIncludePath = false;
-  bool noStdInc = false;
 
   OptionVector codeGeneratorOptions;
 
@@ -77,11 +76,6 @@ bool Options::parse (int *argc_p, char **argv_p[], const Parser& parser)
 	{
 	  printIncludePath = true;
 	  doExit = true;
-	  argv[i] = NULL;
-	}
-      else if (strcmp ("--nostdinc", argv[i]) == 0)
-	{
-	  noStdInc = true;
 	  argv[i] = NULL;
 	}
       else if (!codeGenerator) /* only one code generator allowed */
@@ -157,18 +151,6 @@ bool Options::parse (int *argc_p, char **argv_p[], const Parser& parser)
   if (e)
     *argc_p = e;
 
-  /* add std include path */
-  if (!noStdInc)
-    {
-      const std::string stdinc_path = Bse::installpath (Bse::INSTALLPATH_BSEINCLUDEDIR);
-      char *dir = strtok (const_cast<char*> (stdinc_path.c_str()), G_SEARCHPATH_SEPARATOR_S);
-      while (dir && dir[0])
-	{
-	  includePath.push_back (dir);
-	  dir = strtok (NULL, G_SEARCHPATH_SEPARATOR_S);
-	}
-    }
-
   /* option validation */
 
   if (doHelp)
@@ -201,7 +183,6 @@ void Options::printUsage ()
   fprintf (stderr, "general options:\n");
   fprintf (stderr, " -I <directory>              add this directory to the include path\n");
   fprintf (stderr, " --print-include-path        print include path\n");
-  fprintf (stderr, " --nostdinc                  don't use standard include path\n");
   fprintf (stderr, "\n");
   fprintf (stderr, " --help                      help for %s\n", sfidlName.c_str());
   fprintf (stderr, " --help <binding>            help for a specific binding\n");
