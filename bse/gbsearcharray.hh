43cdda05b402f95bf5de0fca222ab75cd59782fb
Tim Janik <timj@gnu.org>
2018-09-26 20:45:42 +0200

    BSE: gbsearcharray: avoid using BSE_ASSERT_RETURN, since we include no bse headers
    Signed-off-by: Tim Janik <timj@gnu.org>

--- bse/gbsearcharray.hh
+++ bse/gbsearcharray.hh
@@ -114,7 +114,7 @@ g_bsearch_array_create (const GBSearchConfig *bconfig)
   GBSearchArray *barray;
   guint size;
 
-  BSE_ASSERT_RETURN (bconfig != NULL, NULL);
+  g_return_val_if_fail (bconfig != NULL, NULL);
 
   size = sizeof (GBSearchArray) + bconfig->sizeof_node;
   if (bconfig->flags & G_BSEARCH_ARRAY_ALIGN_POWER2)
@@ -172,7 +172,7 @@ g_bsearch_array_get_index (GBSearchArray        *barray,
 {
   guint distance = ((guint8*) node_in_array) - G_BSEARCH_ARRAY_NODES (barray);
 
-  BSE_ASSERT_RETURN (node_in_array != NULL, barray->n_nodes);
+  g_return_val_if_fail (node_in_array != NULL, barray->n_nodes);
 
   distance /= bconfig->sizeof_node;
 
@@ -187,7 +187,7 @@ g_bsearch_array_grow (GBSearchArray        *barray,
   guint new_size = old_size + bconfig->sizeof_node;
   guint8 *node;
 
-  BSE_ASSERT_RETURN (index <= barray->n_nodes, NULL);
+  g_return_val_if_fail (index <= barray->n_nodes, NULL);
 
   if (G_UNLIKELY (bconfig->flags & G_BSEARCH_ARRAY_ALIGN_POWER2))
     {
@@ -251,7 +251,7 @@ g_bsearch_array_remove (GBSearchArray        *barray,
 {
   guint8 *node;
 
-  BSE_ASSERT_RETURN (index < barray->n_nodes, NULL);
+  g_return_val_if_fail (index < barray->n_nodes, NULL);
 
   barray->n_nodes -= 1;
   node = G_BSEARCH_ARRAY_NODES (barray) + index * bconfig->sizeof_node;
@@ -284,7 +284,7 @@ static inline void
 g_bsearch_array_free (GBSearchArray        *barray,
                       const GBSearchConfig *bconfig)
 {
-  BSE_ASSERT_RETURN (barray != NULL);
+  g_return_if_fail (barray != NULL);
 
   g_free (barray);
 }
