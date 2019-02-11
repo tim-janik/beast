e560221301feebc5cfae104679e26b67153eab17
Tim Janik <timj@gnu.org>
2019-02-11 13:31:59 +0100

    version.sh: extract version from NEWS and git archive export-subst fields
    Signed-off-by: Tim Janik <timj@gnu.org>

--- version.sh
+++ version.sh
@@ -1,38 +1,65 @@
 #!/usr/bin/env bash
-set -Ee -o pipefail
+set -Eeuo pipefail
 
-# Usage: git-version.sh [-s]		# print project version
+# Usage: version.sh [-s]		# print project version
+SHORT=false LONG=false RDATE=false
+while test $# -ne 0 ; do
+  case "$1" in
+    -s)			SHORT=true ;;
+    -l)			LONG=true ;;
+    -d)			RDATE=true ;;
+    *)                  : ;;
+  esac
+  shift
+done
+UNOFFICIAL=snapshot
 
-if   test " $1" = " -s" ; then	# short
-  FORMAT="--abbrev=0"
-elif test " $1" = " -l" ; then	# long, clean
-  FORMAT="--abbrev=7 --long"
-else				# default
-  FORMAT="--abbrev=7 --dirty"
-fi
-
-# Find version in toplevel project directory
-cd "$(dirname "$0")"
-
-# If present, git knows the canonical version
-if test -d ${GIT_DIR:-.git} -o -f .git ; then
-  # Find most recent annotated version tag, excluding tags merged
-  # through branches, i.e. find tag through --first-parent
-  TRUNKTAG=$(git describe --match '[0-9]*.[0-9]*.*[0-9a]' --abbrev=0 --first-parent)
-  # Append comit hash and *full* commit count to the tag, to generate
-  # monotonically increasing versions, i.e. *avoid* using --first-parent
-  VERSION=$(git describe --match "$TRUNKTAG" $FORMAT)
-  rm -f git-version.cache	# the cache file maybe stale if git works
-  echo "$VERSION"
+# Print short or long version and exit successfully
+exit_with_version() { # exit_with_version <version> <buildid> <releasedate>
+  if $SHORT ; then
+    echo "$1"
+  elif $LONG ; then
+    echo "$2"
+  elif $RDATE ; then
+    echo "$3"
+  else
+    echo "$@"
+  fi
   exit 0
-fi
+}
 
-# Fallback to tarball version
-if test -f git-version.cache ; then
-  cat git-version.cache
-  exit 0
-fi
+# Determine version from Git if ./.git is present
+COMMIT_DATE=$(git log -1 --format='%ci' 2>/dev/null) &&
+  LAST_TAG=$(git describe --match '[0-9]*.[0-9]*.*[0-9a]' --abbrev=0 --first-parent 2>/dev/null ||
+	     git describe --match v'[0-9]*.[0-9]*.*[0-9a]' --abbrev=0 --first-parent) &&
+  BUILD_ID=$(git describe --match "$LAST_TAG" --abbrev=8 --long --dirty) &&
+  test -n "$BUILD_ID" &&
+  exit_with_version "$LAST_TAG" "$BUILD_ID" "$COMMIT_DATE"
 
-# Give up
-echo "$0: missing version information" >&2
+# Determine version from NEWS file
+test -r NEWS &&
+  NEWS_VERSION="$(sed -nr '/^##\s.*[0-9]+\.[0-9]+\.[0-9]/ { s/.*\s([0-9]+\.[0-9]+\.[0-9]+[a-z0-9A-Z-]+)\b.*/\1/; p; q }' NEWS)" &&
+  NEWS_DATE="$(stat -c %y NEWS | sed 's/\.[0-9]\+//')" ||
+    NEWS_VERSION=
+
+# Determine version via 'git archive' (see man gitattributes / export-subst)
+GITARCHIVE_REFS='$Format:%D,$'
+GITARCHIVE_DATE='$Format:%ci$'
+[[ "$GITARCHIVE_REFS$GITARCHIVE_DATE" =~ '$'Format:% ]] && LAST_TAG= ||
+    LAST_TAG=$(echo " $GITARCHIVE_REFS" | tr , '\n' | \
+		 sed -nr '/\btag: [0-9]\.[0-9]+\.[0-9]+/ { s/^[^0-9]*\b([0-9]+\.[0-9]+\.[0-9]+[a-z0-9A-Z-]+).*/\1/; p; q }')
+test -n "$LAST_TAG" && {
+  # FIXME: LAST_TAG == NEWS is release
+  if test "$LAST_TAG" = "$NEWS_VERSION" ; then
+    exit_with_version "$LAST_TAG" "$LAST_TAG-tarball" "$GITARCHIVE_DATE" # release version
+  else
+    exit_with_version "$LAST_TAG" "$LAST_TAG-$UNOFFICIAL" "$GITARCHIVE_DATE"
+  fi
+}
+
+# Use NEWS_VERSION as last resort
+test -n "$NEWS_VERSION" &&
+  exit_with_version "$NEWS_VERSION" "$NEWS_VERSION-$UNOFFICIAL" "$NEWS_DATE"
+
+echo "$0: ERROR: Failed to find project version, use a valid git repository or release tarball" >&2
 exit 97
