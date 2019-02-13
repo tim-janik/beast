72629d02df9ba92bc29ed81da3edaef9e5bf9683
Tim Janik <timj@gnu.org>
2019-02-13 14:56:56 +0100

    SFI: Makefile.mk: build and check testsfidl
    Signed-off-by: Tim Janik <timj@gnu.org>

--- sfi/Makefile.mk
+++ sfi/Makefile.mk
@@ -16,3 +16,18 @@ sfi/sfidl.cc.FLAGS  = -O0
 $(sfi/sfidl): $(sfi/sfidl.objects) $(MAKEFILE_LIST)
 	$(QECHO) LD $@
 	$Q $(CXX) $(CXXSTD) -fPIC -o $@ $(sfi/sfidl.objects) -Wl,--no-undefined $(GLIB_LIBS)
+
+# == testsfidl ==
+sfi/testsfidl		::= $>/sfi/testsfidl
+ALL_TARGETS	   	 += $(sfi/testsfidl)
+sfi/testsfidl.sources	  = sfi/testsfidl.cc
+sfi/testsfidl.objects	  = $(sort $(sfi/testsfidl.sources:%.cc=$>/%.o))
+$(sfi/testsfidl.objects): EXTRA_INCLUDES ::= -I$> $(GLIB_CFLAGS)
+sfi/testsfidl.cc.FLAGS	  = -O0
+#testsfidl_LDADD    = $(BSE_LIBS)
+$(sfi/testsfidl): $(sfi/testsfidl.objects) $(MAKEFILE_LIST)
+	$(QECHO) LD $@
+	$Q $(CXX) $(CXXSTD) -fPIC -o $@ $(sfi/testsfidl.objects) -Wl,--no-undefined $(GLIB_LIBS)
+sfi-check: .PHONY
+	$(sfi/testsfidl)
+check: sfi-check
