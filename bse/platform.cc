b410a605776d98ef305b73ea649b06aa35d79b2f
Tim Janik <timj@gnu.org>
2019-02-13 21:44:04 +0100

    BSE: platform: determine LIBTOOL_OBJDIR on the fly
    Signed-off-by: Tim Janik <timj@gnu.org>

--- bse/platform.cc
+++ bse/platform.cc
@@ -2,7 +2,6 @@
 #include "platform.hh"
 #include "path.hh"
 #include "bse/internal.hh"
-#include "../config/config.h"
 #include <unistd.h>
 #include <cstring>
 #include <fstream>
@@ -12,12 +11,17 @@
 #include <sys/wait.h>
 #include <sys/time.h>
 #include <sys/syscall.h>        // SYS_gettid
+
 #if defined __APPLE__
 #include <mach-o/dyld.h>        // _NSGetExecutablePath
 #endif // __APPLE__
+
 #ifdef  _WIN32                  // includes _WIN64
 #include <windows.h>
-#endif  // _WIN32
+#define LIBTOOL_OBJDIR          "_libs"
+#else   // !_WIN32
+#define LIBTOOL_OBJDIR          ".libs"
+#endif  // !_WIN32
 
 namespace Bse {
 
@@ -31,7 +35,7 @@ runpath (RPath rpath)
   static bool using_objdir = false;
   static const std::string libbse_installdir = determine_libbse_installdir (&using_objdir);
   static const std::string libbse_prefixdir = Path::dirname (libbse_installdir);
-  const char *objdir = using_objdir ? "/" CONFIGURE_RELPATH_OBJDIR : "";
+  const char *objdir = using_objdir ? "/" LIBTOOL_OBJDIR : "";
   switch (rpath)
     {
     case RPath::PREFIXDIR:      return libbse_prefixdir;
@@ -519,7 +523,7 @@ timestamp_init_ ()
     if (clock_getres (CLOCK_REALTIME, &tp) >= 0)
       monotonic_resolution = tp.tv_sec * 1000000000ULL + tp.tv_nsec;
     uint64 mstart = realtime_start;
-#ifdef CLOCK_MONOTONIC
+#ifdef CLOCK_MONOTONIC          // time.h
     // CLOCK_MONOTONIC_RAW cannot slew, but doesn't measure SI seconds accurately
     // CLOCK_MONOTONIC may slew, but attempts to accurately measure SI seconds
     if (monotonic_clockid == CLOCK_REALTIME && clock_getres (CLOCK_MONOTONIC, &tp) >= 0)
@@ -716,7 +720,7 @@ determine_libbse_installdir (bool *using_objdir)
   char intlibname[256] = { 0, };
   snprintf (intlibname, sizeof (intlibname), "/bse/libbse-%u.so", BSE_MAJOR_VERSION);
   char lbtlibname[256] = { 0, };
-  snprintf (lbtlibname, sizeof (lbtlibname), "/bse/%s/libbse-%u.so", CONFIGURE_RELPATH_OBJDIR, BSE_MAJOR_VERSION);
+  snprintf (lbtlibname, sizeof (lbtlibname), "/bse/%s/libbse-%u.so", LIBTOOL_OBJDIR, BSE_MAJOR_VERSION);
   std::string cxxline;
   while (std::getline (maps, cxxline))
     {
