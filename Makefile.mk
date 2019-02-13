24b0cd21ba0d11587b375f1420a706c468a8f0b0
Tim Janik <timj@gnu.org>
2019-02-13 13:12:47 +0100

    Makefile.mk: add topmost makefile for GNU Make builds
    Signed-off-by: Tim Janik <timj@gnu.org>

---
+++ Makefile.mk
@@ -0,0 +1,53 @@
+# This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
+
+# == Make ==
+all:		# Default Rule
+MAKEFLAGS += -r
+SHELL    ::= /bin/bash -o pipefail
+
+# == Basics ==
+ALL_TARGETS ::=
+CLEANFILES  ::=
+CLEANDIRS   ::=
+OUTDIR      ::= out
+>	    ::= $(OUTDIR)
+.PHONY: ;
+include config-utils.mk
+include config-uname.mk
+
+# == Defaults ==
+INCLUDES	::= -I.
+DEFS		::=
+
+# == Compiler Setup ==
+DEBUGFLAGS       ?= -g
+CXXSTD		::= -std=gnu++14 -pthread -pipe $(DEBUGFLAGS)
+CSTD		::= -std=gnu11 -pthread -pipe $(DEBUGFLAGS)
+EXTRA_DEFS	::= # target private defs, lesser precedence than CXXFLAGS
+EXTRA_INCLUDES	::= # target private defs, lesser precedence than CXXFLAGS
+EXTRA_FLAGS	::= # target private flags, precedence over CXXFLAGS
+CCACHE		 ?= $(if $(CCACHE_DIR), ccache)
+
+# == Rules ==
+all: .PHONY
+check: .PHONY
+install: .PHONY
+uninstall: .PHONY
+clean: .PHONY
+
+# == implicit rules ==
+$>/%/: ; $(Q) mkdir -p $@
+$>/%.o: %.c
+	$(QECHO) CC $@
+	$(Q) $(CCACHE) $(CC) $(CSTD) -fPIC $(COMPILEDEFS) $(CFLAGS) $(COMPILEFLAGS) -o $@ -c $<
+$>/%.o: %.cc
+	$(QECHO) CXX $@
+	$(Q) $(CCACHE) $(CXX) $(CXXSTD) -fPIC $(COMPILEDEFS) $(CXXFLAGS) $(COMPILEFLAGS) -o $@ -c $<
+COMPILEDEFS  = $(DEFS) $(EXTRA_DEFS) $($<.DEFS) $($@.DEFS) $(INCLUDES) $(EXTRA_INCLUDES) $($<.INCLUDES) $($@.INCLUDES)
+COMPILEFLAGS = $(EXTRA_FLAGS) $($<.FLAGS) $($@.FLAGS) -MQ '$@' -MMD -MF '$@'.d
+
+# == rules ==
+clean:
+	@test -z "$(strip $(CLEANFILES))" || (set -x; rm -f $(CLEANFILES) )
+	@test -z "$(strip $(CLEANDIRS))" || (set -x; rm -fr $(CLEANDIRS) )
+all: $(ALL_TARGETS)
