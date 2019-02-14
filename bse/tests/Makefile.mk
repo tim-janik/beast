533e6c00139d9e0b0e398d761af81c0650009d20
Tim Janik <timj@gnu.org>
2019-02-14 01:36:58 +0100

    BSE: tests/Makefile.mk: build bse/tests/integrity and add checks
    Signed-off-by: Tim Janik <timj@gnu.org>

---
+++ bse/tests/Makefile.mk
@@ -0,0 +1,45 @@
+# This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
+include $(wildcard $>/bse/tests/*.d)
+CLEANDIRS += $(wildcard $>/bse/tests/)
+
+
+# == integrity ==
+bse/tests/integrity		::= $>/bse/tests/integrity
+ALL_TARGETS			 += $(bse/tests/integrity)
+bse/tests/integrity.sources	::= bse/tests/integrity.cc
+bse/tests/integrity.objects	::= $(sort $(bse/tests/integrity.sources:%.cc=$>/%.o))
+$(bse/tests/integrity.objects):	| $>/bse/tests/
+$(bse/tests/integrity.objects):	EXTRA_INCLUDES ::= -I$> $(GLIB_CFLAGS)
+bse/tests/integrity.objects.FLAGS = -O0
+# BIN linking
+$(bse/tests/integrity): $(bse/tests/integrity.objects) $(bse/libbse.solinks) $(MAKEFILE_LIST)
+	$(QECHO) LD $@
+	$Q $(CXX) $(CXXSTD) -fPIC -o $@ $(bse/tests/integrity.objects) -Wl,--no-undefined \
+	-Wl,-rpath='$$ORIGIN/../../bse:$$ORIGIN/' -Wl,-L$>/bse/ -lbse-$(VERSION_MAJOR) \
+	$(GLIB_LIBS)
+
+# == check ==
+bse-tests-check: .PHONY
+check: bse-tests-check
+
+# == bse-tests-check-integrity ==
+bse-tests-check-integrity: .PHONY	| $(bse/tests/integrity)
+	$(QECHO) RUN… $@
+	$Q $(bse/tests/integrity)
+bse-tests-check: bse-tests-check-integrity
+
+# == bse-tests-check-assertions ==
+$>/bse/tests/t279-assertions-test: .PHONY	| $(bse/tests/integrity)
+	$(QECHO) RUN $@
+	$Q $(bse/tests/integrity) --return_unless1 || $(QDIE) --return_unless1 failed
+	$Q $(bse/tests/integrity) --assert_return1 || $(QDIE) --assert_return1 failed
+	$Q (trap ':' SIGTRAP && $(bse/tests/integrity) --return_unless0) $(QSTDERR) ; test "$$?" -eq 7 || $(QDIE) --return_unless0 failed
+	$Q (trap ':' SIGTRAP && $(bse/tests/integrity) --assert_return0) $(QSTDERR) ; test "$$?"  != 0 || $(QDIE) --assert_return0 failed
+	$Q (trap ':' SIGTRAP && $(bse/tests/integrity) --assert_return_unreached) $(QSTDERR) ; test "$$?" != 0 || $(QDIE) --assert_return_unreached failed
+	$Q (trap ':' SIGTRAP && $(bse/tests/integrity) --fatal_error) $(QSTDERR) ; test "$$?" != 0 || $(QDIE) --fatal_error failed
+	$Q $(bse/tests/integrity) --backtrace	2> $@.tmp && \
+		grep -qi 'Backtrace'		   $@.tmp && \
+		grep -qi 'in.*my_compare_func'	   $@.tmp || $(QDIE) --backtrace failed
+	$Q rm $@.tmp
+	@echo "  PASS    " $@
+bse-tests-check: $>/bse/tests/t279-assertions-test
