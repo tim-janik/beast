6d781c741a27a7017bbe94ec16ba073b86d6ab40
Tim Janik <timj@gnu.org>
2019-02-22 00:42:43 +0100

    SFI: Makefile.mk: minor cleanups
    Signed-off-by: Tim Janik <timj@gnu.org>

--- sfi/Makefile.mk
+++ sfi/Makefile.mk
@@ -2,26 +2,31 @@
 include $(wildcard $>/sfi/*.d)
 CLEANDIRS += $(wildcard $>/sfi/)
 
-
-# == sfidl ==
+# == sfidl defs ==
 sfi/sfidl	  ::= $>/sfi/sfidl
 ALL_TARGETS	   += $(sfi/sfidl)
 sfi/sfidl.sources ::= sfi/sfidl.cc
 sfi/sfidl.objects ::= $(sort $(sfi/sfidl.sources:%.cc=$>/%.o))
+sfi/sfidl.cc.FLAGS  = -O0
+
+# == sfidl rules ==
 $(sfi/sfidl.objects):	| $>/sfi/
 $(sfi/sfidl.objects): EXTRA_DEFS ::= -DG_LOG_DOMAIN=\"SFI\" -DG_DISABLE_CONST_RETURNS -DPARANOID -DSFIDL_INTERNALS
 $(sfi/sfidl.objects): EXTRA_INCLUDES ::= $(GLIB_CFLAGS)
-sfi/sfidl.cc.FLAGS  = -O0
 $(eval $(call LINKER, $(sfi/sfidl), $(sfi/sfidl.objects), , $(GLIB_LIBS)))
 
-# == testsfidl ==
+# == testsfidl defs ==
 sfi/testsfidl		::= $>/sfi/testsfidl
 ALL_TARGETS	   	 += $(sfi/testsfidl)
 sfi/testsfidl.sources	  = sfi/testsfidl.cc
 sfi/testsfidl.objects	  = $(sort $(sfi/testsfidl.sources:%.cc=$>/%.o))
-$(sfi/testsfidl.objects): EXTRA_INCLUDES ::= -I$> $(GLIB_CFLAGS)
 sfi/testsfidl.cc.FLAGS	  = -O0
+
+# == testsfidl rules ==
+$(sfi/testsfidl.objects): EXTRA_INCLUDES ::= -I$> $(GLIB_CFLAGS)
 $(eval $(call LINKER, $(sfi/testsfidl), $(sfi/testsfidl.objects), , $(GLIB_LIBS)))
+
+# == sfi-check ==
 sfi-check: FORCE	| $(sfi/testsfidl)
 	$(QECHO) RUN $@
 	$Q $(sfi/testsfidl)
