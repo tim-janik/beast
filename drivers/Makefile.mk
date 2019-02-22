a0b1eec976daa9f4c876cda0c3f0077e4ec810c3
Tim Janik <timj@gnu.org>
2019-02-22 01:13:12 +0100

    DRIVERS: Makefile.mk: minor cleanups
    Signed-off-by: Tim Janik <timj@gnu.org>

--- drivers/Makefile.mk
+++ drivers/Makefile.mk
@@ -2,25 +2,29 @@
 include $(wildcard $>/drivers/*.d)
 CLEANDIRS += $(wildcard $>/drivers/)
 
-# == alsapcm ==
-alsapcm.so		::= alsapcm.so
-alsapcm.soname		::= $(alsapcm.so)
-drivers/alsapcm.sofile	::= $>/drivers/$(alsapcm.so)
-ALL_TARGETS		 += $(drivers/alsapcm.sofile)
-drivers/alsapcm.sources	::= drivers/bsepcmdevice-alsa.cc
-drivers/alsapcm.objects	::= $(sort $(drivers/alsapcm.sources:%.cc=$>/%.o))
-$(drivers/alsapcm.objects): $(bse/libbse.deps) | $>/drivers/
-$(drivers/alsapcm.objects): EXTRA_INCLUDES ::= -I$> $(GLIB_CFLAGS)
+# == alsapcm defs ==
+alsapcm.so			  ::= alsapcm.so
+alsapcm.soname			  ::= $(alsapcm.so)
+drivers/alsapcm.sofile		  ::= $>/drivers/$(alsapcm.so)
+ALL_TARGETS			   += $(drivers/alsapcm.sofile)
+drivers/alsapcm.sources		  ::= drivers/bsepcmdevice-alsa.cc
+drivers/alsapcm.objects		  ::= $(sort $(drivers/alsapcm.sources:%.cc=$>/%.o))
 $(drivers/alsapcm.sofile).LDFLAGS ::= -shared -Wl,-soname,$(alsapcm.soname)
-$(eval $(call LINKER, $(drivers/alsapcm.sofile), $(drivers/alsapcm.objects), $(bse/libbse.solinks), -lbse-$(VERSION_MAJOR) $(BSEDEPS_LIBS) $(ALSA_LIBS), ../bse))
 
-# == alsamidi ==
-alsamidi.so		::= alsamidi.so
-alsamidi.soname		::= $(alsamidi.so)
-drivers/alsamidi.sofile	::= $>/drivers/$(alsamidi.so)
-ALL_TARGETS		 += $(drivers/alsamidi.sofile)
+# == alsamidi defs ==
+alsamidi.so			::= alsamidi.so
+alsamidi.soname			::= $(alsamidi.so)
+drivers/alsamidi.sofile		::= $>/drivers/$(alsamidi.so)
+ALL_TARGETS			 += $(drivers/alsamidi.sofile)
 drivers/alsamidi.sources	::= drivers/bsemididevice-alsa.cc
 drivers/alsamidi.objects	::= $(sort $(drivers/alsamidi.sources:%.cc=$>/%.o))
+
+# == alsapcm rules ==
+$(drivers/alsapcm.objects): $(bse/libbse.deps) | $>/drivers/
+$(drivers/alsapcm.objects): EXTRA_INCLUDES ::= -I$> $(GLIB_CFLAGS)
+$(eval $(call LINKER, $(drivers/alsapcm.sofile), $(drivers/alsapcm.objects), $(bse/libbse.solinks), -lbse-$(VERSION_MAJOR) $(BSEDEPS_LIBS) $(ALSA_LIBS), ../bse))
+
+# == alsamidi rules ==
 $(drivers/alsamidi.objects): $(bse/libbse.deps) | $>/drivers/
 $(drivers/alsamidi.objects): EXTRA_INCLUDES ::= -I$> $(GLIB_CFLAGS)
 $(drivers/alsamidi.sofile).LDFLAGS ::= -shared -Wl,-soname,$(alsamidi.soname)
