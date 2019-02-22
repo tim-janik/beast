dc2381209d7ed041bcf0b9f92b7e25d71ec65625
Tim Janik <timj@gnu.org>
2019-02-22 17:59:36 +0100

    DRIVERS: Makefile.mk: build modules with BUILD_SHARED_LIB
    Signed-off-by: Tim Janik <timj@gnu.org>

--- drivers/Makefile.mk
+++ drivers/Makefile.mk
@@ -1,32 +1,34 @@
 # This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 include $(wildcard $>/drivers/*.d)
 CLEANDIRS += $(wildcard $>/drivers/)
+drivers/rpath..bse ::= ../bse
 
 # == alsapcm defs ==
-alsapcm.so			  ::= alsapcm.so
-alsapcm.soname			  ::= $(alsapcm.so)
-drivers/alsapcm.sofile		  ::= $>/drivers/$(alsapcm.so)
-ALL_TARGETS			   += $(drivers/alsapcm.sofile)
-drivers/alsapcm.sources		  ::= drivers/bsepcmdevice-alsa.cc
-drivers/alsapcm.objects		  ::= $(sort $(drivers/alsapcm.sources:%.cc=$>/%.o))
-$(drivers/alsapcm.sofile).LDFLAGS ::= -shared -Wl,-soname,$(alsapcm.soname)
+drivers/alsapcm.so		::= $>/drivers/alsapcm.so
+drivers/alsapcm.sources		::= drivers/bsepcmdevice-alsa.cc
+drivers/alsapcm.objects		::= $(sort $(drivers/alsapcm.sources:%.cc=$>/%.o))
 
 # == alsamidi defs ==
-alsamidi.so			::= alsamidi.so
-alsamidi.soname			::= $(alsamidi.so)
-drivers/alsamidi.sofile		::= $>/drivers/$(alsamidi.so)
-ALL_TARGETS			 += $(drivers/alsamidi.sofile)
+drivers/alsamidi.so		::= $>/drivers/alsamidi.so
 drivers/alsamidi.sources	::= drivers/bsemididevice-alsa.cc
 drivers/alsamidi.objects	::= $(sort $(drivers/alsamidi.sources:%.cc=$>/%.o))
 
 # == alsapcm rules ==
 $(drivers/alsapcm.objects): $(bse/libbse.deps) | $>/drivers/
 $(drivers/alsapcm.objects): EXTRA_INCLUDES ::= -I$> $(GLIB_CFLAGS)
-$(eval $(call LINKER, $(drivers/alsapcm.sofile), $(drivers/alsapcm.objects), $(bse/libbse.sofiles), -lbse-$(VERSION_MAJOR) $(BSEDEPS_LIBS) $(ALSA_LIBS), ../bse))
+$(call BUILD_SHARED_LIB, \
+	$(drivers/alsapcm.so), \
+	$(drivers/alsapcm.objects), \
+	$(bse/libbse.sofiles) | $>/drivers/, \
+	-lbse-$(VERSION_MAJOR) $(BSEDEPS_LIBS) $(ALSA_LIBS), \
+	$(drivers/rpath..bse))
 
 # == alsamidi rules ==
 $(drivers/alsamidi.objects): $(bse/libbse.deps) | $>/drivers/
 $(drivers/alsamidi.objects): EXTRA_INCLUDES ::= -I$> $(GLIB_CFLAGS)
-$(drivers/alsamidi.sofile).LDFLAGS ::= -shared -Wl,-soname,$(alsamidi.soname)
-$(eval $(call LINKER, $(drivers/alsamidi.sofile), $(drivers/alsamidi.objects), $(bse/libbse.sofiles), -lbse-$(VERSION_MAJOR) $(BSEDEPS_LIBS) $(ALSA_LIBS), ../bse))
-
+$(call BUILD_SHARED_LIB, \
+	$(drivers/alsamidi.so), \
+	$(drivers/alsamidi.objects), \
+	$(bse/libbse.sofiles) | $>/drivers/, \
+	-lbse-$(VERSION_MAJOR) $(BSEDEPS_LIBS) $(ALSA_LIBS), \
+	$(drivers/rpath..bse))
