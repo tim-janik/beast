385681981258fdc349d226abee8eba08b9f8faa3
Tim Janik <timj@gnu.org>
2019-03-01 15:34:01 +0100

    DOCS: Makefile.mk: build and install doc/README
    Signed-off-by: Tim Janik <timj@gnu.org>

--- docs/Makefile.mk
+++ docs/Makefile.mk
@@ -4,15 +4,24 @@ CLEANDIRS += $(wildcard $>/doc/)
 
 # == docs/ files ==
 docs/doc.files ::= $(strip		\
+	$>/doc/README			\
 	$>/doc/copyright		\
 )
-docs/doc.dir   ::= $(pkglibdir)/doc
-ALL_TARGETS     += $(docs/doc.files)
+docs/doc.dir	  ::= $(pkglibdir)/doc
+ALL_TARGETS	   += $(docs/doc.files)
+docs/md_flags	  ::= $(strip	--html-q-tags -p -s --section-divs --email-obfuscation=references \
+				-f markdown+autolink_bare_uris+emoji+lists_without_preceding_blankline \
+				--toc --toc-depth=6)
+# --css /pandoc-html.css
 
 # == doc build rules ==
 $>/doc/copyright: docs/copyright		| $>/doc/
 	$(QECHO) COPY $@
 	$Q cp -L $< $@
+$>/doc/%: %.md
+	$(QGEN)
+	$Q $(PANDOC) $(docs/md_flags) -t plain $< -o $@.tmp
+	$Q mv $@.tmp $@
 
 # == installation rules ==
 $(call INSTALL_DATA_RULE, docs/doc.files, $(DESTDIR)$(docs/doc.dir), $(docs/doc.files))
