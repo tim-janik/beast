5c845086bf84b3bbf9cd8492e8e6e33f4796bcc6
Tim Janik <timj@gnu.org>
2019-03-08 21:06:48 +0100

    TESTS: audio/Makefile.mk: create check-audio tests via template
    Signed-off-by: Tim Janik <timj@gnu.org>

--- tests/audio/Makefile.mk
+++ tests/audio/Makefile.mk
@@ -18,24 +18,35 @@ tests/audio/render2wav = $(strip					\
 	  --bse-override-sample-path 'tests/audio:media/Samples'	\
 	  --bse-disable-randomization )
 
+# == tests/audio/template ==
+# $(call tests/audio/template, 1TESTNAME, 2BSE+REF+DEPS, 3RENDERARGS, 4FEATURES-A, 5FEATURES-B, 6THRESHOLD)
+define tests/audio/template.impl
+$1: $2 FORCE
+	$$(QECHO) WAVCHECK $$@
+	$$Q $$(tests/audio/render2wav) $$< $3 $$>/$$@.wav
+	$$Q $$(if $4, $$(tools/bsetool) fextract $$>/$$@.wav $4) > $$>/$$@.tmp
+	$$Q $$(if $5, $$(tools/bsetool) fextract $$>/$$@.wav $5  >>$$>/$$@.tmp )
+	$$Q $$(tools/bsetool) fcompare $(if $(findstring 1, $(V)),--verbose) $$(word 2,$$^) $$>/$$@.tmp --threshold $6
+	$$Q rm -f $$>/$$@.wav $$>/$$@.tmp
+tests/audio/checks += $1
+endef
+tests/audio/template = $(eval $(call tests/audio/template.impl,$1,$2,$3,$4,$5,$6))
+
 # == tests/audio/adsrtest ==
-tests/audio/adsrtest: tests/audio/adsrtest.bse tests/audio/adsrtest.ref
-	$(QECHO) WAVCHECK $@	# ADSR Test checks the mono channel envelope rendering
-	$Q $(tests/audio/render2wav) $< $>/$@.wav
-	$Q $(tools/bsetool) fextract $>/$@.wav --cut-zeros --channel 0 --avg-spectrum --spectrum --avg-energy > $>/$@.tmp
-	$Q $(tools/bsetool) fcompare $(<:%.bse=%.ref) $>/$@.tmp --threshold 99.99
-	$Q rm -f $>/$@.wav $>/$@.tmp
-tests/audio/checks += tests/audio/adsrtest
+$(call tests/audio/template,							\
+	tests/audio/adsrtest,							\
+	tests/audio/adsrtest.bse tests/audio/adsrtest.ref, ,			\
+	--cut-zeros --channel 0 --avg-spectrum --spectrum --avg-energy, ,	\
+	99.99)	# ADSR Test checks the mono channel envelope rendering
 
 # == tests/audio/adsr-wave-1-test ==
-tests/audio/adsr-wave-1-test: tests/audio/adsr-wave-1-test.bse tests/audio/adsr-wave-1-test.ref tests/audio/pseudo-saw.bsewave
-	$(QECHO) WAVCHECK $@	# test for play-type=adsr-wave-1
-	$Q $(tests/audio/render2wav) $< $>/$@.wav
-	$Q $(tools/bsetool) fextract $>/$@.wav --cut-zeros --channel 0 --avg-spectrum --spectrum --avg-energy --end-time  > $>/$@.tmp
-	$Q $(tools/bsetool) fextract $>/$@.wav --cut-zeros --channel 1 --avg-spectrum --spectrum --avg-energy --end-time >> $>/$@.tmp
-	$Q $(tools/bsetool) fcompare $(<:%.bse=%.ref) $>/$@.tmp --threshold 99.99
-	$Q rm -f $>/$@.wav $>/$@.tmp
-tests/audio/checks += tests/audio/adsr-wave-1-test
+$(call tests/audio/template,							\
+	tests/audio/adsr-wave-1-test,						\
+	tests/audio/adsr-wave-1-test.bse tests/audio/adsr-wave-1-test.ref tests/audio/pseudo-saw.bsewave, , \
+	--cut-zeros --channel 0 --avg-spectrum --spectrum --avg-energy --end-time, \
+	--cut-zeros --channel 1 --avg-spectrum --spectrum --avg-energy --end-time, \
+	99.99)	# test for play-type=adsr-wave-1
+
 
 # == check-audio ==
 $(tests/audio/checks): $(tools/bsetool) FORCE		| $>/tests/audio/
