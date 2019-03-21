058fb53c81537d52826163004fcf4dd32a28e6f5
Tim Janik <timj@gnu.org>
2019-03-21 02:26:23 +0100

    BSE: cxxaux: add BSE_DEPRECATED
    Signed-off-by: Tim Janik <timj@gnu.org>

--- bse/cxxaux.hh
+++ bse/cxxaux.hh
@@ -45,6 +45,7 @@ typedef vector<String> StringVector;    ///< Convenience alias for a std::vector
 #define BSE_CLAMP(v,mi,ma)      ((v) < (mi) ? (mi) : ((v) > (ma) ? (ma) : (v))) ///< Yield @a v clamped to [ @a mi .. @a ma ].
 #define BSE_ARRAY_SIZE(array)   (sizeof (array) / sizeof ((array)[0]))          ///< Yield the number of C @a array elements.
 #define BSE_ALIGN(size, base)   ((base) * ((size_t (size) + (base) - 1) / (base))) ///< Round up @a size to multiples of @a base.
+#define BSE_DEPRECATED          __attribute__ ((__deprecated__))
 /// @addtogroup GCC Attributes
 /// Bse macros that are shorthands for <a href="https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html">GCC Attributes</a>.
 /// @{
