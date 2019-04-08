d8dc5cbd137e7f30a4a8a0170e59ccf006f6af81
Tim Janik <timj@gnu.org>
2019-04-08 21:49:06 +0200

    MISC: subdirs.mk: support subdir local build rules with subdir/ prefixing
    Signed-off-by: Tim Janik <timj@gnu.org>

--- misc/subdirs.mk
+++ misc/subdirs.mk
@@ -1,11 +1,11 @@
-# forward make $(MAKECMDGOALS) to the project root
+# Forward make $(MAKECMDGOALS) to the project root, add subdir prefix
 
-# figure topdir from ../misc/subdirs.mk/..
-misc_subdirs_mk_topdir ::= $(abspath $(dir $(lastword $(MAKEFILE_LIST)))/..)
+# Figure topdir from ../misc/subdirs.mk/..
+misc/subdirs_mk/topdir ::= $(abspath $(dir $(lastword $(MAKEFILE_LIST)))/..)
 
-# extract subdir name from $(misc_subdirs_mk_topdir)/sub/dir as sub-dir
-misc_subdirs_mk_subdir_prefix ::= $(subst /,-, $(patsubst $(misc_subdirs_mk_topdir)/%, %, $(abspath .)))
+# extract subdir name from $(misc/subdirs_mk/topdir)/sub/dir as sub-dir
+misc/subdirs_mk/subdir_prefix ::= $(patsubst $(misc/subdirs_mk/topdir)/%, %, $(abspath .))
 
 MAKECMDGOALS ?= all
 ${MAKECMDGOALS}:
-	@$(MAKE) -C $(misc_subdirs_mk_topdir) $(misc_subdirs_mk_subdir_prefix)-$@
+	@exec $(MAKE) -C $(misc/subdirs_mk/topdir) $(misc/subdirs_mk/subdir_prefix)/$@
