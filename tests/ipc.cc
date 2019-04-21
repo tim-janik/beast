dc7620b74f90a49e59c64bb0b702cc18511aec41
Tim Janik <timj@gnu.org>
2019-04-22 01:45:32 +0200

    TESTS: ipc: conditionalize memfd and seals
    This fixes the Fedora 27 builds by providing fallback code for memfd_create().
    
    Signed-off-by: Tim Janik <timj@gnu.org>

--- tests/ipc.cc
+++ tests/ipc.cc
@@ -6,15 +6,64 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+static int
+add_seals (int fd)
+{
+  int retval = 0;
+#ifdef F_ADD_SEALS
+  const size_t seals = F_SEAL_GROW | F_SEAL_SHRINK | F_SEAL_SEAL; // F_SEAL_WRITE
+  retval = fcntl (fd, F_ADD_SEALS, seals);
+#endif
+  return retval;
+}
+
+static int
+open_memfd (ssize_t length)
+{
+  int fd = -1;
+#ifdef F_ADD_SEALS
+  fd = memfd_create ("BSEtests IPC memfd", MFD_ALLOW_SEALING);
+  if (fd >= 0)
+    {
+      if (ftruncate (fd, length) < 0)
+        {
+          int err = errno;
+          close (fd);
+          fd = -1;
+          errno = err;
+        }
+    }
+#else
+  // fd = open ("/dev/zero", O_RDONLY);
+  std::string filepath;
+  uint counter = 1;
+  do
+    filepath = Bse::string_format ("/tmp/.BSEtests IPC memfd%u.tmp", counter++);
+  while (access (filepath.c_str(), F_OK) >= 0);
+  fd = open (filepath.c_str(), O_CREAT | O_EXCL | O_TRUNC | O_RDWR, 0600);
+  if (fd >= 0)
+    {
+      if (lseek (fd, length - 1, SEEK_SET) < 0 ||
+          write (fd, "", 1) < 0 ||
+          unlink (filepath.c_str()) < 0)
+        {
+          int err = errno;
+          close (fd);
+          fd = -1;
+          errno = err;
+        }
+    }
+#endif
+  return fd;
+}
+
 IpcSharedMem*
 IpcSharedMem::create_shared (const std::string &data, int *fd, int64 count)
 {
   const size_t length = sizeof (IpcSharedMem) + data.size() + 1;
-  const size_t seals = F_SEAL_GROW | F_SEAL_SHRINK | F_SEAL_SEAL; // F_SEAL_WRITE
-  *fd = memfd_create ("BSEtests IPC memfd", MFD_ALLOW_SEALING);
+  *fd = open_memfd (length);
   if (*fd < 0 ||
-      ftruncate (*fd, length) < 0 ||
-      fcntl (*fd, F_ADD_SEALS, seals) < 0)
+      add_seals (*fd) < 0)
     die ("%s: memfd_create failed: %s", __func__, Bse::strerror (errno));
   void *mem = mmap (NULL, length, PROT_READ | PROT_WRITE, MAP_SHARED, *fd, 0);
   if (!mem)
@@ -28,19 +77,29 @@ IpcSharedMem::create_shared (const std::string &data, int *fd, int64 count)
   return sm;
 }
 
-IpcSharedMem*
-IpcSharedMem::acquire_shared (int fd)
+static int
+check_seals (int fd)
 {
-  errno = EBADF;
-  const ssize_t length = lseek (fd, 0, SEEK_END);
+#ifdef F_ADD_SEALS
   const ssize_t seals = fcntl (fd, F_GET_SEALS);
-  if (length < sizeof (IpcSharedMem) ||
-      seals < 0)
-    die ("%s: invalid memfd: %s", __func__, Bse::strerror (errno));
+  if (seals < 0)
+    return -1;
   errno = EPERM;
   if (!(seals & F_SEAL_SEAL) ||
       !(seals & F_SEAL_GROW) ||
       !(seals & F_SEAL_SHRINK))
+    return -1;
+#endif
+  return 0;
+}
+
+IpcSharedMem*
+IpcSharedMem::acquire_shared (int fd)
+{
+  errno = EBADF;
+  const size_t length = lseek (fd, 0, SEEK_END);
+  if (length < sizeof (IpcSharedMem) ||
+      check_seals (fd) < 0)
     die ("%s: invalid memfd: %s", __func__, Bse::strerror (errno));
   void *mem = mmap (NULL, length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
   if (!mem)
