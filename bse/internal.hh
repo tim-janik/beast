d3c33420dab7a8756083673069b85085a8eab01b
Tim Janik <timj@gnu.org>
2019-04-30 02:13:38 +0200

    BSE: internal: sort macros semi-alphabetically to ease browsing
    Signed-off-by: Tim Janik <timj@gnu.org>

--- bse/internal.hh
+++ bse/internal.hh
@@ -19,31 +19,29 @@ using Bse::String;
 /// Mark a string for translation, passed through verbatim by the preprocessor.
 #define N_(str)         (str)
 
-/// Produce a const char* string, wrapping @a str into C-style double quotes.
-#define CQUOTE(str)                                     BSE_CQUOTE(str)
+/// Constrain, apply, notify and implement a property change, the property name must equal `__func__`.
+#define APPLY_IDL_PROPERTY(lvalue, rvalue)      BSE_OBJECT_APPLY_IDL_PROPERTY(lvalue, rvalue)
 
-/// Create a Bse::StringVector, from a const char* C-style array.
-#define STRING_VECTOR_FROM_ARRAY(ConstCharArray)        BSE_STRING_VECTOR_FROM_ARRAY(ConstCharArray)
+/// Yield the number of C @a array elements.
+#define ARRAY_SIZE(array)               BSE_ARRAY_SIZE (array)
 
 /// Return from the current function if @a cond is unmet and issue an assertion warning.
 #define assert_return(cond, ...)        BSE_ASSERT_RETURN (cond, __VA_ARGS__)
-
 /// Return from the current function and issue an assertion warning.
 #define assert_return_unreached(...)    BSE_ASSERT_RETURN_UNREACHED (__VA_ARGS__)
 
+/// Produce a const char* string, wrapping @a str into C-style double quotes.
+#define CQUOTE(str)                                     BSE_CQUOTE(str)
+
 /// Hint to the compiler to optimize for @a cond == TRUE.
 #define ISLIKELY(cond)  BSE_ISLIKELY (cond)
-
 /// Hint to the compiler to optimize for @a cond == FALSE.
 #define UNLIKELY(cond)  BSE_UNLIKELY (cond)
 
 /// Return silently if @a cond does not evaluate to true with return value @a ...
 #define return_unless(cond, ...)        BSE_RETURN_UNLESS (cond, __VA_ARGS__)
 
-/// Yield the number of C @a array elements.
-#define ARRAY_SIZE(array)               BSE_ARRAY_SIZE (array)
-
-/// Constrain, apply, notify and implement a property change, the property name must equal `__func__`.
-#define APPLY_IDL_PROPERTY(lvalue, rvalue)      BSE_OBJECT_APPLY_IDL_PROPERTY(lvalue, rvalue)
+/// Create a Bse::StringVector, from a const char* C-style array.
+#define STRING_VECTOR_FROM_ARRAY(ConstCharArray)        BSE_STRING_VECTOR_FROM_ARRAY(ConstCharArray)
 
 #endif  // __BSE_INTERNAL_HH__
