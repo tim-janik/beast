84a532bd01f5542babf1a00ab946857f3487aea0
Tim Janik <timj@gnu.org>
2019-07-11 02:58:15 +0200

    EBEAST: vc/modaldialog.vue: reduce modal dialog wrappers
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ebeast/vc/modaldialog.vue
+++ ebeast/vc/modaldialog.vue
@@ -20,101 +20,93 @@
   ## CSS Classes:
   *vc-modaldialog-mask*
   : The *vc-modaldialog-mask* CSS class is used as backdrop beneath the dialog contents to mask non-modal contents.
-  *vc-modaldialog-container*
-  : The *vc-modaldialog-container* CSS class contains styling for the actual dialog contents.
+  *vc-modaldialog*
+  : The *vc-modaldialog* CSS class contains styling for the actual dialog contents.
 </docs>
 
 <style lang="scss">
   @import 'styles.scss';
-  .vc-modaldialog * { flex-shrink: 0; }
   @mixin vc-flex-vbox() {
-      display: flex; flex-basis: auto; align-items: center; justify-content: center;
-      flex-shrink: 0; flex-flow: column; height: auto;
+    display: flex; flex-basis: auto; align-items: center; justify-content: center;
+    flex-shrink: 0; flex-flow: column; height: auto;
   }
   @mixin vc-flex-hbox() {
-      display: flex; flex-basis: auto; align-items: center; justify-content: center;
-      flex-shrink: 0; flex-flow: row; width: auto;
+    display: flex; flex-basis: auto; align-items: center; justify-content: center;
+    flex-shrink: 0; flex-flow: row; width: auto;
   }
   .vc-modaldialog-mask {
-      @include vc-flex-vbox;
-      position: fixed; top: 0; left: 0; right: 0; bottom: 0;
-      z-index: 99998;
-      background-color: fadeout(darken($vc-theme-background, 15%), 15%); /* backdrop */
+    @include vc-flex-vbox;
+    position: fixed; top: 0; left: 0; right: 0; bottom: 0;
+    z-index: 99998;
+    background-color: fadeout(darken($vc-theme-background, 15%), 15%); /* backdrop */
   }
-  .vc-modaldialog-wrapper {
-      @include vc-flex-vbox;
-      max-height: 100%; max-width: 100%;
-      padding: 1em;
-  }
-  .vc-modaldialog-container {
-      @include vc-flex-vbox; flex-shrink: 1;
-      min-width: 25em; padding: 0;
-      background-color: $vc-theme-background;
-      border-radius: $vc-theme-border-radius;
-      border-top: 3px solid $vc-panel-background-light; border-left: 3px solid $vc-panel-background-light;
-      border-bottom: 3px solid $vc-panel-background-dark; border-right: 3px solid $vc-panel-background-dark;
-      @include vc-popup-box-shadow;
-      /* fix vscrolling: https://stackoverflow.com/a/33455342 */
-      justify-content: space-between;
-      margin: auto;
-      overflow: auto;
+  .vc-modaldialog {
+    $dist: 1em; z-index: 99999;
+    position: fixed; top: $dist; left: $dist; right: $dist; bottom: $dist;
+    max-height: 100%; max-width: 100%;
+    * { flex-shrink: 0; }
+    @include vc-flex-vbox; flex-shrink: 1;
+    min-width: 16em; padding: 0;
+    background-color: $vc-theme-background;
+    border-radius: $vc-theme-border-radius;
+    border-top: 3px solid $vc-panel-background-light; border-left: 3px solid $vc-panel-background-light;
+    border-bottom: 3px solid $vc-panel-background-dark; border-right: 3px solid $vc-panel-background-dark;
+    @include vc-popup-box-shadow;
+    /* fix vscrolling: https://stackoverflow.com/a/33455342 */
+    justify-content: space-between;
+    margin: auto;
+    overflow: auto;
+    transition: all .4s ease-out;
   }
   .vc-modaldialog-header {
-      font-size: 1.5em; font-weight: bold;
-      padding: 1rem 0 1.1rem;
+    font-size: 1.5em; font-weight: bold;
+    padding: 1rem 0 1.1rem;
   }
   .vc-modaldialog-header, .vc-modaldialog-footer {
-      display: block; width: 100%; align-items: center; text-align: center;
-      background-color: darken($vc-theme-background, 8%);
+    display: block; width: 100%; align-items: center; text-align: center;
+    background-color: darken($vc-theme-background, 8%);
   }
   .vc-modaldialog-footer {
-      padding: 1.2rem 0 1rem;
+    padding: 1.2rem 0 1rem;
   }
   .vc-modaldialog-body {
-      padding: 1.5em 2em;
+    padding: 1.5em 2em;
   }
   .vc-modaldialog-mask {
-      transition: all .4s ease;
-  }
-  .vc-modaldialog-container {
-      transition: all .4s ease-out;
+    transition: all .4s ease;
   }
   .vc-modaldialog-transition-enter {
-      opacity: 0;
+    opacity: 0;
   }
   .vc-modaldialog-transition-leave-active {
-      opacity: 0;
+    opacity: 0;
   }
-  .vc-modaldialog-transition-enter .vc-modaldialog-container
+  .vc-modaldialog-transition-enter .vc-modaldialog
   { transform: translateY(-100%) scale(.5); }
-  .vc-modaldialog-transition-leave-active .vc-modaldialog-container
+  .vc-modaldialog-transition-leave-active .vc-modaldialog
   { transform: translateY(-100%) scale(.5); }
 </style>
 
 <template>
   <transition name="vc-modaldialog-transition">
-    <div class="vc-modaldialog vc-modaldialog-mask" @click="close">
-      <div class="vc-modaldialog-wrapper">
-	<div class="vc-modaldialog-container" @click.stop ref="container">
+    <div class="vc-modaldialog" @click.stop ref="container">
 
-	  <div class="vc-modaldialog-header">
-	    <slot name="header">
-	      Modal Dialog
-	    </slot>
-	  </div>
-	  <div class="vc-modaldialog-body">
-	    <slot name="default"></slot>
-	  </div>
-	  <div class="vc-modaldialog-footer">
-	    <slot name="footer">
-	      <vc-button class="vc-modaldialog-button" ref="bclose" @click="close">
-		Close
-	      </vc-button>
-	    </slot>
-	  </div>
-
-	</div>
+      <div class="vc-modaldialog-header">
+	<slot name="header">
+	  Modal Dialog
+	</slot>
       </div>
+      <div class="vc-modaldialog-body">
+	<slot name="default"></slot>
+      </div>
+      <div class="vc-modaldialog-footer">
+	<slot name="footer">
+	  <vc-button class="vc-modaldialog-button" ref="bclose" @click="close">
+	    Close
+	  </vc-button>
+	</slot>
+      </div>
+
     </div>
   </transition>
 </template>
@@ -124,7 +116,9 @@ module.exports = {
   name: 'vc-modaldialog',
   mounted () {
     this.shield = Util.modal_shield (this.close, this.$refs['container']);
-    this.$refs.bclose.$el.focus();
+    const focus_close = true;
+    if (focus_close)
+      this.$refs.bclose.$el.focus();
   },
   beforeDestroy () {
     if (this.shield)
