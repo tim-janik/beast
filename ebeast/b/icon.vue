9e62cdaa0499b821f0b0deb2cd12cdbe514934f5
Tim Janik <timj@gnu.org>
2019-07-16 15:30:30 +0200

    EBEAST: b/icon.vue: add unicode symbols via :uc and material icons via :mi
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ebeast/b/icon.vue
+++ ebeast/b/icon.vue
@@ -7,6 +7,10 @@
   ## Props:
   *fa*
   : The name of a "Font Awesome" 4 icon, see the [Font Awesome Icons](https://fontawesome.com/v4.7.0/icons/).
+  *mi*
+  : The name of a "Material Icons" icon, see the [Material Design Icons](https://material.io/tools/icons/).
+  *uc*
+  : A unicode character literal, see the [Unicode symbols block list](https://en.wikipedia.org/wiki/Unicode_symbols#Symbol_block_list).
   *nosize*
   : Prevent the element from applying default size constraints.
   *fw*
@@ -18,24 +22,28 @@
 <style lang="scss">
   @import 'styles.scss';
   .b-icon {
-    margin: 0; padding: 0;
-    &.b-icon-dfl	{ height: 1em; width: 1em; height: 1em; }
-    &.b-icon-fw	{ height: 1em; width: 1.28571429em; text-align: center; }
-    &.b-icon-lg	{ font-size: 1.33333333em; line-height: 0.75em; vertical-align: -15%; }
+    margin: 0; padding: 0; text-align: center;
+    &.b-icon-dfl { height: 1em; width: 1em; }
+    &.b-icon-fw	 { height: 1em; width: 1.28571429em; text-align: center; }
+    &.b-icon-lg	 { font-size: 1.33333333em; line-height: 0.75em; vertical-align: -15%; }
+    &.material-icons.b-icon-dfl { font-size: 1em; }
   }
 </style>
 
 <template>
-  <svg class="b-icon" :class="iconclasses" role="icon" :aria-label="fa">
-    <use v-if="fa" :href="'assets/fa-sprites.svg#' + fa" />
+  <svg v-if="fa" class="b-icon" :class="iconclasses" role="icon" aria-hidden="true">
+    <use :href="'assets/fa-sprites.svg#' + fa" />
   </svg>
+  <i v-else-if="mi" class="b-icon material-icons" :class="iconclasses" role="icon" aria-hidden="true">{{ mi }}</i>
+  <span v-else-if="uc" class="b-icon" :class="iconclasses" role="icon" aria-hidden="true">{{ uc }}</span>
+  <span v-else-if="1" class="b-icon" :class="iconclasses" role="icon" aria-hidden="true"><slot /></span>
 </template>
 <!-- SVG-1.1 notation: <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="...svg"></use> -->
 
 <script>
 module.exports = {
   name: 'b-icon',
-  props: { 'fa': undefined,
+  props: { 'fa': undefined, 'mi': undefined, 'uc': undefined,
 	   'nosize': undefined, 'fw': undefined, 'lg': undefined },
   computed: {
     iconclasses() {
