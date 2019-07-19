79f9caf00e70309b9d162a692c9347bbe7442eca
Tim Janik <timj@gnu.org>
2019-07-20 00:59:29 +0200

    EBEAST: v8bse/Makefile.mk: silence excessive V8 deprecation warnings
    For v8pp upgrades, It is advisable to remove -UV8_DEPRECATION_WARNINGS and
    -UV8_IMMINENT_DEPRECATION_WARNINGS from cflags_cc.
    
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ebeast/v8bse/Makefile.mk
+++ ebeast/v8bse/Makefile.mk
@@ -38,6 +38,8 @@ $>/ebeast/v8bse/%.cc: ebeast/v8bse/%.cc		| $>/ebeast/v8bse/
 # in $HOME, it requires the electron target version to pick V8 headers and it calls $(MAKE) internally,
 # so we need to unset MAKEFLAGS, to unconfuse the node-gyp invocation of MAKE wrg jobserver setups.
 # See also: /usr/include/nodejs/common.gypi https://electronjs.org/docs/tutorial/using-native-node-modules
+# Note, V8 deprecation warnings can get a little excessive without constantly updating v8pp,
+# so we disable those and leave fixing deprecations to v8pp upgrades.
 $>/ebeast/v8bse/v8bse.node: EXTRA_INCLUDES ::= -I$> -I$>/ebeast/v8bse -Iexternal/v8pp/ $(GLIB_CFLAGS)
 $>/ebeast/v8bse/v8bse.node: $>/ebeast/v8bse/v8bse.cc $(ebeast/v8bse/cc.sources) $(lib/libbse.so) $>/ebeast/npm.rules
 	$(QECHO) 'SETUP' $>/ebeast/v8bse/binding.gyp
@@ -47,7 +49,8 @@ $>/ebeast/v8bse/v8bse.node: $>/ebeast/v8bse/v8bse.cc $(ebeast/v8bse/cc.sources) 
 	$Q echo "  'sources':      [ $(patsubst %, '%'$(,), $(ebeast/v8bse/gyp.inputs)) ],"	>>$@.tmp
 	$Q echo "  'cflags!':      [ $(patsubst %, '%'$(,), $(ebeast/v8bse/gyp.notflags)) ],"	>>$@.tmp
 	$Q echo "  'cflags_cc!':   [ $(patsubst %, '%'$(,), $(ebeast/v8bse/gyp.notflags)) ],"	>>$@.tmp
-	$Q echo "  'cflags_cc':    [ $(patsubst %, '%'$(,), $(ebeast/v8bse/gyp.cxxflags)) ],"	>>$@.tmp
+	$Q echo "  'cflags_cc':    [ $(patsubst %, '%'$(,), $(ebeast/v8bse/gyp.cxxflags))"	>>$@.tmp
+	$Q echo "         '-UV8_DEPRECATION_WARNINGS', '-UV8_IMMINENT_DEPRECATION_WARNINGS' ],"	>>$@.tmp
 	$Q echo "  'include_dirs': [ $(patsubst %, '%'$(,), $(ebeast_v8bse/gyp.incdirs)) ],"	>>$@.tmp
 	$Q echo "  'libraries':    [ $(patsubst %, '%'$(,), $(ebeast/v8bse/gyp.libs))"		>>$@.tmp
 	$Q echo "           \"'-Wl,-rpath,"'$$$$'"ORIGIN/$(ebeast/v8bse/rpath..libbse)'\" ],"	>>$@.tmp
