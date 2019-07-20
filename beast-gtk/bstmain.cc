1d77e247590823f350c9cef2faae0a64bd342575
Tim Janik <timj@gnu.org>
2019-07-21 01:35:52 +0200

    BEAST-GTK: bstmain: remove buildid everywhere and stick to Bse::version()
    Signed-off-by: Tim Janik <timj@gnu.org>

--- beast-gtk/bstmain.cc
+++ beast-gtk/bstmain.cc
@@ -186,7 +186,7 @@ main_init_gxk()
 		       Bse::string_format ("<b><big>BEAST</big></b>\n"
                                            "<b>The BSE Equipped Audio Synthesizer and Tracker</b>\n"
                                            "<b>Version %s</b>\n",
-                                           Bse::version_buildid()));
+                                           Bse::version()));
   bst_splash_update_entity (beast_splash, _("Startup"));
   bst_splash_show_grab (beast_splash);
 }
@@ -403,7 +403,7 @@ main_open_default_window ()
 static void
 main_show_release_notes ()
 {
-  if (BST_GCONFIG (rc_version) != Bse::version_buildid())
+  if (BST_GCONFIG (rc_version) != Bse::version())
     {
       const char *release_notes_title =
         "BEAST/BSE Release Notes";
@@ -412,7 +412,7 @@ main_show_release_notes ()
         "<tagdef name=\"title\" weight=\"bold\" scale=\"1.5\"/>"
         "<tagdef name=\"hyperlink\" underline=\"single\" foreground=\"#0000ff\"/>"
         "<tagdef name=\"mono\" family=\"monospace\"/>"
-        "<span tag=\"title\">BEAST/BSE __BEAST_INTERNAL_BUILDID__ Release Notes</span>"
+        "<span tag=\"title\">BEAST/BSE __BEAST_INTERNAL_VERSION__ Release Notes</span>"
         "<newline/><newline/>"
         "This development series of Beast focusses on improving interoperability and feature integration. "
         "<newline/><newline/>"
@@ -432,14 +432,14 @@ main_show_release_notes ()
         "<newline/>"
         "<span tag=\"mono\">        </span><span tag=\"hyperlink\"><xlink ref=\"http://beast.testbit.eu/\">http://beast.testbit.eu</xlink></span>"
         "</tag-span-markup>";
-      const String release_notes_contents = Bse::string_replace (release_notes_contents_tmpl, "__BEAST_INTERNAL_BUILDID__", Bse::version_buildid());
+      const String release_notes_contents = Bse::string_replace (release_notes_contents_tmpl, "__BEAST_INTERNAL_VERSION__", Bse::version());
       GtkWidget *sctext = gxk_scroll_text_create (GXK_SCROLL_TEXT_WRAP | GXK_SCROLL_TEXT_SANS | GXK_SCROLL_TEXT_CENTER, NULL);
       gxk_scroll_text_set_tsm (sctext, release_notes_contents.c_str());
       GtkWidget *rndialog = (GtkWidget*) gxk_dialog_new (NULL, NULL, GXK_DIALOG_DELETE_BUTTON, release_notes_title, sctext);
       gxk_dialog_set_sizes (GXK_DIALOG (rndialog), 320, 200, 540, 420);
       gxk_scroll_text_rewind (sctext);
       gxk_idle_show_widget (rndialog);
-      bst_gconfig_set_rc_version (Bse::version_buildid().c_str());
+      bst_gconfig_set_rc_version (Bse::version().c_str());
       force_saving_rc_files = true;
     }
 }
@@ -547,7 +547,7 @@ bst_args_parse_early (int *argc_p, char **argv)
       else if (strncmp (argv[i], "-:", 2) == 0)
 	{
 	  const gchar *flags = argv[i] + 2;
-	  printerr ("BEAST(%s): pid = %u\n", Bse::version_buildid(), getpid ());
+	  printerr ("BEAST(%s): pid = %u\n", Bse::version(), getpid ());
 	  if (strchr (flags, 'N') != NULL)
 	    {
 	      register_core_plugins = FALSE;
@@ -723,7 +723,7 @@ bst_exit_print_version (void)
 #endif
   String s;
   gchar *freeme = NULL;
-  printout ("BEAST version %s\n", Bse::version_buildid());
+  printout ("BEAST version %s\n", Bse::version());
   printout ("Libraries: ");
   printout ("GLib %u.%u.%u", glib_major_version, glib_minor_version, glib_micro_version);
   printout (", BSE %s", Bse::version());
@@ -736,7 +736,6 @@ bst_exit_print_version (void)
   printout (", GTK+ %u.%u.%u", gtk_major_version, gtk_minor_version, gtk_micro_version);
   if (bst_have_xkb())
     printout (", XKBlib");
-  printout (", GXK %s", Bse::version_buildid());
   printout ("\n");
   printout ("Compiled for %s %s SSE plugins.\n", Bse::cpu_arch(), with_mmx_sse ? "with" : "without");
   printout ("Intrinsic code selected according to runtime CPU detection:\n");
@@ -907,7 +906,7 @@ beast_show_about_box (void)
   if (!GTK_WIDGET_VISIBLE (beast_splash))
     {
       bst_splash_set_title (beast_splash, _("BEAST About"));
-      bst_splash_update_entity (beast_splash, Bse::string_format (_("BEAST Version %s"), Bse::version_buildid()));
+      bst_splash_update_entity (beast_splash, Bse::string_format (_("BEAST Version %s"), Bse::version()));
       bst_splash_update_item (beast_splash, _("Contributions made by:"));
       bst_splash_animate_strings (beast_splash, contributors);
     }
