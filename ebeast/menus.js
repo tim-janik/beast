0b3d27347bbc7316636bbef15016810a4a25729c
Tim Janik <timj@gnu.org>
2019-07-22 00:01:37 +0200

    EBEAST: menus.js: print loading error to console
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ebeast/menus.js
+++ ebeast/menus.js
@@ -155,6 +155,8 @@ function menu_command (menuitem)
 						save_same_filename = result[0];
 						check_all_menu_items();
 					      }
+					    else
+					      console.error ('Failed to load:', result[0]);
 					  }
 				      });
       break;
