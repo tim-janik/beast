e81b9da4634c574149fb0865c48a7a74c355850d
Tim Janik <timj@gnu.org>
2019-07-25 14:34:42 +0200

    BSE: add beast-sound-engine.cc to build lib/BeastSoundEngine executable
    Signed-off-by: Tim Janik <timj@gnu.org>

--- bse/Makefile.mk
+++ bse/Makefile.mk
@@ -351,6 +351,12 @@ bse/include.headerdir		::= $(pkglibdir)/include/bse
 bse/include.headers		::= $(bse/libbse.headers) $(bse/libbse.deps) $(bse/include.idls)
 bse/all: $(lib/libbse.so)
 
+# == BeastSoundEngine defs ==
+lib/BeastSoundEngine		::= $>/lib/BeastSoundEngine
+bse/BeastSoundEngine.sources	::= bse/beast-sound-engine.cc
+bse/BeastSoundEngine.objects	::= $(call BUILDDIR_O, $(bse/BeastSoundEngine.sources))
+bse/all: $(lib/BeastSoundEngine)
+
 # == bseprocidl defs ==
 bse/bseprocidl			::= $>/bse/bseprocidl
 bse/bseprocidl.sources		::= bse/bseprocidl.cc
@@ -488,6 +494,16 @@ int main (int argc, char *argv[]) {
   return 0;
 }
 endef
+
+# == BeastSoundEngine ==
+$(bse/BeastSoundEngine.objects):	$(bse/libbse.deps)
+$(bse/BeastSoundEngine.objects):	EXTRA_INCLUDES ::= -I$> $(GLIB_CFLAGS)
+$(call BUILD_PROGRAM, \
+	$(lib/BeastSoundEngine), \
+	$(bse/BeastSoundEngine.objects), \
+	$(lib/libbse.so), \
+	-lbse-$(VERSION_MAJOR) $(GLIB_LIBS), \
+	../lib)
 
 # == bseprocidl rules ==
 $(bse/bseprocidl.objects):	$(bse/libbse.deps)
---
+++ bse/beast-sound-engine.cc
@@ -0,0 +1,56 @@
+// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
+#include <bse/bse.hh>
+
+static void
+print_usage (bool help)
+{
+  if (!help)
+    {
+      Bse::printout ("beast-sound-engine version %s\n", Bse::version());
+      return;
+    }
+  Bse::printout ("Usage: beast-sound-engine [OPTIONS]\n");
+  Bse::printout ("  --help     Print command line help\n");
+  Bse::printout ("  --version  Print program version\n");
+}
+
+int
+main (int argc, char *argv[])
+{
+  Bse::init_async (&argc, argv, argv[0]); // Bse::cstrings_to_vector (NULL)
+
+  // parse arguments
+  bool seen_dashdash = false;
+  std::vector<std::string> words;
+  for (size_t i = 0; i < argc; i++)
+    if (!argv[i])
+      continue;
+    else if (argv[i][0] == '-' && argv[i][1] == '-' && argv[i][2] == 0)
+      seen_dashdash = true;
+    else if (!seen_dashdash && argv[i][0] == '-')
+      {
+        const char *arg = argv[i] + 1 + (argv[i][1] == '-');
+        const char *eq = strchr (arg, '=');
+        const std::string arg_name = !eq ? arg : std::string (arg, eq - arg);
+        if (arg_name == "version")
+          {
+            print_usage (false);
+            return 0;
+          }
+        else if (arg_name == "h" || arg_name == "help")
+          {
+            print_usage (true);
+            return 0;
+          }
+        else
+          {
+            Bse::printerr ("%s: invalid argument: %s\n", argv[0], argv[i]);
+            print_usage (true);
+            return 1;
+          }
+      }
+    else
+      words.push_back (argv[i]);
+
+  return 0;
+}
