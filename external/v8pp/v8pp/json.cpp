772aeb5bb2421d6a4d1eff6d9e6772fa4dfc7afd
Tim Janik <timj@gnu.org>
2019-08-09 20:09:28 +0200

    EXTERNAL: v8pp: remove (now unused) v8pp/ project
    Signed-off-by: Tim Janik <timj@gnu.org>

--- external/v8pp/v8pp/json.cpp
+++
@@ -1,5 +0,0 @@
-#include "v8pp/config.hpp"
-
-#if !V8PP_HEADER_ONLY
-#include "v8pp/json.ipp"
-#endif
--- external/v8pp/v8pp/throw_ex.cpp
+++
@@ -1,5 +0,0 @@
-#include "v8pp/config.hpp"
-
-#if !V8PP_HEADER_ONLY
-#include "v8pp/throw_ex.ipp"
-#endif
