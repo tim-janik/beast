c62cbf00a331ed44ab516b787185874cce95c041
Tim Janik <timj@gnu.org>
2019-09-13 19:25:46 +0200

    EBEAST: b/preferencesdialog.vue: move to await and observable_from_getters()
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ebeast/b/preferencesdialog.vue
+++ ebeast/b/preferencesdialog.vue
@@ -19,26 +19,33 @@
   <b-modaldialog class="b-preferencesdialog" @close="$emit ('close')">
     <div slot="header">BEAST Preferences</div>
     <slot></slot>
-    <b-fed-object :ref="fedobject" :value="preferences()" :default="defaults()" :readonly="locked()" @input="value_changed" debounce=500>
+    <b-fed-object :ref="fedobject" :value="prefdata" :default="defaults" :readonly="locked" @input="value_changed" debounce=500>
     </b-fed-object>
   </b-modaldialog>
 </template>
 
 <script>
+function component_data () {
+  const data = {
+    defaults: { getter: async c => Object.freeze (await Bse.server.get_config_defaults()), },
+    locked:   { getter: async c => Bse.server.locked_config(), },
+    prefdata: { getter: async c => {
+      const d = await Bse.server.get_config();
+      if (d.__typename__)
+	d.__typedata__ = await Bse.server.find_typedata (d.__typename__);
+      return Object.freeze (d);
+    }, },
+  };
+  return this.observable_from_getters (data, () => true);
+}
+
 module.exports = {
   name: 'b-preferencesdialog',
+  mixins: [ Util.vue_mixins.dom_updates, Util.vue_mixins.hyphen_props ],
+  data() { return component_data.call (this); },
   methods: {
-    defaults() {
-      return Bse.server.get_config_defaults();
-    },
-    locked() {
-      return Bse.server.locked_config();
-    },
-    preferences() {
-      return Bse.server.get_config();
-    },
-    value_changed (po) {
-      const prefs = Bse.server.get_config();
+    async value_changed (po) {
+      const prefs = await Bse.server.get_config();
       Util.assign_forin (prefs, po);
       Bse.server.set_config (prefs);
     },
