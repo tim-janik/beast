82ca6a2716d09666f500033dbe990eac02639eed
Tim Janik <timj@gnu.org>
2019-09-15 23:23:05 +0200

    DRIVERS: remove old PCM device code
    Signed-off-by: Tim Janik <timj@gnu.org>

--- drivers/Makefile.mk
+++ drivers/Makefile.mk
@@ -3,11 +3,6 @@ include $(wildcard $>/drivers/*.d)
 CLEANDIRS += $(wildcard $>/drivers/)
 drivers/rpath..libbse ::= ../lib
 
-# == alsapcm defs ==
-drivers/alsapcm.so		::= $>/drivers/alsapcm.so
-drivers/alsapcm.sources		::= drivers/bsepcmdevice-alsa.cc
-drivers/alsapcm.objects		::= $(call BUILDDIR_O, $(drivers/alsapcm.sources))
-
 # == alsamidi defs ==
 drivers/alsamidi.so		::= $>/drivers/alsamidi.so
 drivers/alsamidi.sources	::= drivers/bsemididevice-alsa.cc
@@ -16,17 +11,7 @@ drivers/alsamidi.objects	::= $(call BUILDDIR_O, $(drivers/alsamidi.sources))
 # == install rules ==
 $(call INSTALL_BIN_RULE, plugins/drivers, \
 	$(DESTDIR)$(pkglibdir)/plugins, \
-	$(drivers/alsapcm.so) $(drivers/alsamidi.so))
-
-# == alsapcm rules ==
-$(drivers/alsapcm.objects): $(bse/libbse.deps) | $>/drivers/
-$(drivers/alsapcm.objects): EXTRA_INCLUDES ::= -I$> $(GLIB_CFLAGS)
-$(call BUILD_SHARED_LIB, \
-	$(drivers/alsapcm.so), \
-	$(drivers/alsapcm.objects), \
-	$(lib/libbse.so) | $>/drivers/, \
-	-lbse-$(VERSION_MAJOR) $(BSEDEPS_LIBS) $(ALSA_LIBS), \
-	$(drivers/rpath..libbse))
+	$(drivers/alsamidi.so))
 
 # == alsamidi rules ==
 $(drivers/alsamidi.objects): $(bse/libbse.deps) | $>/drivers/
