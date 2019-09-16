a035ca7d4ea0aad57162a9756125431d3bfa5296
Tim Janik <timj@gnu.org>
2019-09-16 14:02:03 +0200

    DRIVERS: remove old MIDI device code
    Signed-off-by: Tim Janik <timj@gnu.org>

--- drivers/Makefile.mk
+++ drivers/Makefile.mk
@@ -2,23 +2,3 @@
 include $(wildcard $>/drivers/*.d)
 CLEANDIRS += $(wildcard $>/drivers/)
 drivers/rpath..libbse ::= ../lib
-
-# == alsamidi defs ==
-drivers/alsamidi.so		::= $>/drivers/alsamidi.so
-drivers/alsamidi.sources	::= drivers/bsemididevice-alsa.cc
-drivers/alsamidi.objects	::= $(call BUILDDIR_O, $(drivers/alsamidi.sources))
-
-# == install rules ==
-$(call INSTALL_BIN_RULE, plugins/drivers, \
-	$(DESTDIR)$(pkglibdir)/plugins, \
-	$(drivers/alsamidi.so))
-
-# == alsamidi rules ==
-$(drivers/alsamidi.objects): $(bse/libbse.deps) | $>/drivers/
-$(drivers/alsamidi.objects): EXTRA_INCLUDES ::= -I$> $(GLIB_CFLAGS)
-$(call BUILD_SHARED_LIB, \
-	$(drivers/alsamidi.so), \
-	$(drivers/alsamidi.objects), \
-	$(lib/libbse.so) | $>/drivers/, \
-	-lbse-$(VERSION_MAJOR) $(BSEDEPS_LIBS) $(ALSA_LIBS), \
-	$(drivers/rpath..libbse))
