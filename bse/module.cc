493fc12b9e268a43a90a6634488ce6001a1f7b23
Tim Janik <timj@gnu.org>
2019-10-16 22:22:07 +0200

    BSE: implement module listing and creation
    Signed-off-by: Tim Janik <timj@gnu.org>

--- bse/module.cc
+++ bse/module.cc
@@ -1,5 +1,6 @@
 // Licensed GNU LGPL v2.1 or later: http://www.gnu.org/licenses/lgpl.html
 #include "module.hh"
+#include "internal.hh"
 
 namespace Bse {
 
@@ -9,6 +10,22 @@ ModuleImpl::ModuleImpl (const String &module_type) :
 
 ModuleImpl::~ModuleImpl ()
 {}
+
+ModuleTypeInfo
+ModuleImpl::module_type_info (const String &module_id)
+{
+  ModuleTypeInfo info;
+  info.id = module_id;
+  return info;
+}
+
+StringSeq
+ModuleImpl::list_module_types ()
+{
+  StringSeq seq;
+  seq.push_back ("Dumb");
+  return seq;
+}
 
 String
 ModuleImpl::get_module_type ()
--- bse/module.hh
+++ bse/module.hh
@@ -6,15 +6,19 @@
 
 namespace Bse {
 
-class ModuleImpl : public virtual ModuleIface {
+class ModuleImpl : public ObjectImpl, public virtual ModuleIface {
   const String module_type_;
 protected:
+  friend class           FriendAllocator<ModuleImpl>;
   virtual               ~ModuleImpl       ();
 public:
   explicit               ModuleImpl       (const String &module_type);
   virtual String         get_module_type  () override;
   virtual ModuleTypeInfo module_type_info () override;
+  static ModuleTypeInfo  module_type_info  (const String &module_id);
+  static StringSeq       list_module_types ();
 };
+using ModuleImplP = std::shared_ptr<ModuleImpl>;
 
 } // Bse
 
