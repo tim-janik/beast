f26e60c1f1e0e8b764ec553e9e4b314a5c619c17
Tim Janik <timj@gnu.org>
2019-10-25 00:20:06 +0200

    TOOLS: magictest.cc: print Bse::FileMagic matches and find .bse files
    Signed-off-by: Tim Janik <timj@gnu.org>

--- tools/magictest.cc
+++ tools/magictest.cc
@@ -22,6 +22,8 @@ magic_test (const ArgParser &ap)
     { "Bourne shell script text",		"0,string,#!/bin/sh", },
     { "Bourne shell script text",		"0,string,#!\\ /bin/sh", },
     { "Bourne shell script text",		"0,string,#!\\t/bin/sh", },
+    { "Bourne shell script text",		"0,string,#!/usr/bin/env bash", },
+    { "Bourne shell script text",		"0,string,#!/usr/bin/env\tbash", },
     { "GIF image data",				"0,string,GIF8", },
     { "X window image dump (v7)",		("# .xwd files\n"
 						 "0x04,ubelong,0x0000007"), },
@@ -35,6 +37,16 @@ magic_test (const ArgParser &ap)
   SfiRing *magic_list = NULL;
   gboolean test_open = FALSE;
   /* initialization */
+  FileMagic::register_magic (".bse", "0 string ; Bse", "BseProject file");
+  FileMagic::register_magic (".bse",
+                             "0  string  PK\003\004\n"
+                             "8  short   0x0000\n" // uncompressed first entry
+                             "26 leshort =8\n"     // file name length
+                             "28 leshort 0x0000\n" // no extra field
+                             "30 string mimetypeapplication/x-bsePK",
+                             "Bse::Project file (application/x-bse; zipped)");
+  for (i = 0; i < n_magic_presets; i++)
+    FileMagic::register_magic ("", magic_presets[i][1], magic_presets[i][0]);
   for (i = 0; i < n_magic_presets; i++)
     magic_list = sfi_ring_append (magic_list,
 				  gsl_magic_create ((void*) magic_presets[i][0],
@@ -48,6 +60,7 @@ magic_test (const ArgParser &ap)
     {
       BseLoader *loader = bse_loader_match (file_name.c_str());
       GslMagic *magic = gsl_magic_list_match_file (magic_list, file_name.c_str());
+      String magic_desc = Bse::FileMagic::match_magic (file_name);
       const uint l = file_name.size();
       char *pad;
 
@@ -55,13 +68,15 @@ magic_test (const ArgParser &ap)
       pad = g_strnfill (MAX (40, l) - l, ' ');
       printout ("%s", pad);
       g_free (pad);
-      if (!magic && !loader)
+      if (!magic && !loader && magic_desc.empty())
         printout (" no magic/loader found");
       else
         {
+          if (!magic_desc.empty())
+            printout (" %s", magic_desc);
           if (magic)
             printout (" MAGIC: %s", (char*) magic->data);
-          if (loader)
+          if (loader && test_open)
             {
               if (test_open)
                 {
