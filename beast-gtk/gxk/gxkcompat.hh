db4921517830600d959afa2cdac661ee648db535
Tim Janik <timj@gnu.org>
2019-11-07 20:01:26 +0100

    BEAST-GTK: gxk/: purge unused beast-gtk/gxk directory
    Signed-off-by: Tim Janik <timj@gnu.org>

--- beast-gtk/gxk/gxkcompat.hh
+++
@@ -1,9 +0,0 @@
-// Licensed GNU LGPL v2.1 or later: http://www.gnu.org/licenses/lgpl.html
-#ifndef __GXK_COMPAT_HH__
-#define __GXK_COMPAT_HH__
-
-#include <gxk/gxkglobals.hh>
-
-GdkPixbuf* gxk_pixbuf_new_from_inline (int data_length, const guint8 *data, gboolean copy_pixels, GError **error);
-
-#endif /* __GXK_COMPAT_HH__ */
