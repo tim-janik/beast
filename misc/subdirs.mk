320be01346868eb47e26bf7c6e5f7ade6b39df92
Tim Janik <timj@gnu.org>
2019-11-12 15:43:13 +0100

    MISC: subdirs.mk: support 'make check somefile.o' in subdirs without prefix
    Signed-off-by: Tim Janik <timj@gnu.org>

--- misc/subdirs.mk
+++ misc/subdirs.mk
@@ -4,8 +4,22 @@
 misc/subdirs_mk/topdir ::= $(abspath $(dir $(lastword $(MAKEFILE_LIST)))/..)
 
 # extract subdir name from $(misc/subdirs_mk/topdir)/sub/dir as sub-dir
-misc/subdirs_mk/subdir_prefix ::= $(patsubst $(misc/subdirs_mk/topdir)/%, %, $(abspath .))
+misc/subdirs_mk/subdir_prefix ::= $(patsubst $(misc/subdirs_mk/topdir)/%,%, $(abspath .))
 
+# Read default config, in particular $(builddir)
+-include $(misc/subdirs_mk/topdir)/config-defaults.mk
+builddir ?= $(O)
+builddir ?= out
+
+# Fallback to 'make all' in topdir if MAKECMDGOALS is empty
 MAKECMDGOALS ?= all
-${MAKECMDGOALS}:
-	@exec $(MAKE) -C $(misc/subdirs_mk/topdir) $(misc/subdirs_mk/subdir_prefix)/$@
+
+# Prefix filenames in MAKECMDGOALS
+sed_subdirprefix ::= 's! \([^./ ]\+\.[^/ ]\+\)! $(builddir)/$(misc/subdirs_mk/subdir_prefix)/\1!g'
+PREFIXED_MAKECMDGOALS != echo " $(MAKECMDGOALS)" | sed $(sed_subdirprefix)
+
+# Trigger a single recursive $(MAKE) call
+.PHONY: $(MAKECMDGOALS) .._todir_MAKECMDGOALS
+$(MAKECMDGOALS): .._todir_MAKECMDGOALS ; @:
+.._todir_MAKECMDGOALS:
+	@exec $(MAKE) -C $(misc/subdirs_mk/topdir) $(PREFIXED_MAKECMDGOALS)
