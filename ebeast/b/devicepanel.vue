2a413fa955c9f97567bf5130eaba5e2b16592457
Tim Janik <timj@gnu.org>
2019-12-15 02:25:51 +0100

    EBEAST: b/devicepanel.vue: add a `[+]` indicator for adding more devices
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ebeast/b/devicepanel.vue
+++ ebeast/b/devicepanel.vue
@@ -16,11 +16,12 @@
 </style>
 
 <template>
-  <b-hflex class="b-devicepanel" style="width: 100%; height: 100%" @contextmenu.stop="menuopen" >
+  <b-hflex class="b-devicepanel" style="width: 100%; height: 100%" >
     <b-deviceeditor v-for="device in devices" :key="device.$id"
 		    :device="device" center style="margin: 5px" >
       {{ device.$id }}
     </b-deviceeditor>
+    <b-more @click.native.stop="menuopen" />
     <b-contextmenu ref="cmenu" @click="menuactivation" >
       <b-menutitle> Device </b-menutitle>
       <b-menuitem fa="plus-circle"      role="add-device" >      Add Device		</b-menuitem>
