ea63382492e99c28e6138d0d377583bea3e29767
Tim Janik <timj@gnu.org>
2019-12-23 01:51:50 +0100

    EBEAST: babel.config.js: prepare babel to parse EcmaScript proposals
    Signed-off-by: Tim Janik <timj@gnu.org>

---
+++ ebeast/babel.config.js
@@ -0,0 +1,26 @@
+module.exports = (api) => {
+  api.cache.never();
+
+  const presets = [
+    [ '@babel/preset-env', {
+      targets: {
+	"browsers": [
+	  "ff 68",
+	  "chrome 76",
+	], },
+    } ],
+  ];
+
+  const plugins = [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-object-rest-spread',
+  ];
+
+  return {
+    presets,
+    plugins,
+  };
+};
