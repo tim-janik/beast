77ad03157052706ef814e8190c71aa471e962b97
Tim Janik <timj@gnu.org>
2019-12-25 03:54:36 +0100

    EBEAST: add and enable @babel/plugin-proposal-numeric-separator
    Using '_' as numeric separator is implemented in V8 v7.5.
    
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ebeast/babel.config.js
+++ ebeast/babel.config.js
@@ -15,9 +15,10 @@ module.exports = (api) => {
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-numeric-separator',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-optional-chaining',
   ];
 
   return {
