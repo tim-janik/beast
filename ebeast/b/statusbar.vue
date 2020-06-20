f8123a457986e59cf4ee0b0b731a739403fd74e7
Tim Janik <timj@gnu.org>
2020-06-20 03:16:36 +0200

    EBEAST: b/statusbar.vue: add statusbar theming
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ebeast/b/statusbar.vue
+++ ebeast/b/statusbar.vue
@@ -8,40 +8,47 @@
 <style lang="scss">
   @import 'mixins.scss';
 
-  $status-spacing: 0.5em;
   .b-statusbar {
     display: flex; justify-content: space-between; white-space: nowrap;
-    padding: 0 $status-spacing; margin: 5px 0;
+    padding: 0 $b-statusbar-field-spacing; margin: 5px 0;
     user-select: none;
     .b-statusbar-field {
       display: flex; flex-wrap: nowrap; flex-shrink: 0; flex-grow: 0; white-space: nowrap;
     }
     .b-statusbar-text {
-      display: inline; overflow: hidden; flex-shrink: 1; flex-grow: 1; white-space: nowrap;
-      margin-left: $status-spacing * 2;
+      display: inline-block; overflow-y: visible; //* avoid scrolling */
+      overflow-x: hidden; white-space: nowrap;
+      flex-shrink: 1; flex-grow: 1;
+      margin-left: $b-statusbar-field-spacing * 2;
     }
     .b-statusbar-spacer {
       display: inline; flex-shrink: 1; flex-grow: 0; white-space: nowrap;
       text-align: center;
-      margin: 0 $status-spacing;
-      border-left: 1px solid darker($b-main-foreground, 50%);
+      margin: 0 $b-statusbar-field-spacing;
+      border-left: 1px solid $b-statusbar-separator-shade;
       width: 1px;
     }
     .b-icon {
       align-items: center;
-      padding: 0 $status-spacing;
-      filter: brightness(0.8);
-      &:hover {
-	filter: brightness(1.2);
+      padding: 0 $b-statusbar-field-spacing;
+      filter: brightness(asfactor($b-statusbar-icon-brightness));
+      &:hover:not(.b-active) {
+	filter: brightness(1 / asfactor($b-statusbar-icon-brightness));
 	transform: scale(1.1);
+      }
+      &.b-active {
+	color: $b-color-active;
       }
     }
 
     /* markdown styling for statusbar */
-    .b-markdown-it-outer {
+    .b-statusbar-text { /* .b-markdown-it-outer */
       @include b-markdown-it-inlined;
-      color: darker($b-main-foreground, 35%);
-      * { padding: 0; margin: 0; font-size: inherit; white-space: nowrap; }
+      color: $b-statusbar-text-shade;
+      * {
+	display: inline-block; overflow-y: visible; //* avoids scrolling */
+	padding: 0; margin: 0; font-size: inherit; white-space: nowrap;
+      }
       strong { color: $b-main-foreground; font-weight: normal; padding: 0 0.5em; }
     }
   }
@@ -50,14 +57,19 @@
 <template>
   <b-hflex class="b-statusbar" >
     <span class="b-statusbar-field" >
-      <b-icon fa="tasks" style="font-size:110%;" hflip data-tip="**CLICK** Show Devices" />
-      <b-icon mi="queue_music" style="font-size:140%" data-tip="**CLICK** Show Piano Roll Editor" />
+      <b-icon fa="tasks" style="font-size:110%;" hflip :class="App.panel2 == 'd' && 'b-active'"
+	      @click.native="App.switch_panel2 ('d')"
+	      data-tip="**CLICK** Show Device Stack" />
+      <b-icon mi="queue_music" style="font-size:140%" :class="App.panel2 == 'p' && 'b-active'"
+	      @click.native="App.switch_panel2 ('p')"
+	      data-tip="**CLICK** Show Piano Roll Editor" />
     </span>
     <span class="b-statusbar-spacer" />
     <span class="b-statusbar-text" ref="b-statusbar-text" />
     <span class="b-statusbar-spacer" />
     <span class="b-statusbar-field" >
-      <b-icon fa="info-circle" style="font-size:120% ; color: #71cff2" data-tip="**CLICK** Show Information View" />
+      <b-icon fa="info-circle" style="font-size:120%"
+	      data-tip="**CLICK** Show Information View" />
       <b-icon mi="folder_open" style="font-size:120%" data-tip="**CLICK** Show Browser" />
     </span>
   </b-hflex>
