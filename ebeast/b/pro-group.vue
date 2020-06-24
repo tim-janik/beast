7482492e557bfd3e5336e88e04a50ccfcbc48928
Tim Janik <timj@gnu.org>
2020-06-25 01:55:36 +0200

    EBEAST: b/pro-group.vue: layout properties into grid rows and cells
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ebeast/b/pro-group.vue
+++ ebeast/b/pro-group.vue
@@ -15,7 +15,24 @@
 <style lang="scss">
   @import 'mixins.scss';
   .b-pro-group        {
-    display: flex; justify-content: flex-start;
+    display: grid;
+    padding: 5px; grid-gap: 5px;
+    justify-content: flex-start;
+    border-radius: $b-device-radius;
+    margin: 0 0 3px 3px;
+    background: $b-device-area1;
+    &:nth-child(2n) {
+      background: $b-device-area2;
+    }
+    .b-pro-group-title {
+      justify-self: center;
+    }
+    .b-pro-group-vprop {
+      align-items: center;
+    }
+    .b-pro-group-nick {
+      font-size: 90%;
+    }
     .b-pro-group-numeric {
       /*max-height: 2em;*/
     }
@@ -23,14 +40,40 @@
 </style>
 
 <template>
-  <b-hflex class="b-pro-group tabular-nums" :data-bubble="name" >
-    <b-pro-input v-for="tp in ptypes" :key="tp.prop.$id" :prop="tp.prop"
-		 :class="ptype_class (tp)" :labeled="false"
-		 :readonly="readonly" />
-  </b-hflex>
+  <div class="b-pro-group tabular-nums" >
+    <span class="b-pro-group-title" :style="`grid-column: 1 / ${maxcols}`" > {{ name }} </span>
+    <b-vflex class="b-pro-group-vprop" v-for="tp in ptypes" :key="tp.prop.$id" :style="tp.style" >
+      <b-pro-input :prop="tp.prop"
+		   :class="ptype_class (tp)" :labeled="false"
+		   :readonly="readonly" />
+      <span class="b-pro-group-nick" >
+	{{ tp.nick }}
+      </span>
+    </b-vflex>
+  </div>
 </template>
 
 <script>
+function prop_visible (nick, is_numeric, hints) {
+  hints = ':' + hints + ':';
+  if (hints.search (/:G:/) < 0)
+    return false;
+  return true;
+}
+
+function prop_row (idx, count) {
+  let nrows = 1;
+  if (count > 8)
+    nrows = 2;
+  if (count > 16)
+    nrows = 3;
+  if (count > 24)
+    nrows = 4;
+  const run = Math.ceil (count / nrows);
+  let r = Math.trunc (idx / run);
+  r += 2; // first row is the title
+  return r;
+}
 
 async function property_types (props) {
   props = await props;
@@ -38,29 +81,49 @@ async function property_types (props) {
   const promises = [];
   for (const p of props)
     {
-      promises.push (p.is_numeric());	// ①
-      promises.push (p.hints()); 	// ②
+      promises.push (p.is_numeric());		// ①
+      promises.push (p.hints()); 		// ②
+      promises.push (p.nick()); 		// ③
     }
   const results = await Promise.all (promises);
-  console.assert (results.length == 2 * props.length);
-  // assign ptypes
+  console.assert (results.length == 3 * props.length);
+  // assign ptypes, count UI elements
   const ptypes = [];
   for (let i = 0; i < props.length; i++)
     {
+      const is_numeric = results[i * 3 + 0];	// ①
+      const hints = results[i * 3 + 1];		// ②
+      const nick = results[i * 3 + 2];		// ③
+      const visible = prop_visible (nick, is_numeric, hints);
+      if (!visible)
+	continue;
       const type = {
-	is_numeric: results[i * 2 + 0],	// ①
-	hints:      results[i * 2 + 1],	// ②
+	is_numeric, hints, nick,
 	prop:	    props[i],
+	style:      '',
       };
       ptypes.push (type);
     }
+  // layout visible props
+  const cols = {};
+  for (let i = 0; i < ptypes.length; i++)
+    {
+      const type = ptypes[i];
+      const row = prop_row (i, ptypes.length);
+      const c = cols[row] || 1;
+      cols[row] = c + 1;
+      cols.max = Math.max (cols.max | 0, cols[row]);
+      type.style = `grid-row: ${row}; grid-column: ${c};`;
+    }
   // return list
+  this.maxcols = cols.max;
   return Object.freeze (ptypes); // [ { prop, is_numeric, hints }… ]
 }
 
 function pro_group_data () {
   const data = {
-    ptypes:	{ default: [], getter: async c => property_types (this.props), },
+    ptypes:	{ default: [], getter: async c => property_types.call (this, this.props), },
+    maxcols:    { default: 1, },
   };
   return this.observable_from_getters (data, () => this.props);
 }
