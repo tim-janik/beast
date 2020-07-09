fbdad304e611462336dd5249c2caa213e3787829
Tim Janik <timj@gnu.org>
2020-07-09 18:03:17 +0200

    DEVICES: blepsynth/blepsynth.cc: fix add_param() parameters
    Signed-off-by: Tim Janik <timj@gnu.org>

--- devices/blepsynth/blepsynth.cc
+++ devices/blepsynth/blepsynth.cc
@@ -254,49 +254,49 @@ class BlepSynth : public AudioSignal::Processor {
 
     auto oscparams = [&] (int o) {
       start_param_group (string_format ("Oscillator %d", o + 1));
-      osc_params[o].shape = add_param (string_format ("Osc %d Shape", o + 1), "Shape", -100, 100, "G:big", 0, "%");
-      osc_params[o].pulse_width = add_param (string_format ("Osc %d Pulse Width", o + 1), "P.W", 0, 100, "G:big", 50, "%");
-      osc_params[o].sub = add_param (string_format ("Osc %d Subharmonic", o + 1), "Sub", 0, 100, "G:big", 0, "%");
-      osc_params[o].sub_width = add_param (string_format ("Osc %d Subharmonic Width", o + 1), "Sub.W", 0, 100, "G:big", 50, "%");
-      osc_params[o].sync = add_param (string_format ("Osc %d Sync Slave", o + 1), "Sync", 0, 60, "G:big", 0, "semitones");
+      osc_params[o].shape = add_param (string_format ("Osc %d Shape", o + 1), "Shape", -100, 100, 0, "%");
+      osc_params[o].pulse_width = add_param (string_format ("Osc %d Pulse Width", o + 1), "P.W", 0, 100, 50, "%");
+      osc_params[o].sub = add_param (string_format ("Osc %d Subharmonic", o + 1), "Sub", 0, 100, 0, "%");
+      osc_params[o].sub_width = add_param (string_format ("Osc %d Subharmonic Width", o + 1), "Sub.W", 0, 100, 50, "%");
+      osc_params[o].sync = add_param (string_format ("Osc %d Sync Slave", o + 1), "Sync", 0, 60, 0, "semitones");
 
       /* TODO: unison_voices property should be an integer property, range 1-16, default 1 */
-      osc_params[o].unison_voices = add_param (string_format ("Osc %d Unison Voices", o + 1), "Voices", 0, 100, "G:big", 0, "%");
-      osc_params[o].unison_detune = add_param (string_format ("Osc %d Unison Detune", o + 1), "Detune", 0.5, 50, "G:big", 6, "%");
-      osc_params[o].unison_stereo = add_param (string_format ("Osc %d Unison Stereo", o + 1), "Stereo", 0, 100, "G:big", 0, "%");
+      osc_params[o].unison_voices = add_param (string_format ("Osc %d Unison Voices", o + 1), "Voices", 0, 100, 0, "%");
+      osc_params[o].unison_detune = add_param (string_format ("Osc %d Unison Detune", o + 1), "Detune", 0.5, 50, 6, "%");
+      osc_params[o].unison_stereo = add_param (string_format ("Osc %d Unison Stereo", o + 1), "Stereo", 0, 100, 0, "%");
     };
 
     oscparams (0);
 
     start_param_group ("Filter");
     /* TODO: cutoff property should have logarithmic scaling */
-    pid_cutoff_ = add_param ("Cutoff", "Cutoff", 20, 24000, "G:big", 1000, "Hz");
-    pid_resonance_ = add_param ("Resonance", "Reso", 0, 100, "G:big", 25.0, "%");
+    pid_cutoff_ = add_param ("Cutoff", "Cutoff", 20, 24000, 1000, "Hz");
+    pid_resonance_ = add_param ("Resonance", "Reso", 0, 100, 25.0, "%");
     ChoiceEntries centries;
     centries += { "L4", "4 pole lowpass, 24db/octave" };
     centries += { "L3", "3 pole lowpass, 18db/octave" };
     centries += { "L2", "2 pole lowpass, 12db/octave" };
     centries += { "L1", "1 pole lowpass, 6db/octave" };
     centries += { "None", "disable filter" };
-    pid_mode_ = add_param ("Filter Mode", "Mode", std::move (centries), "G:dropdown", 0, "Ladder Filter Mode to be used");
+    pid_mode_ = add_param ("Filter Mode", "Mode", std::move (centries), 0, "", "Ladder Filter Mode to be used");
 
     oscparams (1);
 
     start_param_group ("Volume Envelope");
-    pid_attack_ = add_param ("Attack", "A", 0, 100, "G:big", 11.0, "%");
-    pid_decay_  = add_param ("Decay", "D", 0, 100, "G:big", 20.0, "%");
-    pid_sustain_ = add_param ("Sustain", "S", 0, 100, "G:big", 50.0, "%");
-    pid_release_ = add_param ("Release", "R", 0, 100, "G:big", 30.0, "%");
+    pid_attack_ = add_param ("Attack", "A", 0, 100, 11.0, "%");
+    pid_decay_  = add_param ("Decay", "D", 0, 100, 20.0, "%");
+    pid_sustain_ = add_param ("Sustain", "S", 0, 100, 50.0, "%");
+    pid_release_ = add_param ("Release", "R", 0, 100, 30.0, "%");
 
     start_param_group ("Mix");
-    pid_mix_ = add_param ("Mix", "Mix", 0, 100, "G:big", 0, "%");
+    pid_mix_ = add_param ("Mix", "Mix", 0, 100, 0, "%");
 
     start_param_group ("Keyboard Input");
-    pid_c_ = add_param ("Main Input  1",  "C", "G:big", false);
-    pid_d_ = add_param ("Main Input  2",  "D", "G:big", false);
-    pid_e_ = add_param ("Main Input  3",  "E", "G:big", false);
-    pid_f_ = add_param ("Main Input  4",  "F", "G:big", false);
-    pid_g_ = add_param ("Main Input  5",  "G", "G:big", false);
+    pid_c_ = add_param ("Main Input  1",  "C", false);
+    pid_d_ = add_param ("Main Input  2",  "D", false);
+    pid_e_ = add_param ("Main Input  3",  "E", false);
+    pid_f_ = add_param ("Main Input  4",  "F", false);
+    pid_g_ = add_param ("Main Input  5",  "G", false);
     old_c_ = old_d_ = old_e_ = old_f_ = old_g_ = false;
   }
   void
