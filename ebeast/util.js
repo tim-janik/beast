6315e25679ab03b8dda815a153949d36fe4ec533
Tim Janik <timj@gnu.org>
2020-08-01 17:55:27 +0200

    EBEAST: util.js: add element_text(), clone_menu_icon()
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ebeast/util.js
+++ ebeast/util.js
@@ -1704,6 +1704,39 @@ export function matches_forof (element, iteratable)
   return false;
 }
 
+/// Extract filtered text nodes from Element.
+export function element_text (element, filter)
+{
+  let texts = [];
+  const each = e => {
+    if (e.nodeType == 3) // text element
+      {
+	texts.push (e.data);
+	return;
+      }
+    if (filter && filter (e) == false)
+      return;
+    for (const c of e.childNodes)
+      each (c);
+  };
+  each (element);
+  return texts.join ('');
+}
+
+/// Clone a menuitem icon via its `uri`.
+export function clone_menu_icon (menu, uri, title = '')
+{
+  const menuitem = menu?.find_menuitem (uri);
+  if (!menuitem)
+    return {};
+  App.zmove (); // pick up 'data-tip'
+  return {
+    ic: menuitem.ic, fa: menuitem.fa, mi: menuitem.mi, uc: menuitem.uc,
+    'data-kbd': menuitem.kbd,
+    'data-tip': title ? title + ' ' + menuitem.get_text() : '',
+  };
+}
+
 /// Symbolic names for key codes
 export const KeyCode = {
   BACKSPACE: 8, TAB: 9, LINEFEED: 10, ENTER: 13, RETURN: 13, CAPITAL: 20, CAPSLOCK: 20, ESC: 27, ESCAPE: 27, SPACE: 32,
