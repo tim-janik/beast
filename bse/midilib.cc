875eb093cd06892cbd1fd2b57ba24097a13892a3
Tim Janik <timj@gnu.org>
2020-08-12 14:15:12 +0200

    BSE: midilib: start MIDI input device
    Signed-off-by: Tim Janik <timj@gnu.org>

---
+++ bse/midilib.cc
@@ -0,0 +1,51 @@
+// Licensed GNU LGPL v2.1 or later: http://www.gnu.org/licenses/lgpl.html
+#include "midilib.hh"
+#include <bse/signalmath.hh>
+#include "bse/internal.hh"
+
+#define MDEBUG(...)     Bse::debug ("midilib", __VA_ARGS__)
+
+namespace Bse {
+
+/// Namespace used for Midi Processor implementations.
+namespace MidiLib {
+using namespace AudioSignal;
+
+// == MidiInputImpl ==
+class MidiInputImpl : public AudioSignal::Processor {
+  void
+  query_info (ProcessorInfo &info) override
+  {
+    info.uri = "Bse.MidiLib.MidiInput";
+    info.version = "0";
+    info.label = "MIDI-Input";
+    info.category = "Input & Output";
+  }
+  void
+  initialize () override
+  {}
+  void
+  configure (uint n_ibusses, const SpeakerArrangement *ibusses, uint n_obusses, const SpeakerArrangement *obusses) override
+  {
+    remove_all_buses();
+    prepare_event_output();
+  }
+  void
+  reset() override
+  {}
+  void
+  render (uint n_frames) override
+  {
+    EventStream evout = get_event_output(); // needs prepare_event_output()
+    EventRange evinp { evout };
+    uint n = evinp.events_pending();
+    (void) n;
+    for (const auto ev : evinp)
+      if (ev.type == Event::NOTE_ON)
+        evout.append (ev.frame, ev);
+  }
+};
+static auto midilib_midiinputimpl = Bse::enroll_asp<MidiInputImpl>();
+
+} // MidiLib
+} // Bse
---
+++ bse/midilib.hh
@@ -0,0 +1,15 @@
+// Licensed GNU LGPL v2.1 or later: http://www.gnu.org/licenses/lgpl.html
+#ifndef __BSE_MIDILIB_HH__
+#define __BSE_MIDILIB_HH__
+
+#include <bse/processor.hh>
+
+namespace Bse {
+
+namespace MidiLib {
+
+} // MidiLib
+} // Bse
+
+#endif // __BSE_MIDILIB_HH__
+
