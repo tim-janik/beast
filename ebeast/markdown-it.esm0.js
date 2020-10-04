3e35dd6fc50af919e0cab47c6482fc5adfa83de7
Tim Janik <timj@gnu.org>
2020-10-04 12:29:04 +0200

    EBEAST: upgrade markdown-it
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ebeast/markdown-it.esm0.js
+++ ebeast/markdown-it.esm0.js
@@ -1,10 +1,10 @@
 // This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 "use strict";
 
-// Rollup tends to mangle the target variable of require() assignments
-const required_markdown_it = require ("markdown-it");
+const required_markdown_it = require ("./node_modules/markdown-it/dist/markdown-it.js");
 
 const ESM6EXPORT = { default: required_markdown_it };
 
-// A Makefile rule turns the ESM6EXPORT.default assertion into a real export
 console.assert (ESM6EXPORT.default);
+
+module.exports = ESM6EXPORT.default;
